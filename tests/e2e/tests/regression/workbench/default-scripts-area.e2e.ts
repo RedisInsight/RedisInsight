378dbe7b05d0821f818efafb77c9355a0a9ceac6
Tanya Troyanova <tatsiana.trayanava@softeq.com>
2021-11-19 19:21:28 +0300

    edit the speed of drag

--- tests/e2e/tests/regression/workbench/default-scripts-area.e2e.ts
+++ tests/e2e/tests/regression/workbench/default-scripts-area.e2e.ts
@@ -28,12 +28,12 @@ test.only('Verify that user can resize(maximize)/(minimize) the enablement area 
     const offsetX = 100;
     const areaWidthBefore = await workbenchPage.preselectsAreaContainer.clientWidth;
     //Minimize the area with default scripts
-    await t.drag(workbenchPage.resizeButtonForPreselectsArea, -offsetX, 0, { speed: 0.2 });
+    await t.drag(workbenchPage.resizeButtonForPreselectsArea, -offsetX, 0, { speed: 0.1 });
     await t.expect(await workbenchPage.preselectsAreaContainer.clientWidth).lt(areaWidthBefore, 'Default scripts area is smaller after resize');
     //Maximize the area with default scripts
     const areaWidthAfter = await workbenchPage.preselectsAreaContainer.clientWidth;
     await t.doubleClick(workbenchPage.preselectsAreaContainer);
-    await t.drag(workbenchPage.resizeButtonForPreselectsArea, offsetX, 0, { speed: 0.2 });
+    await t.drag(workbenchPage.resizeButtonForPreselectsArea, offsetX, 0, { speed: 0.1 });
     await t.expect(await workbenchPage.preselectsAreaContainer.clientWidth).gt(areaWidthAfter, 'Default scripts area is bigger after resize');
 });
 test('Verify that user can expand/collapse the enablement area', async t => {

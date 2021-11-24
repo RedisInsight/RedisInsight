4c5dd2630dbc419bade765b13fbc866c226f487e
Tanya Troyanova <tatsiana.trayanava@softeq.com>
2021-11-24 18:14:44 +0300

    e2e - Store certificates

--- tests/e2e/tests/critical-path/database/verify-agreements.e2e.ts
+++ tests/e2e/tests/critical-path/database/verify-agreements.e2e.ts
@@ -1,9 +1,10 @@
 import { RequestMock } from 'testcafe';
 import { commonUrl } from '../../../helpers/conf';
-import { UserAgreementPage, AddRedisDatabasePage, SettingsPage } from '../../../pageObjects';
+import { UserAgreementPage, AddRedisDatabasePage, SettingsPage, MyRedisDatabasePage } from '../../../pageObjects';
 
 const addRedisDatabasePage = new AddRedisDatabasePage();
 const settingsPage = new SettingsPage();
+const myRedisDatabasePage = new MyRedisDatabasePage();
 
 const mockedSettingsResponse = {
     agreements: {
@@ -41,4 +42,15 @@ test('Verify that user should accept User Agreements to continue working with th
     //Verify I can work with the application
     await t.click(addRedisDatabasePage.addDatabaseButton);
     await t.expect(addRedisDatabasePage.addDatabaseManually.exists).ok('User can add a database');
+});
+test('Verify that user when user agrees to RI terms and conditions with encryption enabled, user is redirected to the Welcome page', async t => {
+    //Click on "I have read and understood the Server Side Public License" and submit
+    await t.click(settingsPage.switchEulaOption);
+    await t.click(settingsPage.submitConsentsPopupButton);
+    //Verify that Welcome page is displayed
+    await t.expect(addRedisDatabasePage.welcomePageTitle.exists).ok('Welcome page is displayed');
+});
+test('Verify that user when user agrees to RI terms and conditions with encryption enabled, user is redirected to the Welcome page', async t => {
+    // Verify that encryption enabled by default
+    await t.expect(userAgreementPage.switchOptionEncryption.withAttribute('aria-checked', 'true').exists).ok('Encryption enabled by default');
 });

1a853dc9e2ad4a7727aec0d5f18c01e421f983b1
Roman.Sergeenko <romansergeenko@softeq.com>
2021-12-27 14:46:30 +0300

    #RI-2125 - change host

--- redisinsight/ui/src/utils/common.ts
+++ redisinsight/ui/src/utils/common.ts
@@ -1,9 +1,8 @@
 const baseApiUrl = process.env.BASE_API_URL
-const resourcesBaseUrl = process.env.RESOURCES_BASE_URL
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isWebApp = process.env.APP_ENV === 'web'
 const apiPort = process.env.API_PORT
 
 export const getBaseApiUrl = () => (!isDevelopment && isWebApp
-  ? `${resourcesBaseUrl}:${apiPort}`
+  ? window.location.host
   : `${baseApiUrl}:${apiPort}`)

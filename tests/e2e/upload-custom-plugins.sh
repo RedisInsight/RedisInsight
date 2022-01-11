a05c65e050e8702d48b551c2dbfbd34d57be3367
Artem <artem.horuzhenko@softeq.com>
2022-01-11 11:01:25 +0300

    fix entrypoint to run command after finished

--- tests/e2e/upload-custom-plugins.sh
+++ tests/e2e/upload-custom-plugins.sh
@@ -4,4 +4,10 @@ curl --request GET -sL \
      --url 'https://s3.amazonaws.com/redisinsight.test/public/custom_plugins/plugins.zip'\
      --output './plugins.zip'
 
-unzip plugins.zip -d ./plugins
+echo "Custom plugins archive was downloaded"
+
+unzip -o plugins.zip -d ./plugins
+
+echo "Custom plugins were unarchived"
+
+exec "$@"

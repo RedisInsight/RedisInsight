eebf68ca23c43f53a28d773bff4b413a64444f93
Elena Naboko <elena.naboko@softeq.com>
2022-01-21 00:22:05 +0300

    [E2E] Add Stop Monitor test

--- tests/e2e/tests/regression/monitor/monitor.e2e.ts
+++ tests/e2e/tests/regression/monitor/monitor.e2e.ts
@@ -25,6 +25,10 @@ fixture `Monitor`
         await addNewStandaloneDatabase(ossStandaloneConfig);
         await myRedisDatabasePage.clickOnDBByName(ossStandaloneConfig.databaseName);
     })
+    .afterEach(async t => {
+        await t.click(myRedisDatabasePage.myRedisDBButton);
+        await myRedisDatabasePage.deleteDatabaseByName(ossStandaloneConfig.databaseName);
+    })
 test('Verify that when user closes the Monitor by clicking on "Close Monitor" button Monitor stopped', async t => {
     //Run monitor
     await monitorPage.startMonitor();
@@ -36,4 +40,14 @@ test('Verify that when user closes the Monitor by clicking on "Close Monitor" bu
     await t.click(monitorPage.expandMonitor);
     //Verify that when user reopens Monitor history is not displayed
     await t.expect(monitorPage.startMonitorButton.visible).ok('Start monitor button');
+});
+test('Verify that Monitor is stopped when user clicks on "Stop" button', async t => {
+    //Run monitor
+    await monitorPage.startMonitor();
+    //Click on Stop Monitor button
+    await t.click(monitorPage.runMonitorToggle);
+    //Check for "Monitor is stopped." text
+    await t.expect(monitorPage.monitorIsStoppedText.visible).ok('Monitor is stopped text');
+    //Check that no commands are displayed after "Monitor is stopped" text
+    await t.expect(monitorPage.monitorIsStoppedText.nextSibling().exists).notOk('No commands in monitor');
 });

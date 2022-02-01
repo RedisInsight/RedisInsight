c0b9834ba2576965934e48e8fe9d221fab0b07e9
Roman.Sergeenko <romansergeenko@softeq.com>
2022-02-01 14:27:24 +0300

    #RI-2392, RI-2393, RI-2394 - fix monitor output issues

--- redisinsight/ui/src/components/monitor/MonitorOutputList/MonitorOutputList.tsx
+++ redisinsight/ui/src/components/monitor/MonitorOutputList/MonitorOutputList.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { debounce } from 'lodash'
 import cx from 'classnames'
 import { EuiTextColor } from '@elastic/eui'
-import { CellMeasurer, List, CellMeasurerCache, ListRowProps, OnScrollParams } from 'react-virtualized'
+import { CellMeasurer, List, CellMeasurerCache, ListRowProps } from 'react-virtualized'
 
 import { getFormatTime } from 'uiSrc/utils'
 import { DEFAULT_TEXT } from 'uiSrc/components/notifications'
@@ -27,33 +27,41 @@ const PROTRUDING_OFFSET = 2
 
 const MonitorOutputList = (props: Props) => {
   const { compressed, items = [], width = 0, height = 0 } = props
-  const [autoScroll, setAutoScroll] = useState(true)
-  const [, forceRender] = useState({})
 
-  const handleWheel = ({ clientHeight, scrollTop, scrollHeight }: OnScrollParams) => {
-    setAutoScroll(clientHeight + scrollTop >= scrollHeight)
+  const listRef = useRef<List>(null)
+  const timeoutRef = useRef<any>(null)
+
+  const clearCacheAndUpdate = (scrollToBottom = true) => {
+    cache.clearAll()
+    listRef?.current?.recomputeGridSize()
+
+    if (scrollToBottom) {
+      listRef?.current?.scrollToRow(items.length - 1)
+      timeoutRef.current && clearTimeout(timeoutRef.current)
+
+      // double scrollToRow because of first call scrollToRow has bottom gap
+      timeoutRef.current = setTimeout(() => {
+        listRef?.current?.scrollToRow(items.length - 1)
+      }, 0)
+    }
   }
 
-  const updateWidth = debounce(() => {
-    cache.clearAll()
-    setTimeout(() => {
-      forceRender({})
-    }, 0)
-  }, 50, { maxWait: 100 })
+  const debouncedClearCacheAndUpdate = debounce(() => clearCacheAndUpdate(false), 50, { maxWait: 100 })
 
   useEffect(() => {
-    // function "handleWheel" after the first render rewrite initial state value "true"
-    setAutoScroll(true)
-
-    globalThis.addEventListener('resize', updateWidth)
+    globalThis.addEventListener('resize', debouncedClearCacheAndUpdate)
     return () => {
-      globalThis.removeEventListener('resize', updateWidth)
+      globalThis.removeEventListener('resize', debouncedClearCacheAndUpdate)
     }
   }, [])
 
   useEffect(() => {
-    updateWidth()
-  }, [width, compressed])
+    debouncedClearCacheAndUpdate()
+  }, [compressed])
+
+  useEffect(() => {
+    clearCacheAndUpdate()
+  }, [items])
 
   const getArgs = (args: string[]): JSX.Element => (
     <span className={cx(styles.itemArgs, { [styles.itemArgs__compressed]: compressed })}>
@@ -78,8 +86,8 @@ const MonitorOutputList = (props: Props) => {
         parent={parent}
         rowIndex={index}
       >
-        {({ registerChild }) => (
-          <div className={styles.item} ref={registerChild} style={style}>
+        {({ registerChild, measure }) => (
+          <div onLoad={measure} className={styles.item} ref={registerChild} style={style}>
             {!isError && (
               <>
                 <span>{getFormatTime(time)}</span>
@@ -98,6 +106,7 @@ const MonitorOutputList = (props: Props) => {
 
   return (
     <List
+      ref={listRef}
       width={width - PROTRUDING_OFFSET}
       height={height - PROTRUDING_OFFSET}
       rowCount={items.length}
@@ -106,8 +115,6 @@ const MonitorOutputList = (props: Props) => {
       overscanRowCount={30}
       className={styles.listWrapper}
       deferredMeasurementCache={cache}
-      scrollToIndex={autoScroll ? items.length - 1 : undefined}
-      onScroll={handleWheel}
     />
   )
 }

5ee83ec47347632c8af14af623772c2bdaacc8a7
tatsiana.trayanava <Tatsiana.Trayanava@softeq.com>
2022-02-08 16:23:00 +0300

    e2e - cli logical fix

--- tests/e2e/tests/regression/cli/cli.e2e.ts
+++ tests/e2e/tests/regression/cli/cli.e2e.ts
@@ -1,6 +1,6 @@
-import { acceptLicenseTerms, acceptLicenseTermsAndAddDatabase, deleteDatabase } from '../../../helpers/database';
+import { acceptLicenseTermsAndAddDatabase, deleteDatabase } from '../../../helpers/database';
 import { Common } from '../../../helpers/common';
-import { BrowserPage, CliPage, AddRedisDatabasePage, MyRedisDatabasePage } from '../../../pageObjects';
+import { BrowserPage, CliPage } from '../../../pageObjects';
 import {
     commonUrl,
     ossStandaloneConfig
@@ -12,18 +12,8 @@ const cliPage = new CliPage();
 const common = new Common();
 const chance = new Chance();
 const browserPage = new BrowserPage();
-const addRedisDatabasePage =  new AddRedisDatabasePage();
-const myRedisDatabasePage = new MyRedisDatabasePage();
 
 let keyName = chance.word({ length: 20 });
-let index = '0';
-let databaseEndpoint = `${ossStandaloneConfig.host}:${ossStandaloneConfig.port}`;
-const cliMessage = [
-    'Pinging Redis server on ',
-    databaseEndpoint,
-    'Connected.',
-    'Ready to execute commands.'
-];
 
 fixture `CLI`
     .meta({ type: 'regression' })
@@ -100,82 +90,4 @@ test
         await t.pressKey('enter');
         //Verify result
         await t.expect(cliPage.cliOutputResponseSuccess.count).eql(repeats, `CLI contains ${repeats} results`);
-});
-test
-    .meta({ rte: rte.standalone })
-    .before(async () => {
-        await acceptLicenseTerms();
-    })
-    ('Verify that working with logical DBs, user can not see 0 DB index in CLI', async t => {
-        await addRedisDatabasePage.addRedisDataBase(ossStandaloneConfig);
-        //Enter logical index
-        await t.click(addRedisDatabasePage.databaseIndexCheckbox);
-        await t.typeText(addRedisDatabasePage.databaseIndexInput, index, { paste: true });
-        //Click for saving
-        await t.click(addRedisDatabasePage.addRedisDatabaseButton);
-        await myRedisDatabasePage.clickOnDBByName(ossStandaloneConfig.databaseName);
-        //Open CLI
-        await t.click(cliPage.cliExpandButton);
-        //Verify that user can not see 0 DB index in CLI
-        for ( const text of cliMessage ) {
-            await t.expect(cliPage.cliArea.textContent).contains(text, 'No DB index is displayed in the CLI message');
-        }
-        await t.expect(cliPage.cliDbIndex.visible).eql(false, 'No DB index before the > character in CLI is displayed');
-        await t.expect(cliPage.cliEndpoint.textContent).eql(databaseEndpoint, 'Database index 0 is not displayed in the CLI endpoint');
-});
-test
-    .meta({ rte: rte.standalone })
-    .before(async () => {
-        await acceptLicenseTerms();
-    })
-    .after(async () => {
-        //Delete database
-        await deleteDatabase(ossStandaloneConfig.databaseName + ` [${index}]`);
-    })
-    ('Verify that working with logical DBs, user can see N DB index in CLI', async t => {
-        index = '1';
-        databaseEndpoint = `${ossStandaloneConfig.host}:${ossStandaloneConfig.port}[${index}]`;
-        await addRedisDatabasePage.addRedisDataBase(ossStandaloneConfig);
-        //Enter logical index
-        await t.click(addRedisDatabasePage.databaseIndexCheckbox);
-        await t.typeText(addRedisDatabasePage.databaseIndexInput, index, { paste: true });
-        //Click for saving
-        await t.click(addRedisDatabasePage.addRedisDatabaseButton);
-        await myRedisDatabasePage.clickOnDBByName(ossStandaloneConfig.databaseName + ` [${index}]`);
-        //Open CLI
-        await t.click(cliPage.cliExpandButton);
-        //Verify that user can see DB index in CLI
-        for ( const text of cliMessage ) {
-            await t.expect(cliPage.cliArea.textContent).contains(text, 'DB index is displayed in the CLI message');
-        }
-        await t.expect(cliPage.cliDbIndex.textContent).eql(`[${index}] `, 'DB index before the > character in CLI is displayed');
-        await t.expect(cliPage.cliEndpoint.textContent).eql(databaseEndpoint, 'Database index is displayed in the CLI endpoint');
-});
-test
-    .meta({ rte: rte.standalone })
-    .before(async () => {
-        await acceptLicenseTerms();
-    })
-    .after(async () => {
-        //Delete database
-        await deleteDatabase(ossStandaloneConfig.databaseName + ` [${index}]`);
-    })
-    ('Verify that when user re-creates client in CLI the new client is connected to the DB index selected for the DB by default', async t => {
-        index = '2';
-        databaseEndpoint = `${ossStandaloneConfig.host}:${ossStandaloneConfig.port}[${index}]`;
-        await addRedisDatabasePage.addRedisDataBase(ossStandaloneConfig);
-        //Enter logical index
-        await t.click(addRedisDatabasePage.databaseIndexCheckbox);
-        await t.typeText(addRedisDatabasePage.databaseIndexInput, index, { paste: true });
-        //Click for saving
-        await t.click(addRedisDatabasePage.addRedisDatabaseButton);
-        await myRedisDatabasePage.clickOnDBByName(ossStandaloneConfig.databaseName + ` [${index}]`);
-        //Open CLI and verify that user can see DB index in CLI
-        await t.click(cliPage.cliExpandButton);
-        await t.expect(cliPage.cliDbIndex.textContent).eql(`[${index}] `, 'DB index before the > character in CLI is displayed');
-        //Re-creates client in CLI
-        await t.click(cliPage.cliCollapseButton);
-        await t.click(cliPage.cliExpandButton);
-        //Verify that the new client is connected to the DB index selected for the DB by default
-        await t.expect(cliPage.cliDbIndex.textContent).eql(`[${index}] `, 'The new client is connected to the DB index selected for the DB by default');
 });

72ee6335aab28c40f91886668a11b6c0d008e20e
romansergeenkosofteq <82098578+romansergeenkosofteq@users.noreply.github.com>
2022-02-10 08:57:50 +0300

    Revert "Revert "#RI-2386 - Cover cases when user does not have permissions for Monitor command""

--- redisinsight/ui/src/constants/socketErrors.ts
+++ redisinsight/ui/src/constants/socketErrors.ts
@@ -1,3 +1,8 @@
 export enum SocketErrors {
   TransportError = 'TransportError',
 }
+
+export enum MonitorErrorMessages {
+  NoPerm = 'The Profiler cannot be started. This user has no permissions to run the \'monitor\' command',
+  LostConnection = 'Error: Connection was lost'
+}

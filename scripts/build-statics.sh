8a0c12b4f4d6b8f445a7dc4a89c7d344a42503ac
Artyom Podymov <artyom.podymov@softeq.com>
2022-02-22 18:45:10 +0300

    #RI-2219 - get static content from repo

--- scripts/build-statics.sh
+++ scripts/build-statics.sh
@@ -18,6 +18,3 @@ yarn --cwd "${REDISEARCH_DIR}"
 yarn --cwd "${REDISEARCH_DIR}" build
 mkdir -p "${PLUGINS_DIR}/redisearch"
 cp -R "${REDISEARCH_DIR}/dist" "${REDISEARCH_DIR}/package.json" "${PLUGINS_DIR}/redisearch"
-
-# =============== Static content ===============
-cp -R ".\redisinsight\ui\src\packages\content" ".\redisinsight\api\static\content\"

878a25b9f47085312ab8715ba44af8f733f341bb
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2022-03-05 00:41:30 +0300

    added files

---
+++ redisinsight/ui/src/utils/redistack.ts
@@ -0,0 +1,30 @@
+import { map, isEqual } from 'lodash'
+import { Instance } from 'uiSrc/slices/interfaces'
+
+export const REDISTACK_PORT = 6379
+export const REDISTACK_MODULES = ['ReJSON', 'graph', 'timeseries', 'search', 'bf'].sort()
+
+const checkRediStack = (instances: Instance[]): Instance[] => {
+  let isRediStack = false
+
+  let newInstances = instances.map((instance) => {
+    isRediStack = instance.port === REDISTACK_PORT && isEqual(map(instance.modules, 'name').sort(), REDISTACK_MODULES)
+    return {
+      ...instance,
+      isRediStack
+    }
+  })
+
+  if (!isRediStack) {
+    newInstances = newInstances.map((instance) => ({
+      ...instance,
+      isRediStack: isEqual(map(instance.modules, 'name').sort(), REDISTACK_MODULES)
+    }))
+  }
+
+  console.log(instances, newInstances)
+
+  return newInstances
+}
+
+export { checkRediStack }

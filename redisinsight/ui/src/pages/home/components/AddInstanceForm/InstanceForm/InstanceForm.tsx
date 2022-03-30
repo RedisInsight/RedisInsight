c79e36f46c3cb254c3326af34061eab00e4b984f
Roman.Sergeenko <roman.sergeenko@softeq.com>
2022-03-30 15:04:10 +0300

    #RI-2651 - fix logo for overview #RI-2650 - fix label for checkbox

--- redisinsight/ui/src/pages/home/components/AddInstanceForm/InstanceForm/InstanceForm.tsx
+++ redisinsight/ui/src/pages/home/components/AddInstanceForm/InstanceForm/InstanceForm.tsx
@@ -709,7 +709,7 @@ const AddStandaloneForm = (props: Props) => {
             <EuiCheckbox
               id={`${htmlIdGenerator()()} over db`}
               name="showDb"
-              label="Database Index"
+              label="Select the Redis logical database"
               checked={!!formik.values.showDb}
               onChange={handleChangeDbIndexCheckbox}
               data-testid="showDb"
@@ -737,7 +737,7 @@ const AddStandaloneForm = (props: Props) => {
             { [styles.dbInputBig]: !flexItemClassName }
           )}
           >
-            <EuiFormRow label="Select the Redis logical database" helpText="Should not exceed 15.">
+            <EuiFormRow label="Database Index" helpText="Should not exceed 15.">
               <EuiFieldNumber
                 name="db"
                 id="db"

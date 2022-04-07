cffd35d1f7195d94c250e4ae1e9e4b719d0e8764
Roman.Sergeenko <roman.sergeenko@softeq.com>
2022-04-07 12:06:46 +0300

    #RI-2661 - fix color for info icon on open #RI-2670 - sort data types #RI-2675 - UI fixes for filter #RI-2676 - show clear btn when options are added #RI-2678 - fix clear btn text #RI-2679 - remove titles for badges

--- redisinsight/ui/src/pages/browser/components/filter-key-type/constants.ts
+++ redisinsight/ui/src/pages/browser/components/filter-key-type/constants.ts
@@ -12,9 +12,9 @@ export const FILTER_KEY_TYPE_OPTIONS = [
     color: GROUP_TYPES_COLORS[KeyTypes.Hash],
   },
   {
-    text: 'Sorted Set',
-    value: KeyTypes.ZSet,
-    color: GROUP_TYPES_COLORS[KeyTypes.ZSet],
+    text: 'List',
+    value: KeyTypes.List,
+    color: GROUP_TYPES_COLORS[KeyTypes.List],
   },
   {
     text: 'Set',
@@ -22,14 +22,14 @@ export const FILTER_KEY_TYPE_OPTIONS = [
     color: GROUP_TYPES_COLORS[KeyTypes.Set],
   },
   {
+    text: 'Sorted Set',
+    value: KeyTypes.ZSet,
+    color: GROUP_TYPES_COLORS[KeyTypes.ZSet],
+  },
+  {
     text: 'String',
     value: KeyTypes.String,
     color: GROUP_TYPES_COLORS[KeyTypes.String],
-  },
-  {
-    text: 'List',
-    value: KeyTypes.List,
-    color: GROUP_TYPES_COLORS[KeyTypes.List],
   },
   {
     text: 'JSON',

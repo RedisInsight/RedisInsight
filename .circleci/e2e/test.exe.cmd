bde74c7d0293647ae420088960e47da69b34334c
Artem <artem.horuzhenko@softeq.com>
2022-04-14 09:09:58 +0300

    fix app name in the win e2e

--- .circleci/e2e/test.exe.cmd
+++ .circleci/e2e/test.exe.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set COMMON_URL=%USERPROFILE%/AppData/Local/Programs/redisinsight/resources/app.asar/index.html
-set ELECTRON_PATH=%USERPROFILE%/AppData/Local/Programs/redisinsight/RedisInsight-preview.exe
+set ELECTRON_PATH=%USERPROFILE%/AppData/Local/Programs/redisinsight/RedisInsight-v2.exe
 set OSS_STANDALONE_HOST=%E2E_CLOUD_DATABASE_HOST%
 set OSS_STANDALONE_PORT=%E2E_CLOUD_DATABASE_PORT%
 set OSS_STANDALONE_USERNAME=%E2E_CLOUD_DATABASE_USERNAME%
@@ -10,14 +10,14 @@ set SOCKETS_CORS=true
 
 call yarn --cwd tests/e2e install
 
-call "./release/RedisInsight-preview-win-installer.exe"
+call "./release/RedisInsight-v2-win-installer.exe"
 
 :: waiting until app auto launches
 timeout 5
 
 :: close an auto launched app
-taskkill /im RedisInsight-preview.exe /f
-taskkill /im RedisInsight-preview-win-installer.exe /f
+taskkill /im RedisInsight-v2.exe /f
+taskkill /im RedisInsight-v2-win-installer.exe /f
 
 :: run tests
 call yarn --cwd tests/e2e dotenv -e .desktop.env yarn --cwd tests/e2e test:desktop:ci:win

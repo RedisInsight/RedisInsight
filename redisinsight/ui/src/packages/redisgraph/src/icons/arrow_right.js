9b66205d10a0df1fbcdd2dc86d108ad9e2458e72
tatsiana.trayanava <Tatsiana.Trayanava@softeq.com>
2022-05-14 17:00:04 +0300

    handle error for redisgraph icons for testcafe/hammerhead

--- redisinsight/ui/src/packages/redisgraph/src/icons/arrow_right.js
+++ redisinsight/ui/src/packages/redisgraph/src/icons/arrow_right.js
@@ -17,17 +17,24 @@ const EuiIconArrowRight = function EuiIconArrowRight(_ref) {
   const { titleId } = _ref
   const props = _objectWithoutProperties(_ref, ['title', 'titleId'])
 
-  return /* #__PURE__ */React.createElement('svg', { width: 16,
-    height: 16,
-    viewBox: '0 0 16 16',
-    xmlns: 'http://www.w3.org/2000/svg',
-    'aria-labelledby': titleId,
-    ...props }, title ? /* #__PURE__ */React.createElement('title', {
-    id: titleId
-  }, title) : null, /* #__PURE__ */React.createElement('path', {
-    fillRule: 'nonzero',
-    d: 'M5.157 13.069l4.611-4.685a.546.546 0 000-.768L5.158 2.93a.552.552 0 010-.771.53.53 0 01.759 0l4.61 4.684c.631.641.63 1.672 0 2.312l-4.61 4.684a.53.53 0 01-.76 0 .552.552 0 010-.771z'
-  }))
+  // For e2e tests. Hammerhead cannot create svg throw createElementNS
+  try {
+    document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+    return /* #__PURE__ */React.createElement('svg', { width: 16,
+      height: 16,
+      viewBox: '0 0 16 16',
+      xmlns: 'http://www.w3.org/2000/svg',
+      'aria-labelledby': titleId,
+      ...props }, title ? /* #__PURE__ */React.createElement('title', {
+      id: titleId
+    }, title) : null, /* #__PURE__ */React.createElement('path', {
+      fillRule: 'nonzero',
+      d: 'M5.157 13.069l4.611-4.685a.546.546 0 000-.768L5.158 2.93a.552.552 0 010-.771.53.53 0 01.759 0l4.61 4.684c.631.641.63 1.672 0 2.312l-4.61 4.684a.53.53 0 01-.76 0 .552.552 0 010-.771z'
+    }))
+  } catch (e) {
+    return <span>&#8594;</span>
+  }
 }
 
 export var icon = EuiIconArrowRight

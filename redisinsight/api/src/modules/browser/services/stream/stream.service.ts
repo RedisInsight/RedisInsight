cfdd917cb23b3c300af9baed7343771a260284a1
Amir Allayarov <amir.allayarov@softeq.com>
2022-05-17 19:46:47 +0400

    #RI-2855- update stream check

--- redisinsight/api/src/modules/browser/services/stream/stream.service.ts
+++ redisinsight/api/src/modules/browser/services/stream/stream.service.ts
@@ -77,12 +77,12 @@ export class StreamService {
         keyName,
         total: info.length,
         lastGeneratedId: info['last-generated-id'],
-        firstEntry: entries.length !== 0
-          ? StreamService.formatReplyToDto([info['first-entry']])[0]
-          : null,
-        lastEntry: entries.length !== 0
-          ? StreamService.formatReplyToDto([info['last-entry']])[0]
-          : null,
+        firstEntry: info['first-entry'] === null
+          ? null
+          : StreamService.formatReplyToDto([info['first-entry']])[0],
+        lastEntry: info['last-entry'] === null
+          ? null
+          : StreamService.formatReplyToDto([info['last-entry']])[0],
         entries,
       };
     } catch (error) {

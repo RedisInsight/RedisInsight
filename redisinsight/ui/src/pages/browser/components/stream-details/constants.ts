b575cac13b2eb6e3e2658dca8641e5aa3a6cc184
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2022-06-02 10:58:08 +0400

    * #RI-3004 - The navigation icon in the tabs list is wider then on mockups * #RI-3006 - There is no space between the opened Pending message and arrow icon in the navigation

--- redisinsight/ui/src/pages/browser/components/stream-details/constants.ts
+++ redisinsight/ui/src/pages/browser/components/stream-details/constants.ts
@@ -1,6 +1,14 @@
+import React from 'react'
 import { StreamViewType } from 'uiSrc/slices/interfaces/stream'
+import { Maybe } from 'uiSrc/utils'
 
-export const streamViewTypeTabs = [
+interface StreamTabs {
+  id: StreamViewType,
+  label: string,
+  separator?: Maybe<React.ReactElement>
+}
+
+export const streamViewTypeTabs: StreamTabs[] = [
   {
     id: StreamViewType.Data,
     label: 'Stream data',

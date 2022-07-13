95085c6ccafbb23aa5246ebe60d18313018247c9
vlad-dargel <u.darhel@aisnovations.com>
2022-07-13 18:48:11 +0200

    updates by pr comments

--- tests/e2e/tests/regression/pub-sub/debug-mode.e2e.ts
+++ tests/e2e/tests/regression/pub-sub/debug-mode.e2e.ts
@@ -25,36 +25,36 @@ fixture `PubSub debug mode`
     });
 
 test('Verify that when user navigating away and back to pubsub window the debug mode state will be reset to default auto-scroll', async t => {
-        //Scroll to the first messages
-        await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('first'));
-        //Verify that new messages keep arriving in the background and user can always see up to date count of total messages received.
-        await pubSubPage.publishMessage('test', 'message sent in the background');
-        await t.expect(pubSubPage.totalMessagesCount.textContent).contains('31', 'Total counter value is incorrect');
-        //Verify that when user scroll away from the newest message the auto-scroll is stopped
-        await cliPage.sendCommandInCli('30 publish channel additionalMessages');
-        await pubSubPage.publishMessage('test', 'new message with no scroll');
-        await t.expect(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('new message with no scroll').visible).notOk('Message is still visible');
-        //Go to Browser Page
-        await t.click(myRedisDatabasePage.myRedisDBButton);
-        //Go to PubSub page
-        await t.click(myRedisDatabasePage.pubSubButton);
-        //Verify that the debug mode state is reset to default auto-scroll
-        await t.expect(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('new message with no scroll').visible).ok('Message is not visible');
-    });
+    //Scroll to the first messages
+    await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('first'));
+    //Verify that new messages keep arriving in the background and user can always see up to date count of total messages received.
+    await pubSubPage.publishMessage('test', 'message sent in the background');
+    await t.expect(pubSubPage.totalMessagesCount.textContent).contains('31', 'Total counter value is incorrect');
+    //Verify that when user scroll away from the newest message the auto-scroll is stopped
+    await cliPage.sendCommandInCli('30 publish channel additionalMessages');
+    await pubSubPage.publishMessage('test', 'new message with no scroll');
+    await pubSubPage.verifyMessageDisplaying('new message with no scroll', false);
+    //Go to Browser Page
+    await t.click(myRedisDatabasePage.myRedisDBButton);
+    //Go to PubSub page
+    await t.click(myRedisDatabasePage.pubSubButton);
+    //Verify that the debug mode state is reset to default auto-scroll
+    await pubSubPage.verifyMessageDisplaying('new message with no scroll', true);
+});
 test('Verify that when user scroll all the way to the newest available message (down), auto-scroll resumes automatically.', async t => {
-        //Scroll to the first messages
-        await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('first'));
-        await pubSubPage.publishMessage('test', 'message to scroll');
-        await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('message to scroll'));
-        await cliPage.sendCommandInCli('20 publish channel fourth');
-        //Verify auto-scroll resumes automatically
-        await t.expect(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('fourth').visible).ok('Message is not visible');
-    });
+    //Scroll to the first messages
+    await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('first'));
+    await pubSubPage.publishMessage('test', 'message to scroll');
+    await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('message to scroll'));
+    await cliPage.sendCommandInCli('20 publish channel fourth');
+    //Verify auto-scroll resumes automatically
+    await pubSubPage.verifyMessageDisplaying('fourth', true);
+});
 test('Verify that user can get to the newest message in one click', async t => {
-        //Scroll to the first messages
-        await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('first'));
-        await pubSubPage.publishMessage('test', 'new message');
-        await t.click(pubSubPage.scrollDownButton);
-        //Verify the user scrolled to the newest message
-        await t.expect(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('new message').visible).ok('The newest message is not visible', { timeout: 5000 });
-    });
+    //Scroll to the first messages
+    await t.scrollIntoView(pubSubPage.pubSubPageContainer.find(pubSubPage.cssSelectorMessage).withText('first'));
+    await pubSubPage.publishMessage('test', 'new message');
+    await t.click(pubSubPage.scrollDownButton);
+    //Verify the user scrolled to the newest message
+    await pubSubPage.verifyMessageDisplaying('new message', true);
+});

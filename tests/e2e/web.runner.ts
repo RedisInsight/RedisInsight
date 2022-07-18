96a308f27afa856843304ee0b39492aa51b28d9e
vlad-dargel <u.darhel@aisnovations.com>
2022-07-18 13:27:05 +0200

    updates for web runner

--- tests/e2e/web.runner.ts
+++ tests/e2e/web.runner.ts
@@ -31,7 +31,7 @@ import testcafe from 'testcafe';
                     browserInitTimeout: 60000,
                     selectorTimeout: 5000,
                     speed: 1,
-                    quarantineMode: { successThreshold: '1', attemptLimit: '3' }
+                    quarantineMode: { successThreshold: 1, attemptLimit: 3 }
                 });
         })
         .then((failedCount) => {

2023e7b545884d1cb495a58b78b7c39212585030
Artem <artem.horuzhenko@softeq.com>
2022-07-19 13:05:01 +0300

    replace oss-5-cluster with oss-7-cluster

---
+++ tests/e2e/rte/oss-cluster-7/cluster-create.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo 'Try to sleep for a while...'
+sleep 5
+echo 'Creating cluster...'
+echo "yes" | redis-cli \
+  --cluster create \
+  172.31.100.211:6379 \
+  172.31.100.212:6379 \
+  172.31.100.213:6379 \
+  --cluster-replicas 0

bda51cd6eed20aaaae00c58554f77c3179d3da25
vlad-dargel <u.darhel@aisnovations.com>
2022-07-21 19:00:24 +0200

    add tests + api steps

---
+++ tests/e2e/helpers/api/api-keys.ts
@@ -0,0 +1,105 @@
+import { t } from 'testcafe';
+import * as request from 'supertest';
+import { AddNewDatabaseParameters } from '../../pageObjects/add-redis-database-page';
+import { apiUrl } from '../../helpers/conf';
+import { HashKeyParameters, ListKeyParameters, SetKeyParameters, SortedSetKeyParameters, StreamKeyParameters } from '../../pageObjects/browser-page';
+import { getDatabaseByName } from './api-database';
+
+const endpoint = apiUrl;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // lgtm[js/disabling-certificate-validation]
+
+/**
+ * Add Hash key
+ * @param keyParameters The key parameters
+ */
+export async function addHashKeyApi(keyParameters: HashKeyParameters, databaseParameters: AddNewDatabaseParameters): Promise<void> {
+    const databaseId = await getDatabaseByName(databaseParameters.databaseName);
+    const response = await request(endpoint).post(`/instance/${databaseId}/hash`)
+        .send({
+            'keyName': keyParameters.keyName,
+            'fields': keyParameters.fields
+        })
+        .set('Accept', 'application/json');
+
+    await t.expect(response.status).eql(201, 'The creation of new Hash key request failed');
+}
+
+/**
+ * Add Stream key
+ * @param keyParameters The key parameters
+ */
+export async function addStreamKeyApi(keyParameters: StreamKeyParameters, databaseParameters: AddNewDatabaseParameters): Promise<void> {
+    const databaseId = await getDatabaseByName(databaseParameters.databaseName);
+    const response = await request(endpoint).post(`/instance/${databaseId}/streams`)
+        .send({
+            'keyName': keyParameters.keyName,
+            'entries': keyParameters.entries
+        })
+        .set('Accept', 'application/json');
+
+    await t.expect(response.status).eql(201, 'The creation of new Stream key request failed');
+}
+
+/**
+ * Add Set key
+ * @param keyParameters The key parameters
+ */
+ export async function addSetKeyApi(keyParameters: SetKeyParameters, databaseParameters: AddNewDatabaseParameters): Promise<void> {
+    const databaseId = await getDatabaseByName(databaseParameters.databaseName);
+    const response = await request(endpoint).post(`/instance/${databaseId}/set`)
+        .send({
+            'keyName': keyParameters.keyName,
+            'members': keyParameters.members
+        })
+        .set('Accept', 'application/json');
+
+    await t.expect(response.status).eql(201, 'The creation of new Set key request failed');
+}
+
+/**
+ * Add Sorted Set key
+ * @param keyParameters The key parameters
+ */
+ export async function addSortedSetKeyApi(keyParameters: SortedSetKeyParameters, databaseParameters: AddNewDatabaseParameters): Promise<void> {
+    const databaseId = await getDatabaseByName(databaseParameters.databaseName);
+    const response = await request(endpoint).post(`/instance/${databaseId}/zSet`)
+        .send({
+            'keyName': keyParameters.keyName,
+            'members': keyParameters.members
+        })
+        .set('Accept', 'application/json');
+
+    await t.expect(response.status).eql(201, 'The creation of new Sorted Set key request failed');
+}
+
+/**
+ * Add List key
+ * @param keyParameters The key parameters
+ */
+ export async function addListKeyApi(keyParameters: ListKeyParameters, databaseParameters: AddNewDatabaseParameters): Promise<void> {
+    const databaseId = await getDatabaseByName(databaseParameters.databaseName);
+    const response = await request(endpoint).post(`/instance/${databaseId}/list`)
+        .send({
+            'keyName': keyParameters.keyName,
+            'element': keyParameters.element
+        })
+        .set('Accept', 'application/json');
+
+    await t.expect(response.status).eql(201, 'The creation of new List key request failed');
+}
+
+/**
+ * Delete Key by name
+ * @param keyParameters The key parameters
+ */
+ export async function deleteKeyApi(keyParameters: ListKeyParameters, databaseParameters: AddNewDatabaseParameters): Promise<void> {
+    const databaseId = await getDatabaseByName(databaseParameters.databaseName);
+    const response = await request(endpoint).post(`/instance/${databaseId}/list`)
+        .send({
+            'keyName': keyParameters.keyName,
+            'element': keyParameters.element
+        })
+        .set('Accept', 'application/json');
+
+    await t.expect(response.status).eql(201, 'The creation of new List key request failed');
+}
--- tests/e2e/tests/regression/browser/large-key-details-values.e2e.ts
+++ tests/e2e/tests/regression/browser/large-key-details-values.e2e.ts
@@ -1,48 +1,129 @@
 import { rte } from '../../../helpers/constants';
 import { acceptLicenseTermsAndAddDatabaseApi } from '../../../helpers/database';
-import { BrowserPage, CliPage } from '../../../pageObjects';
+import { BrowserPage } from '../../../pageObjects';
 import { commonUrl, ossStandaloneConfig } from '../../../helpers/conf';
 import { deleteStandaloneDatabaseApi } from '../../../helpers/api/api-database';
 import { Common } from '../../../helpers/common';
+import { addHashKeyApi, addListKeyApi, addSetKeyApi, addSortedSetKeyApi, addStreamKeyApi } from '../../../helpers/api/api-keys';
 
 const browserPage = new BrowserPage();
-const cliPage = new CliPage();
 const common = new Common();
 
-let keyName = common.generateWord(20);
+let hashKeyName = common.generateWord(20);
+let streamKeyName = common.generateWord(20);
+let setKeyName = common.generateWord(20);
+let sortedSetKeyName = common.generateWord(20);
+let listKeyName = common.generateWord(20);
 let field = common.generateWord(20);
 let value = common.generateSentence(200);
+let value1 = common.generateWord(20);
+let hashKeyParameters = { keyName: hashKeyName, fields: [{ field, value: value1 }, { field, value }] };
+let streamKeyParameters = { keyName: streamKeyName, entries: [{ id: '*', fields: [[field, value], [field, value1]] }] };
+let setKeyParameters = { keyName: setKeyName, members: [value] };
+let sortedSetKeyParameters = { keyName: sortedSetKeyName, members: [{ name: value, score: '1' }] };
+let listKeyParameters = { keyName: listKeyName, element: value };
 
-fixture `Expand/Collapse large values in key details`
+fixture`Expand/Collapse large values in key details`
     .meta({ type: 'regression', rte: rte.standalone })
     .page(commonUrl)
     .beforeEach(async () => {
         await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneConfig, ossStandaloneConfig.databaseName);
+        await addHashKeyApi(hashKeyParameters, ossStandaloneConfig);
+        await addStreamKeyApi(streamKeyParameters, ossStandaloneConfig);
+        await addSetKeyApi(setKeyParameters, ossStandaloneConfig);
+        await addSortedSetKeyApi(sortedSetKeyParameters, ossStandaloneConfig);
+        await addListKeyApi(listKeyParameters, ossStandaloneConfig);
     })
     .afterEach(async () => {
         //Delete database
-        await browserPage.deleteKeyByName(keyName);
+        await browserPage.deleteKeyByName(hashKeyName);
+        await browserPage.deleteKeyByName(streamKeyName);
+        await browserPage.deleteKeyByName(setKeyName);
+        await browserPage.deleteKeyByName(sortedSetKeyName);
+        await browserPage.deleteKeyByName(listKeyName);
         await deleteStandaloneDatabaseApi(ossStandaloneConfig);
     })
 test('Verify that user can click on a row to expand it if any of its cells contains a value which is truncated.', async t => {
-    let value1 = common.generateWord(20);
-    let cliCommands = [
-        `XADD ${keyName} * '${field}' '${value}'`,
-        `XADD ${keyName} * '${field}' '${value1}'`
-    ]
-    //Add new Stream key with 2 fields
-    for (const command of cliCommands) {
-        await cliPage.sendCommandInCli(command);
-    }
-    //Open key details
-    await browserPage.openKeyDetails(keyName);
+    const entryFieldLong = browserPage.streamEntryFields.nth(1).parent(1);
+    const entryFieldSmall = browserPage.streamEntryFields.nth(0).parent(1);
+    // Open Stream key details
+    await browserPage.openKeyDetails(streamKeyName);
+    // Remember height of the cells
+    const startLongCellHeight = await entryFieldLong.clientHeight;
+    const startSmallCellHeight = await entryFieldSmall.clientHeight;
+    await t.click(entryFieldSmall);
+    await t.expect(entryFieldSmall.clientHeight).lt(startSmallCellHeight + 5, 'Row is expanded', { timeout: 5000 });
+    // Verify that user can expand/collapse for stream data type
+    await t.click(entryFieldLong);
+    await t.expect(entryFieldLong.clientHeight).gt(startLongCellHeight + 150, 'Row is not expanded', { timeout: 5000 });
+    // Verify that user can collapse the row by clicking anywhere on the expanded row
+    await t.click(entryFieldLong);
+    await t.expect(entryFieldLong.clientHeight).eql(startLongCellHeight, 'Row is not collapsed', { timeout: 5000 });
+});
+test('Verify that user can expand/collapse for hash data type', async t => {
+    const fieldValueCell = browserPage.hashFieldValue.parent(2);
+    // Open Hash key details
+    await browserPage.openKeyDetails(hashKeyName);
     // Remember height of the cell with long value
-    const startLongCellHeight = await browserPage.streamEntryFields.nth(1).parent(1).clientHeight;
-    const startSmallCellHeight = await browserPage.streamEntryFields.nth(0).parent(1).clientHeight;
-    const endLongCellHeight = startLongCellHeight + 150;
-    const endSmallCellHeight = startSmallCellHeight + 5;
-    await t.click(browserPage.streamEntryFields.nth(0).parent(1));
-    await t.expect(browserPage.streamEntryFields.nth(0).parent(1).clientHeight).lt(endSmallCellHeight, 'Cell height is expanded', { timeout: 5000 });
-    await t.click(browserPage.streamEntryFields.nth(1).parent(1));
-    await t.expect(browserPage.streamEntryFields.nth(1).parent(1).clientHeight).gt(endLongCellHeight, 'Cell height is not expanded', { timeout: 5000 });
+    const startCellHeight = await fieldValueCell.clientHeight;
+    // Verify that user can expand a row of hash data type
+    await t.click(fieldValueCell);
+    await t.expect(fieldValueCell.clientHeight).gt(startCellHeight + 150, 'Row is not expanded', { timeout: 5000 });
+    // Verify that user can collapse a row of hash data type
+    await t.click(fieldValueCell);
+    await t.expect(fieldValueCell.clientHeight).eql(startCellHeight, 'Row is not collapsed', { timeout: 5000 });
+});
+test('Verify that user can expand/collapse for set data type', async t => {
+    const memberValueCell = browserPage.setMembersList.parent(2);
+    // Open Set key details
+    await browserPage.openKeyDetails(setKeyName);
+    // Remember height of the cell with long value
+    const startLongCellHeight = await memberValueCell.clientHeight;
+    // Verify that user can expand a row of set data type
+    await t.click(memberValueCell);
+    await t.expect(memberValueCell.clientHeight).gt(startLongCellHeight + 150, 'Row is not expanded', { timeout: 5000 });
+    // Verify that user can collapse a row of set data type
+    await t.click(memberValueCell);
+    await t.expect(memberValueCell.clientHeight).eql(startLongCellHeight, 'Row is not collapsed', { timeout: 5000 });
+});
+test('Verify that user can expand/collapse for sorted set data type', async t => {
+    const memberValueCell = browserPage.zsetMembersList.parent(1);
+    // Open Sorted Set key details
+    await browserPage.openKeyDetails(sortedSetKeyName);
+    // Remember height of the cell with long value
+    const startLongCellHeight = await memberValueCell.clientHeight;
+    // Verify that user can expand a row of sorted set data type
+    await t.click(memberValueCell);
+    await t.expect(memberValueCell.clientHeight).gt(startLongCellHeight + 150, 'Row is not expanded', { timeout: 5000 });
+    // Verify that user can collapse a row of sorted set data type
+    await t.click(memberValueCell);
+    await t.expect(memberValueCell.clientHeight).eql(startLongCellHeight, 'Row is not collapsed', { timeout: 5000 });
+});
+test('Verify that user can expand/collapse for list data type', async t => {
+    const elementValueCell = browserPage.listElementsList.parent(2);
+    // Open List key details
+    await browserPage.openKeyDetails(listKeyName);
+    // Remember height of the cell with long value
+    const startLongCellHeight = await elementValueCell.clientHeight;
+    // Verify that user can expand a row of list data type
+    await t.click(elementValueCell);
+    await t.expect(elementValueCell.clientHeight).gt(startLongCellHeight + 150, 'Row is not expanded', { timeout: 5000 });
+    // Verify that user can collapse a row of list data type
+    await t.click(elementValueCell);
+    await t.expect(elementValueCell.clientHeight).eql(startLongCellHeight, 'Row is not collapsed', { timeout: 5000 });
+});
+test('Verify that user can work in full mode with expanded/collapsed value', async t => {
+    const elementValueCell = browserPage.listElementsList.parent(2);
+    // Open List key details
+    await browserPage.openKeyDetails(listKeyName);
+    // Open full mode for key details
+    await t.click(browserPage.fullScreenModeButton);
+    // Remember height of the cell with long value
+    const startLongCellHeight = await elementValueCell.clientHeight;
+    // Verify that user can expand a row in full mode
+    await t.click(elementValueCell);
+    await t.expect(elementValueCell.clientHeight).gt(startLongCellHeight + 60, 'Row is not expanded', { timeout: 5000 });
+    // Verify that user can collapse a row in full mode
+    await t.click(elementValueCell);
+    await t.expect(elementValueCell.clientHeight).eql(startLongCellHeight, 'Row is not collapsed', { timeout: 5000 });
 });

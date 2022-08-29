7264acfdcc0617bf587c6c971599fb2af4153548
ALENA NABOKA <alena.naboka@softeq.com>
2022-08-29 14:47:52 +0300

    add Sentinel cloning

--- tests/e2e/pageObjects/add-redis-database-page.ts
+++ tests/e2e/pageObjects/add-redis-database-page.ts
@@ -23,6 +23,8 @@ export class AddRedisDatabasePage {
     connectToRedisStackButton = Selector('[aria-label="Connect to database"]');
     discoverSentinelDatabaseButton = Selector('[data-testid=btn-submit]');
     cloneDatabaseButton = Selector('[data-testid=clone-db-btn]');
+    sentinelNavigation = Selector('[data-testid=primary-group]');
+    sentinelDatabasesNavigation = Selector('[data-testid=primary-group]');
     //TEXT INPUTS (also referred to as 'Text fields')
     hostInput = Selector('[data-testid=host]');
     portInput = Selector('[data-testid=port]');
@@ -35,6 +37,8 @@ export class AddRedisDatabasePage {
     databaseIndexInput = Selector('[data-testid=db]');
     errorMessage = Selector('[data-test-subj=toast-error]');
     databaseIndexMessage = Selector('[data-testid=db-index-message]');
+    primaryGroupNameInput = Selector('[data-testid=primary-group]');
+    masterGroupPassword = Selector('data-testid=sentinel-master-password');
     //Links
     buildFromSource = Selector('a').withExactText('Build from source');
     buildFromDocker = Selector('a').withExactText('Docker');
--- tests/e2e/tests/critical-path/database/clone-databases.e2e.ts
+++ tests/e2e/tests/critical-path/database/clone-databases.e2e.ts
@@ -1,12 +1,14 @@
 import { rte } from '../../../helpers/constants';
 import { AddRedisDatabasePage, MyRedisDatabasePage } from '../../../pageObjects';
-import {commonUrl, ossClusterConfig, ossStandaloneConfig} from '../../../helpers/conf';
+import { commonUrl, ossClusterConfig, ossSentinelConfig, ossStandaloneConfig } from '../../../helpers/conf';
 import { acceptLicenseTerms } from '../../../helpers/database';
 import {
     addNewOSSClusterDatabaseApi,
     addNewStandaloneDatabaseApi,
+    deleteAllSentinelDatabasesApi,
     deleteOSSClusterDatabaseApi,
-    deleteStandaloneDatabaseApi
+    deleteStandaloneDatabaseApi,
+    discoverSentinelDatabaseApi
 } from '../../../helpers/api/api-database';
 
 const addRedisDatabasePage = new AddRedisDatabasePage();
@@ -30,7 +32,7 @@ test
         // Delete database
         await deleteStandaloneDatabaseApi(ossStandaloneConfig);
     })
-    .meta({ rte: rte.standalone })('Verify that user can close Standalone db', async t => {
+    .meta({ rte: rte.standalone })('Verify that user can clone Standalone db', async t => {
         await myRedisDatabasePage.clickOnEditDBByName(ossStandaloneConfig.databaseName);
         await t.click(addRedisDatabasePage.cloneDatabaseButton);
         await t
@@ -51,7 +53,7 @@ test
         // Delete database
         await deleteOSSClusterDatabaseApi(ossClusterConfig);
     })
-    .meta({ rte: rte.standalone })('Verify that user can close OSS Cluster', async t => {
+    .meta({ rte: rte.standalone })('Verify that user can clone OSS Cluster', async t => {
         const newDatabaseAlias = 'cloned oss cluster';
         await myRedisDatabasePage.clickOnEditDBByName(ossClusterConfig.ossClusterDatabaseName);
         await t.click(addRedisDatabasePage.cloneDatabaseButton);
@@ -64,4 +66,33 @@ test
         await t.click(addRedisDatabasePage.addRedisDatabaseButton);
         await t.expect(myRedisDatabasePage.dbNameList.withExactText(newDatabaseAlias).visible).ok('DB was not closed');
         await t.expect(myRedisDatabasePage.dbNameList.withExactText(ossClusterConfig.ossClusterDatabaseName).visible).ok('Original DB is not displayed');
+    });
+test
+    .before(async t => {
+        await acceptLicenseTerms();
+        // Add Sentinel databases
+        await discoverSentinelDatabaseApi(ossSentinelConfig);
+        await t.eval(() => location.reload());
+    })
+    .after(async() => {
+        // Delete all primary groups
+        await deleteAllSentinelDatabasesApi(ossSentinelConfig);
+    })
+    .meta({ rte: rte.standalone })('Verify that user can clone Sentinel', async t => {
+        await myRedisDatabasePage.clickOnEditDBByName(ossSentinelConfig.name[0]);
+        await t.click(addRedisDatabasePage.cloneDatabaseButton);
+        // Validate common form
+        await t
+            .expect(myRedisDatabasePage.editAliasButton.withText('Clone ').visible).ok('Clone panel is not displayed')
+            .expect(addRedisDatabasePage.databaseAliasInput.getAttribute('value')).eql(ossSentinelConfig.name[0], 'Invalid primary group alias value')
+            .expect(addRedisDatabasePage.primaryGroupNameInput.getAttribute('value')).eql(ossSentinelConfig.name[0], 'Invalid primary group name value');
+        // Validate Sentinel section
+        await t
+            .click(addRedisDatabasePage.sentinelNavigation)
+            .expect(addRedisDatabasePage.portInput.getAttribute('value')).eql(ossSentinelConfig.sentinelPort, 'Invalid sentinel port')
+            .expect(addRedisDatabasePage.passwordInput.getAttribute('value')).eql(ossSentinelConfig.sentinelPort, 'Invalid sentinel port');
+        // Validate Databases section
+        await t
+            .click(addRedisDatabasePage.sentinelDatabasesNavigation)
+            .expect(addRedisDatabasePage.masterGroupPassword.getAttribute('value')).eql(ossSentinelConfig.masters[0].password, 'Invalid sentinel port');
     });

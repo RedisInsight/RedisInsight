dc4f8ff993bf6642ec5b77a8841aa46c42444520
vlad-dargel <u.darhel@aisnovations.com>
2022-08-30 11:29:21 +0200

    add binary tests

--- tests/e2e/tests/critical-path/browser/formatters.e2e.ts
+++ tests/e2e/tests/critical-path/browser/formatters.e2e.ts
@@ -6,6 +6,7 @@ import { BrowserPage } from '../../../pageObjects';
 import { commonUrl, ossStandaloneConfig } from '../../../helpers/conf';
 import { deleteStandaloneDatabaseApi } from '../../../helpers/api/api-database';
 import { Common } from '../../../helpers/common';
+const dataSet = require('../../../test-data/formattersData');
 
 const browserPage = new BrowserPage();
 const common = new Common();
@@ -20,19 +21,11 @@ const formatters = [{
     format: 'Msgpack',
     fromText: '{ "field": "value" }',
     fromTextEdit: '{ "field": "value123" }',
-    toText: 'DF00000001A56669656C64A576616C7565'
+    formattedText: 'DF00000001A56669656C64A576616C7565'
 }, {
-    format: 'ASCII',
-    fromText: '山女子水 рус ascii',
-    fromTextEdit: '山女子水 рус ascii 山女子',
-    toText: '\\xe5\\xb1\\xb1\\xe5\\xa5\\xb3\\xe5\\xad\\x90\\xe6\\xb0\\xb4 \\xd1\\x80\\xd1\\x83\\xd1\\x81 ascii',
-    toTextEdit: '\\xe5\\xb1\\xb1\\xe5\\xa5\\xb3\\xe5\\xad\\x90\\xe6\\xb0\\xb4 \\xd1\\x80\\xd1\\x83\\xd1\\x81 ascii \\xe5\\xb1\\xb1\\xe5\\xa5\\xb3\\xe5\\xad\\x90'
-}, {
-    format: 'HEX',
-    fromText: '山女子水 рус ascii',
-    fromTextEdit: '山女子水 рус ascii 山女子',
-    toText: 'e5b1b1e5a5b3e5ad90e6b0b420d180d183d181206173636969',
-    toTextEdit: 'e5b1b1e5a5b3e5ad90e6b0b420d180d183d18120617363696920e5b1b1e5a5b3e5ad90'
+    format: 'Protobuf',
+    fromText: '[ { "1": 2009 }, { "2": 344 } ]',
+    formattedText: '08d90f10d802'
 }];
 
 fixture `Formatters`
@@ -41,18 +34,18 @@ fixture `Formatters`
         rte: rte.standalone
     })
     .page(commonUrl)
-    .beforeEach(async() => {
+    .beforeEach(async () => {
         await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneConfig, ossStandaloneConfig.databaseName);
         // Create new keys
         await addKeysViaCli(keysData);
     })
-    .afterEach(async() => {
+    .afterEach(async () => {
         // Clear keys and database
         await deleteKeysViaCli(keysData);
         await deleteStandaloneDatabaseApi(ossStandaloneConfig);
     });
 test
-    .before(async() => {
+    .before(async () => {
         await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneConfig, ossStandaloneConfig.databaseName);
         // Create new keys
         await addKeysViaCli(keysData, formatters[0].fromText, formatters[0].fromText);
@@ -80,108 +73,120 @@ test('Verify that user can edit the values in the key regardless if they are val
     // Verify for JSON and Msgpack formatters
     const invalidText = 'invalid text';
     for (const formatter of formatters) {
-        if (formatter.format === 'JSON' || formatter.format === 'Msgpack') {
-            // Open key details and select formatter
-            await browserPage.openKeyDetails(keysData[0].keyName);
-            await browserPage.selectFormatter(formatter.format);
-            await browserPage.editHashKeyValue(invalidText);
-            // Verify that invalid value can be saved
-            await t.expect(browserPage.hashFieldValue.textContent).contains(invalidText, `Invalid ${formatter.format} value is not saved`);
-            await browserPage.editHashKeyValue(formatter.fromText);
-            // Verify that valid value can be saved on edit
-            await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.fromText, `Valid ${formatter.format} value is not saved`);
-            await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).ok(`Value is not formatted to ${formatter.format}`);
-            await browserPage.editHashKeyValue(formatter.fromTextEdit!);
-            // Verify that valid value can be edited to another valid value
-            await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.fromTextEdit!, `Valid ${formatter.format} value is not saved`);
-            await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).ok(`Value is not formatted to ${formatter.format}`);
-        }
+        // Open key details and select formatter
+        await browserPage.openKeyDetails(keysData[0].keyName);
+        await browserPage.selectFormatter(formatter.format);
+        await browserPage.editHashKeyValue(invalidText);
+        // Verify that invalid value can be saved
+        await t.expect(browserPage.hashFieldValue.textContent).contains(invalidText, `Invalid ${formatter.format} value is not saved`);
+        await browserPage.editHashKeyValue(formatter.fromText);
+        // Verify that valid value can be saved on edit
+        await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.fromText, `Valid ${formatter.format} value is not saved`);
+        await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).ok(`Value is not formatted to ${formatter.format}`);
+        await browserPage.editHashKeyValue(formatter.fromTextEdit!);
+        // Verify that valid value can be edited to another valid value
+        await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.fromTextEdit!, `Valid ${formatter.format} value is not saved`);
+        await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).ok(`Value is not formatted to ${formatter.format}`);
     }
 });
-test('Verify that user can see tooltip with convertion failed message on hover when data is not valid JSON/Msgpack', async t => {
-    // Verify for JSON and Msgpack formatters
+test('Verify that user can see tooltip with convertion failed message on hover when data is not valid JSON/Msgpack/Protobuf', async t => {
+    // Verify for JSON, Msgpack, Protobuf formatters
     for (const formatter of formatters) {
-        if (formatter.format === 'JSON' || formatter.format === 'Msgpack') {
-            const failedMessage = `Failed to convert to ${formatter.format}`;
-            for (let i = 0; i < keysData.length; i++) {
-                const valueSelector = Selector(`[data-testid^=${keysData[i].keyName.split('-')[0]}-][data-testid*=${keysData[i].data}]`);
-                // Open key details and select formatter
-                await browserPage.openKeyDetailsByKeyName(keysData[i].keyName);
-                await browserPage.selectFormatter(formatter.format);
-                // Verify that not valid value is not formatted
-                await t.expect(valueSelector.find(browserPage.cssJsonValue).exists).notOk(`${keysData[i].textType} Value is formatted to ${formatter.format}`);
-                await t.hover(valueSelector, { offsetX: 5 });
-                // Verify that tooltip with convertion failed message displayed
-                await t.expect(browserPage.tooltip.textContent).contains(failedMessage, `"${failedMessage}" is not displayed in tooltip`);
-            }
+        const failedMessage = `Failed to convert to ${formatter.format}`;
+        for (let i = 0; i < keysData.length; i++) {
+            const valueSelector = Selector(`[data-testid^=${keysData[i].keyName.split('-')[0]}-][data-testid*=${keysData[i].data}]`);
+            // Open key details and select formatter
+            await browserPage.openKeyDetailsByKeyName(keysData[i].keyName);
+            await browserPage.selectFormatter(formatter.format);
+            // Verify that not valid value is not formatted
+            await t.expect(valueSelector.find(browserPage.cssJsonValue).exists).notOk(`${keysData[i].textType} Value is formatted to ${formatter.format}`);
+            await t.hover(valueSelector, { offsetX: 5 });
+            // Verify that tooltip with convertion failed message displayed
+            await t.expect(browserPage.tooltip.textContent).contains(failedMessage, `"${failedMessage}" is not displayed in tooltip`);
         }
     }
 });
 test
-    .before(async() => {
+    .before(async () => {
         await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneConfig, ossStandaloneConfig.databaseName);
         // Create new keys
         await addKeysViaCli(keysData, formatters[1].fromText);
     })('Verify that user can see highlighted key details in Msgpack format', async t => {
-        // Open HaSH key details
+        // Open Hash key details
         await browserPage.openKeyDetailsByKeyName(keysData[0].keyName);
         // Verify that msgpack value not formatted with default formatter
         await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).notOk('Value is formatted to Msgpack');
         // Add valid msgpack in HEX format
         await browserPage.selectFormatter('HEX');
-        await browserPage.editHashKeyValue(formatters[1].toText!);
+        await browserPage.editHashKeyValue(formatters[1].formattedText!);
         await browserPage.selectFormatter('Msgpack');
         // Verify that msgpack value is formatted and highlighted
         await t.expect(browserPage.hashFieldValue.innerText).contains(formatters[1].fromText!, 'Value is not saved as msgpack');
         await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).ok('Value is not formatted to Msgpack');
     });
-test
-    .before(async() => {
-        await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneConfig, ossStandaloneConfig.databaseName);
-        // Create new keys
-        await addKeysViaCli(keysData, formatters[2].fromText);
-    })('Verify that user can see key details converted to ASCII/HEX format', async t => {
-        // Verify for ASCII and HEX formatters
-        for (const formatter of formatters) {
-            if (formatter.format === 'ASCII' || formatter.format === 'HEX') {
-                // Verify for Hash, List, Set, ZSet, String, Stream keys
-                for (let i = 0; i < keysData.length; i++) {
-                    const valueSelector = Selector(`[data-testid^=${keysData[i].keyName.split('-')[0]}-][data-testid*=${keysData[i].data}]`);
-                    await browserPage.openKeyDetailsByKeyName(keysData[i].keyName);
-                    // Verify that value not formatted with default formatter
-                    await t.expect(valueSelector.innerText).contains(formatter.fromText!, `Value is formatted as ${formatter.format} in Unicode`);
-                    await browserPage.selectFormatter(formatter.format);
-                    // Verify that value is formatted
-                    await t.expect(valueSelector.innerText).contains(formatter.toText!, `Value is formatted to ${formatter.format}`);
-                    // Verify that Hash field is formatted to ASCII/HEX
-                    if (keysData[i].keyName === 'hash') {
-                        await t.expect(browserPage.hashField.innerText).contains(formatter.toText!, `Hash field is not formatted to ${formatter.format}`);
-                    }
+dataSet.forEach(data => {
+    test
+        .before(async () => {
+            await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneConfig, ossStandaloneConfig.databaseName);
+            // Create new keys
+            await addKeysViaCli(keysData, data.fromText);
+        })(`Verify that user can see key details converted to '${data.format}' format`, async t => {
+            // Verify for ASCII, HEX, Binary formatters
+            // Verify for Hash, List, Set, ZSet, String, Stream keys
+            for (let i = 0; i < keysData.length; i++) {
+                const valueSelector = Selector(`[data-testid^=${keysData[i].keyName.split('-')[0]}-][data-testid*=${keysData[i].data}]`);
+                await browserPage.openKeyDetailsByKeyName(keysData[i].keyName);
+                // Verify that value not formatted with default formatter
+                await t.expect(valueSelector.innerText).contains(data.fromText, `Value is formatted as ${data.format} in Unicode`);
+                await browserPage.selectFormatter(data.format);
+                // Verify that value is formatted
+                await t.expect(valueSelector.innerText).contains(data.formattedText!, `Value is not formatted to ${data.format}`);
+                // Verify that Hash field is formatted to ASCII/HEX/Binary
+                if (keysData[i].keyName === 'hash') {
+                    await t.expect(browserPage.hashField.innerText).contains(data.formattedText!, `Hash field is not formatted to ${data.format}`);
                 }
             }
-        }
+        });
+    test(`Verify that user can edit value for Hash field in '${data.format}' and convert then to another format`, async t => {
+        // Verify for ASCII, HEX, Binary formatters
+        // Open key details and select formatter
+        await browserPage.openKeyDetails(keysData[0].keyName);
+        await browserPage.selectFormatter(data.format);
+        // Add value in selected format
+        await browserPage.editHashKeyValue(data.formattedText!);
+        // Verify that value saved in selected format
+        await t.expect(browserPage.hashFieldValue.innerText).contains(data.formattedText!, `${data.format} value is not saved`);
+        await browserPage.selectFormatter('Unicode');
+        // Verify that value converted to Unicode
+        await t.expect(browserPage.hashFieldValue.innerText).contains(data.fromText!, `${data.format} value is not converted to Unicode`);
+        await browserPage.selectFormatter(data.format);
+        await browserPage.editHashKeyValue(data.formattedTextEdit!);
+        // Verify that valid converted value can be edited to another
+        await t.expect(browserPage.hashFieldValue.innerText).contains(data.formattedTextEdit!, `${data.format} value is not saved`);
+        await browserPage.selectFormatter('Unicode');
+        // Verify that value converted to Unicode
+        await t.expect(browserPage.hashFieldValue.innerText).contains(data.fromTextEdit!, `${data.format} value is not converted to Unicode`);
     });
-test('Verify that user can edit value for Hash field in ASCII/HEX and convert then to another format', async t => {
-    // Verify for ASCII and HEX formaterrs
-    for (const formatter of formatters) {
-        if (formatter.format === 'ASCII' || formatter.format === 'HEX') {
-            // Open key details and select formatter
-            await browserPage.openKeyDetails(keysData[0].keyName);
-            await browserPage.selectFormatter(formatter.format);
-            // Add value in selected format
-            await browserPage.editHashKeyValue(formatter.toText!);
-            // Verify that value saved in selected format
-            await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.toText!, `${formatter.format} value is not saved`);
-            await browserPage.selectFormatter('Unicode');
-            // Verify that value converted to Unicode
-            await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.fromText!, `${formatter.format} value is not converted to Unicode`);
-            await browserPage.selectFormatter(formatter.format);
-            await browserPage.editHashKeyValue(formatter.toTextEdit!);
-            // Verify that valid converted value can be edited to another
-            await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.toTextEdit!, `${formatter.format} value is not saved`);
-            await browserPage.selectFormatter('Unicode');
-            // Verify that value converted to Unicode
-            await t.expect(browserPage.hashFieldValue.innerText).contains(formatter.fromTextEdit!, `${formatter.format} value is not converted to Unicode`);
-        }
-    }
+});
+test('Verify that user can see highlighted key details in Protobuf format', async t => {
+    // Open Hash key details
+    await browserPage.openKeyDetailsByKeyName(keysData[0].keyName);
+    // Add valid Protobuf in HEX format
+    await browserPage.selectFormatter('HEX');
+    await browserPage.editHashKeyValue(formatters[2].formattedText!);
+    await browserPage.selectFormatter('Protobuf');
+    // Verify that Protobuf value is formatted and highlighted
+    await t.expect(browserPage.hashFieldValue.innerText).contains(formatters[2].fromText!, 'Value is not saved as Protobuf');
+    await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).ok('Value is not formatted to Protobuf');
+});
+test('Verify that user can see highlighted key details in PHP unserialise format', async t => {
+    // Open Hash key details
+    await browserPage.openKeyDetailsByKeyName(keysData[0].keyName);
+    // Add valid Protobuf in HEX format
+    await browserPage.selectFormatter('HEX');
+    await browserPage.editHashKeyValue(formatters[2].formattedText!);
+    await browserPage.selectFormatter('Protobuf');
+    // Verify that Protobuf value is formatted and highlighted
+    await t.expect(browserPage.hashFieldValue.innerText).contains(formatters[2].fromText!, 'Value is not saved as Protobuf');
+    await t.expect(browserPage.hashFieldValue.find(browserPage.cssJsonValue).exists).ok('Value is not formatted to Protobuf');
 });

de2b7db8e2f03bd580b55dda6582eafd6176468d
Roman.Sergeenko <roman.sergeenko@softeq.com>
2022-10-19 17:09:08 +0400

    #RI-3679 - fix negative pipeline param

--- redisinsight/ui/src/utils/tests/workbench.spec.ts
+++ redisinsight/ui/src/utils/tests/workbench.spec.ts
@@ -15,17 +15,20 @@ describe('getExecuteParams', () => {
     const btnParams3: CodeButtonParams = { pipeline: 'abc' }
     const btnParams4: CodeButtonParams = { results: 'single', mode: 'raw' }
     const btnParams5: CodeButtonParams = { results: 'single', mode: 'raw', pipeline: '4' }
+    const btnParams6: CodeButtonParams = { results: 'single', mode: 'raw', pipeline: '-4' }
 
     const expect1 = { activeRunQueryMode: RunQueryMode.Raw, resultsMode: ResultsMode.GroupMode, batchSize: 5 }
     const expect2 = { activeRunQueryMode: RunQueryMode.ASCII, resultsMode: ResultsMode.GroupMode, batchSize: 10 }
     const expect3 = { activeRunQueryMode: RunQueryMode.ASCII, resultsMode: ResultsMode.GroupMode, batchSize: 10 }
     const expect4 = { activeRunQueryMode: RunQueryMode.Raw, resultsMode: ResultsMode.Default, batchSize: 10 }
     const expect5 = { activeRunQueryMode: RunQueryMode.Raw, resultsMode: ResultsMode.Default, batchSize: 4 }
+    const expect6 = { activeRunQueryMode: RunQueryMode.Raw, resultsMode: ResultsMode.Default, batchSize: 10 }
 
     expect(getExecuteParams(btnParams1, paramsState)).toEqual(expect1)
     expect(getExecuteParams(btnParams2, paramsState)).toEqual(expect2)
     expect(getExecuteParams(btnParams3, paramsState)).toEqual(expect3)
     expect(getExecuteParams(btnParams4, paramsState)).toEqual(expect4)
     expect(getExecuteParams(btnParams5, paramsState)).toEqual(expect5)
+    expect(getExecuteParams(btnParams6, paramsState)).toEqual(expect6)
   })
 })

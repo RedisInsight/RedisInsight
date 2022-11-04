06a91c7a0737beb1575e055978634381652e807a
vlad-dargel <u.darhel@aisnovations.com>
2022-11-04 16:42:14 +0100

    fixes after bugfix

--- tests/e2e/tests/critical-path/browser/filtering.e2e.ts
+++ tests/e2e/tests/critical-path/browser/filtering.e2e.ts
@@ -83,7 +83,7 @@ test
             await browserPage.selectFilterGroupType(textType);
             // Check key type label
             await t.expect((await browserPage.filteringLabel.textContent).toUpperCase).eql(textType.toUpperCase, `The label of type ${textType} is displayed`);
-            if (['STREAM', 'GRAPH', 'TS'].includes(textType)) {
+            if (['Stream', 'Graph', 'TS'].includes(textType)) {
                 await t.expect(browserPage.keysNumberOfResults.textContent).eql('0', 'Number of found keys');
             }
             else {

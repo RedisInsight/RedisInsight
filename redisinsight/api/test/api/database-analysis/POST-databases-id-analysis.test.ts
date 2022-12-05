06eb860b5b62062858753a314df41966c4824a14
Amir Allayarov <amir.allayarov@softeq.com>
2022-12-05 16:50:21 +0400

    #RI-3569 - fix cloud IT

--- redisinsight/api/test/api/database-analysis/POST-databases-id-analysis.test.ts
+++ redisinsight/api/test/api/database-analysis/POST-databases-id-analysis.test.ts
@@ -256,27 +256,6 @@ describe('POST /databases/:instanceId/analysis', () => {
         }
       },
       {
-        name: 'Should create new database analysis with useSmallerKeys recommendation',
-        data: {
-          delimiter: '-',
-        },
-        statusCode: 201,
-        responseSchema,
-        before: async () => {
-          const KEYS_NUMBER = 1_000_006;
-          await rte.data.generateNKeys(KEYS_NUMBER, false);
-        },
-        checkFn: async ({ body }) => {
-          expect(body.recommendations).to.deep.eq([
-            constants.TEST_SMALLER_KEYS_DATABASE_ANALYSIS_RECOMMENDATION,
-            constants.TEST_COMBINE_SMALL_STRING_TO_HASHES_RECOMMENDATION,
-          ]);
-        },
-        after: async () => {
-          expect(await repository.count()).to.eq(5);
-        }
-      },
-      {
         name: 'Should create new database analysis with compressionForList recommendation',
         data: {
           delimiter: '-',
@@ -338,11 +317,7 @@ describe('POST /databases/:instanceId/analysis', () => {
 
     describe('useSmallerKeys recommendation', () => {
       // generate 1M keys take a lot of time
-      requirements('!rte.cloud');
-  
-      beforeEach(async () => {
-        await rte.data.truncate();
-      });
+      requirements('!rte.type=CLUSTER');
   
       [
         {
@@ -353,6 +328,7 @@ describe('POST /databases/:instanceId/analysis', () => {
           statusCode: 201,
           responseSchema,
           before: async () => {
+            await rte.data.truncate();
             const KEYS_NUMBER = 1_000_006;
             await rte.data.generateNKeys(KEYS_NUMBER, false);
           },

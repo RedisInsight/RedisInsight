990cb90c827c537fad0a51aaa9501a68311008ec
Roman.Sergeenko <roman.sergeenko@softeq.com>
2022-12-07 17:36:12 +0400

    #RI-3854 - remove page from build_features config

--- redisinsight/ui/src/constants/featuresHighlighting.tsx
+++ redisinsight/ui/src/constants/featuresHighlighting.tsx
@@ -12,7 +12,6 @@ export const BUILD_FEATURES: { [key: string]: BuildHighlightingFeature } = {
   importDatabases: {
     type: 'tooltip',
     title: 'Import Database Connections',
-    content: 'Import your database connections from other Redis UIs',
-    page: 'settings'
+    content: 'Import your database connections from other Redis UIs'
   }
 }

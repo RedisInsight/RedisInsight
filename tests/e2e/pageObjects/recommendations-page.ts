0aabc97f885a69acd6bf46bb09bcd97aa0af9cd4
nmammadli <nihad.mammadli@softeq.com>
2023-01-23 12:02:19 +0100

    Update recommendations-page.ts
    Delete unused nth from verifyVoteDisabled

--- tests/e2e/pageObjects/recommendations-page.ts
+++ tests/e2e/pageObjects/recommendations-page.ts
@@ -1,7 +1,6 @@
 import {Selector, t} from 'testcafe';
 
 export class RecommendationsPage {
-    //CSS Selectors
     veryUsefulVoteBtn = Selector('[data-testid=very-useful-vote-btn]').nth(0);
     usefulVoteBtn = Selector('[data-testid=useful-vote-btn]').nth(0);
     notUsefulVoteBtn = Selector('[data-testid=not-useful-vote-btn]').nth(0);
@@ -24,11 +23,11 @@ export class RecommendationsPage {
 
     async verifyVoteDisabled(): Promise<void>{
         // Verify that user can rate recommendations with one of 3 existing types at the same time
-        await t.expect(this.veryUsefulVoteBtn.nth(0)
+        await t.expect(this.veryUsefulVoteBtn
             .hasAttribute('disabled')).ok();
-        await t.expect(this.usefulVoteBtn.nth(0)
+        await t.expect(this.usefulVoteBtn
             .hasAttribute('disabled')).ok();
-        await t.expect(this.notUsefulVoteBtn.nth(0)
+        await t.expect(this.notUsefulVoteBtn
             .hasAttribute('disabled')).ok();
     }
 }

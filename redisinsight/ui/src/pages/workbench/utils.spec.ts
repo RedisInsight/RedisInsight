9d2e65c1754f3bd7ac810fa1028ebf5f3350c9d2
Gnanesh <gnaneshkunal@outlook.com>
2023-01-23 21:41:57 +0530

    - Add null checks for generate profile command functions - https://github.com/RedisInsight/RedisInsight/pull/1537#discussion_r1084103676

--- redisinsight/ui/src/pages/workbench/utils.spec.ts
+++ redisinsight/ui/src/pages/workbench/utils.spec.ts
@@ -12,14 +12,14 @@ const generateGraphProfileQueryTests: Record<string, any>[] = [
   { input: 'GRAPH.QUERY key "MATCH (n) RETURN n"', output: 'graph.explain key "MATCH (n) RETURN n"', type: ProfileQueryType.Explain },
   { input: 'graph.query key "MATCH (n) RETURN n"', output: 'graph.profile key "MATCH (n) RETURN n"', type: ProfileQueryType.Profile },
   { input: 'graph.query key "MATCH (n) RETURN n"', output: 'graph.explain key "MATCH (n) RETURN n"', type: ProfileQueryType.Explain },
+  { input: null, output: null, type: ProfileQueryType.Profile },
+  { input: null, output: null, type: ProfileQueryType.Explain },
 ]
 
 describe('generateGraphProfileQuery', () => {
-
   generateGraphProfileQueryTests.forEach(test => {
     it(`should be output: ${test.output} for input: ${test.input} and type: ${test.type}`, () => {
       const result = generateGraphProfileQuery(test.input, test.type);
-
       expect(result).toEqual(test.output);
     });
   })
@@ -29,24 +29,24 @@ describe('generateGraphProfileQuery', () => {
 const generateSearchProfileQueryTests: Record<string, any>[] = [
   { input: 'FT.SEARCH index tomatoes', output: 'ft.profile index SEARCH QUERY tomatoes', type: ProfileQueryType.Profile },
   { input: 'FT.AGGREGATE index tomatoes', output: 'ft.profile index AGGREGATE QUERY tomatoes', type: ProfileQueryType.Profile },
-{ input: 'FT.SEARCH index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
+  { input: 'FT.SEARCH index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
   { input: 'FT.AGGREGATE index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
-{ input: 'ft.search index tomatoes', output: 'ft.profile index search QUERY tomatoes', type: ProfileQueryType.Profile },
+  { input: 'ft.search index tomatoes', output: 'ft.profile index search QUERY tomatoes', type: ProfileQueryType.Profile },
   { input: 'ft.aggregate index tomatoes', output: 'ft.profile index aggregate QUERY tomatoes', type: ProfileQueryType.Profile },
-{ input: 'ft.search index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
+  { input: 'ft.search index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
   { input: 'ft.aggregate index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
-{ input: 'ft.SEARCH index tomatoes', output: 'ft.profile index SEARCH QUERY tomatoes', type: ProfileQueryType.Profile },
+  { input: 'ft.SEARCH index tomatoes', output: 'ft.profile index SEARCH QUERY tomatoes', type: ProfileQueryType.Profile },
   { input: 'ft.AGGREGATE index tomatoes', output: 'ft.profile index AGGREGATE QUERY tomatoes', type: ProfileQueryType.Profile },
-{ input: 'ft.SEARCH index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
+  { input: 'ft.SEARCH index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
   { input: 'ft.AGGREGATE index tomatoes', output: 'ft.explain index tomatoes', type: ProfileQueryType.Explain },
+  { input: null, output: null, type: ProfileQueryType.Profile },
+  { input: null, output: null, type: ProfileQueryType.Explain },
 ]
 
 describe('generateSearchProfileQuery', () => {
-
   generateSearchProfileQueryTests.forEach(test => {
     it(`should be output: ${test.output} for input: ${test.input} and type: ${test.type}`, () => {
       const result = generateSearchProfileQuery(test.input, test.type);
-
       expect(result).toEqual(test.output);
     });
   })
@@ -63,9 +63,9 @@ const generateProfileQueryForCommandTests: Record<string, any>[] = [
   { input: 'GRAPH.EXPLAIN key "MATCH (n) RETURN n"', output: null, type: ProfileQueryType.Explain },
   { input: 'ft._LIST', output: null, type: ProfileQueryType.Profile },
   { input: 'ft._LIST', output: null, type: ProfileQueryType.Explain },
-{ input: 'ft.profile index SEARCH QUERY tomatoes', output: null, type: ProfileQueryType.Profile },
+  { input: 'ft.profile index SEARCH QUERY tomatoes', output: null, type: ProfileQueryType.Profile },
   { input: 'ft.profile index AGGREGATE QUERY tomatoes', output: null, type: ProfileQueryType.Explain },
-{ input: 'ft.explain index tomatoes', output: null, type: ProfileQueryType.Profile },
+  { input: 'ft.explain index tomatoes', output: null, type: ProfileQueryType.Profile },
   { input: 'ft.explain index tomatoes', output: null, type: ProfileQueryType.Explain },
 ]
 describe('generateProfileQueryForCommand', () => {

d69330f651dfa9114a6820f25a79cb11e8285563
Roman Sergeenko <roman.sergeenko@softeq.com>
2023-01-30 21:03:50 +0300

    #RI-4069 - update icons

--- redisinsight/ui/src/packages/clients-list/src/icons/arrow_down.js
+++ redisinsight/ui/src/packages/clients-list/src/icons/arrow_down.js
@@ -11,18 +11,26 @@ var EuiIconArrowDown = function EuiIconArrowDown(_ref) {
       titleId = _ref.titleId,
       props = _objectWithoutProperties(_ref, ["title", "titleId"]);
 
-  return /*#__PURE__*/React.createElement("svg", _extends({
-    width: 16,
-    height: 16,
-    viewBox: "0 0 16 16",
-    xmlns: "http://www.w3.org/2000/svg",
-    "aria-labelledby": titleId
-  }, props), title ? /*#__PURE__*/React.createElement("title", {
-    id: titleId
-  }, title) : null, /*#__PURE__*/React.createElement("path", {
-    fillRule: "non-zero",
-    d: "M13.069 5.157L8.384 9.768a.546.546 0 01-.768 0L2.93 5.158a.552.552 0 00-.771 0 .53.53 0 000 .759l4.684 4.61c.641.631 1.672.63 2.312 0l4.684-4.61a.53.53 0 000-.76.552.552 0 00-.771 0z"
-  }));
+  // For e2e tests. Hammerhead cannot create svg throw createElementNS
+  try {
+    document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+    return /*#__PURE__*/React.createElement("svg", _extends({
+      width: 16,
+      height: 16,
+      viewBox: "0 0 16 16",
+      xmlns: "http://www.w3.org/2000/svg",
+      "aria-labelledby": titleId
+    }, props), title ? /*#__PURE__*/React.createElement("title", {
+      id: titleId
+    }, title) : null, /*#__PURE__*/React.createElement("path", {
+      fillRule: "non-zero",
+      d: "M13.069 5.157L8.384 9.768a.546.546 0 01-.768 0L2.93 5.158a.552.552 0 00-.771 0 .53.53 0 000 .759l4.684 4.61c.641.631 1.672.63 2.312 0l4.684-4.61a.53.53 0 000-.76.552.552 0 00-.771 0z"
+    }));
+  } catch (e) {
+    return <span>&#8595;</span>
+  }
+
 };
 
 export var icon = EuiIconArrowDown;

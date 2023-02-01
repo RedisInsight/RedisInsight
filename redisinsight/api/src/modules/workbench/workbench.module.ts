97c7b6810733cc129f2d849bf08f4fcb07c286a1
Artem <artem.horuzhenko@softeq.com>
2023-02-01 12:23:44 +0200

    #Ri-3904 finalize WorkbenchModule rework after review

--- redisinsight/api/src/modules/workbench/workbench.module.ts
+++ redisinsight/api/src/modules/workbench/workbench.module.ts
@@ -1,10 +1,13 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  DynamicModule, MiddlewareConsumer, Module, NestModule, Type,
+} from '@nestjs/common';
 import { WorkbenchController } from 'src/modules/workbench/workbench.controller';
 import { RedisConnectionMiddleware } from 'src/middleware/redis-connection.middleware';
 import { RouterModule } from 'nest-router';
 import { WorkbenchService } from 'src/modules/workbench/workbench.service';
 import { WorkbenchCommandsExecutor } from 'src/modules/workbench/providers/workbench-commands.executor';
-import { CommandExecutionProvider } from 'src/modules/workbench/providers/command-execution.provider';
+import { CommandExecutionRepository } from 'src/modules/workbench/repositories/command-execution.repository';
+import { PluginStateRepository } from 'src/modules/workbench/repositories/plugin-state.repository';
 import { CommandsModule } from 'src/modules/commands/commands.module';
 import { CommandsService } from 'src/modules/commands/commands.service';
 import { CommandsJsonProvider } from 'src/modules/commands/commands-json.provider';
@@ -14,42 +17,58 @@ import { ClientContext } from 'src/common/models';
 import { PluginsService } from 'src/modules/workbench/plugins.service';
 import { PluginCommandsWhitelistProvider } from 'src/modules/workbench/providers/plugin-commands-whitelist.provider';
 import { PluginsController } from 'src/modules/workbench/plugins.controller';
-import { PluginStateProvider } from 'src/modules/workbench/providers/plugin-state.provider';
+import { LocalPluginStateRepository } from 'src/modules/workbench/repositories/local-plugin-state.repository';
+import { LocalCommandExecutionRepository } from 'src/modules/workbench/repositories/local-command-execution.repository';
 import config from 'src/utils/config';
 import { WorkbenchAnalyticsService } from './services/workbench-analytics/workbench-analytics.service';
 
 const COMMANDS_CONFIGS = config.get('commands');
 
-@Module({
-  imports: [
-    CommandsModule,
-  ],
-  controllers: [
-    WorkbenchController,
-    PluginsController,
-  ],
-  providers: [
-    WorkbenchService,
-    WorkbenchCommandsExecutor,
-    CommandExecutionProvider,
-    {
-      provide: RedisToolService,
-      useFactory: (redisToolFactory: RedisToolFactory) => redisToolFactory.createRedisTool(ClientContext.Workbench),
-      inject: [RedisToolFactory],
-    },
-    {
-      provide: CommandsService,
-      useFactory: () => new CommandsService(
-        COMMANDS_CONFIGS.map(({ name, url }) => new CommandsJsonProvider(name, url)),
-      ),
-    },
-    PluginsService,
-    PluginCommandsWhitelistProvider,
-    PluginStateProvider,
-    WorkbenchAnalyticsService,
-  ],
-})
+@Module({})
 export class WorkbenchModule implements NestModule {
+  static register(
+    commandExecutionRepository: Type<CommandExecutionRepository> = LocalCommandExecutionRepository,
+    pluginStateRepository: Type<PluginStateRepository> = LocalPluginStateRepository,
+  ): DynamicModule {
+    return {
+      module: WorkbenchModule,
+      imports: [
+        CommandsModule,
+      ],
+      controllers: [
+        WorkbenchController,
+        PluginsController,
+      ],
+      providers: [
+        WorkbenchService,
+        WorkbenchCommandsExecutor,
+        {
+          provide: CommandExecutionRepository,
+          useClass: commandExecutionRepository,
+        },
+        {
+          provide: PluginStateRepository,
+          useClass: pluginStateRepository,
+        },
+        {
+          provide: RedisToolService,
+          useFactory: (redisToolFactory: RedisToolFactory) => redisToolFactory.createRedisTool(ClientContext.Workbench),
+          inject: [RedisToolFactory],
+        },
+        {
+          provide: CommandsService,
+          useFactory: () => new CommandsService(
+            COMMANDS_CONFIGS.map(({ name, url }) => new CommandsJsonProvider(name, url)),
+          ),
+        },
+        PluginsService,
+        PluginCommandsWhitelistProvider,
+        WorkbenchAnalyticsService,
+      ],
+    };
+  }
+
+  // todo: check if still needed
   configure(consumer: MiddlewareConsumer): any {
     consumer
       .apply(RedisConnectionMiddleware)

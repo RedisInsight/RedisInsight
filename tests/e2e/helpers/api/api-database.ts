a090a64f8c935987bc911ca7e5caf359cdf8d6a8
vlad-dargel <u.darhel@aisnovations.com>
2023-02-06 12:38:58 +0100

    test

--- tests/e2e/helpers/api/api-database.ts
+++ tests/e2e/helpers/api/api-database.ts
@@ -33,10 +33,14 @@ export async function addNewStandaloneDatabaseApi(databaseParameters: AddNewData
             'key': databaseParameters.clientCert!.key
         };
     }
+    const getCerts = await request(endpoint).get('/certificates/ca')
+    .set('Accept', 'application/json');
 
+    await console.log(getCerts);
     const response = await request(endpoint).post('/databases')
         .send(requestBody)
         .set('Accept', 'application/json');
+    await console.log(getCerts);
     await t
         .expect(response.status).eql(201, `The creation of new standalone database request failed: ${await response.body.message}`)
         .expect(await response.body.name).eql(databaseParameters.databaseName, `Database Name is not equal to ${databaseParameters.databaseName} in response`);

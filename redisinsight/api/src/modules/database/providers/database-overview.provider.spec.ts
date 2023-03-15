b1133fa4f3da1acc7fd4392dfbc3a84c9cb42e51
Artem <artem.horuzhenko@softeq.com>
2023-03-15 11:17:47 +0200

    #RI-4262 fix total memory and total keys eq 0 for sentinel

--- redisinsight/api/src/modules/database/providers/database-overview.provider.spec.ts
+++ redisinsight/api/src/modules/database/providers/database-overview.provider.spec.ts
@@ -57,7 +57,7 @@ const mockNodeInfo = {
   keyspace: mockKeyspace,
 };
 
-const mockGetTotalResponse_1 = 1;
+const mockGetTotalResponse1 = 1;
 
 export const mockDatabaseOverview: DatabaseOverview = {
   version: mockServerInfo.redis_version,
@@ -125,6 +125,42 @@ describe('OverviewService', () => {
           ...mockDatabaseOverview,
           totalKeys: 0,
           totalKeysPerDb: undefined,
+        });
+      });
+      it('should return total 3 and empty total per db object (even when role is not master)', async () => {
+        spyGetNodeInfo.mockResolvedValueOnce({
+          ...mockNodeInfo,
+          replication: {
+            role: 'slave',
+          },
+          keyspace: {
+            db0: 'keys=3,expires=0,avg_ttl=0',
+          },
+        });
+
+        expect(await service.getOverview(mockClientMetadata, mockClient)).toEqual({
+          ...mockDatabaseOverview,
+          totalKeys: 3,
+          totalKeysPerDb: undefined,
+        });
+      });
+      it('should not return particular fields when metrics are not available', async () => {
+        spyGetNodeInfo.mockResolvedValueOnce({
+          ...mockNodeInfo,
+          replication: {
+            role: 'slave',
+          },
+          keyspace: undefined,
+          memory: undefined,
+          clients: undefined,
+        });
+
+        expect(await service.getOverview(mockClientMetadata, mockClient)).toEqual({
+          ...mockDatabaseOverview,
+          totalKeys: undefined,
+          usedMemory: undefined,
+          totalKeysPerDb: undefined,
+          connectedClients: undefined,
         });
       });
       it('check for cpu on second attempt', async () => {
@@ -197,7 +233,7 @@ describe('OverviewService', () => {
     });
     describe('Cluster', () => {
       it('Should calculate overview and ignore replica where needed', async () => {
-        const getTotal = jest.spyOn(Utils, 'getTotal').mockResolvedValue(mockGetTotalResponse_1);
+        const getTotal = jest.spyOn(Utils, 'getTotal').mockResolvedValue(mockGetTotalResponse1);
         mockCluster.nodes = jest.fn()
           .mockReturnValue(new Array(6).fill(Promise.resolve()));
 

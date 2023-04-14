467f02ffc496fdc49ad84bc6771156cf0fec341e
Amir Allayarov <amir.allayarov@softeq.com>
2023-04-14 14:01:16 +0700

    #RI-4307 - update welcome screen in insights panel

--- redisinsight/ui/src/components/live-time-recommendations/components/welcome-screen/WelcomeScreen.tsx
+++ redisinsight/ui/src/components/live-time-recommendations/components/welcome-screen/WelcomeScreen.tsx
@@ -1,30 +1,52 @@
 import React from 'react'
-import { EuiText, EuiIcon } from '@elastic/eui'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import cx from 'classnames'
+import { EuiText, EuiButton } from '@elastic/eui'
+
+import { Pages } from 'uiSrc/constants'
+import { setIsContentVisible } from 'uiSrc/slices/recommendations/recommendations'
+import { connectedInstanceSelector } from 'uiSrc/slices/instances/instances'
 import { ReactComponent as WelcomeIcon } from 'uiSrc/assets/img/icons/welcome.svg'
-import { ReactComponent as AnalysisIcon } from 'uiSrc/assets/img/icons/analysis.svg'
 
 import styles from './styles.module.scss'
 
-const NoRecommendationsScreen = () => (
-  <div className={styles.container} data-testid="no-recommendations-screen">
-    <EuiText className={styles.bigText}>Welcome to recommendations</EuiText>
-    <EuiText className={styles.mediumText}>Where we help you improve your database.</EuiText>
-    <WelcomeIcon className={styles.icon} />
-    <EuiText className={styles.text}>Eager to start? Go to</EuiText>
-    <EuiText className={styles.page}>
-      <EuiIcon
-        type={AnalysisIcon}
-        className={styles.smallIcon}
-        data-testid="analysis-icon"
-      />
-      database analysis page
-    </EuiText>
-    <EuiText className={styles.text}>
-      and click on -
-      <b> new report</b>
-    </EuiText>
-    <EuiText className={styles.text}>in order to see the magic happens :)</EuiText>
-  </div>
-)
+const NoRecommendationsScreen = () => {
+  const { id: instanceId } = useSelector(connectedInstanceSelector)
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const handleClickDbAnalysisLink = () => {
+    dispatch(setIsContentVisible(false))
+    history.push(Pages.databaseAnalysis(instanceId))
+  }
+
+  return (
+    <div className={styles.container} data-testid="no-recommendations-screen">
+      <EuiText className={styles.bigText}>Welcome to</EuiText>
+      <EuiText className={styles.hugeText}>Insights!</EuiText>
+      <EuiText className={styles.mediumText}>Where we will help you improve your database.</EuiText>
+      <EuiText className={cx(styles.text, styles.bigMargin)}>
+        Work in the database to see new recommendations appeared on how to improve performance, optimize memory usage,
+        and enhance the performance of your database.
+      </EuiText>
+      <WelcomeIcon className={styles.icon} />
+      <EuiText className={styles.text}>
+        Eager to see more recommendations right now? Go to Database Analysis
+        and click on the new report in order to see the magic happens.
+      </EuiText>
+      <EuiButton
+        fill
+        color="secondary"
+        size="s"
+        onClick={handleClickDbAnalysisLink}
+        data-testid="insights-db-analysis-link"
+      >
+        Go to Database Analysis
+      </EuiButton>
+    </div>
+  )
+}
 
 export default NoRecommendationsScreen

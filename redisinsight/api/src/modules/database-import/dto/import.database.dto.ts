4d3667f86603412d44d53c5eff2187f464a3fac7
Artem <artem.horuzhenko@softeq.com>
2023-04-24 17:45:12 +0300

    #RI-4445 handle provider field during db import

--- redisinsight/api/src/modules/database-import/dto/import.database.dto.ts
+++ redisinsight/api/src/modules/database-import/dto/import.database.dto.ts
@@ -14,7 +14,7 @@ import { UseClientCertificateDto } from 'src/modules/certificate/dto/use.client-
 export class ImportDatabaseDto extends PickType(Database, [
   'host', 'port', 'name', 'db', 'username', 'password',
   'connectionType', 'tls', 'verifyServerCert', 'sentinelMaster', 'nodes',
-  'new', 'ssh', 'sshOptions',
+  'new', 'ssh', 'sshOptions', 'provider',
 ] as const) {
   @Expose()
   @IsNotEmpty()

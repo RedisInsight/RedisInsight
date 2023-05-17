f2bc70c22d4f9e618cf73a3d139070950650e2f0
vlad-dargel <u.darhel@aisnovations.com>
2023-05-17 21:04:09 +0200

    fix

--- tests/e2e/helpers/api/api-info.ts
+++ tests/e2e/helpers/api/api-info.ts
@@ -1,8 +1,6 @@
 import { t } from 'testcafe';
 import * as request from 'supertest';
 import { Common } from '../common';
-import * as express from 'express';
-import * as fs from 'fs-extra';
 
 const endpoint = Common.getEndpoint();
 
@@ -15,15 +13,15 @@ export async function syncFeaturesApi(): Promise<void> {
     await t.expect(response.status).eql(200, `Synchronization request failed: ${await response.body.message}`);
 }
 
-/**
- * Initiate remote server to fetch various static data like notificaitons or features configs
- */
-export const initRemoteServer = async () => {
-    const path = '../../test-data/remote';
-    await fs.ensureDir(path);
+// /**
+//  * Initiate remote server to fetch various static data like notificaitons or features configs
+//  */
+// export const initRemoteServer = async () => {
+//     const path = '../../test-data/remote';
+//     await fs.ensureDir(path);
 
-    const app = express();
-    app.use('/remote', express.static(path));
-    // Start the server
-    await app.listen(3000);
-}
+//     const app = express();
+//     app.use('/remote', express.static(path));
+//     // Start the server
+//     await app.listen(3000);
+// }

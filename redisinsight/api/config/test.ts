6cc96f7284c525319f8e006aba51cf9e370084ae
Artem <artem.horuzhenko@softeq.com>
2023-05-22 17:55:21 +0300

    #RI-4489 fix ITests

--- redisinsight/api/config/test.ts
+++ redisinsight/api/config/test.ts
@@ -1,7 +1,7 @@
 export default {
   server: {
     env: 'test',
-    requestTimeout: 1000,
+    requestTimeout: parseInt(process.env.REQUEST_TIMEOUT, 10) || 1000,
   },
   db: {
     synchronize: process.env.DB_SYNC ? process.env.DB_SYNC === 'true' : true,
@@ -11,11 +11,11 @@ export default {
     logFileIdleThreshold: parseInt(process.env.PROFILER_LOG_FILE_IDLE_THRESHOLD, 10) || 1000 * 2, // 3sec
   },
   notifications: {
-    updateUrl: 'https://s3.amazonaws.com/redisinsight.test/public/tests/notifications.json',
+    updateUrl: process.env.NOTIFICATION_UPDATE_URL
+      || 'https://s3.amazonaws.com/redisinsight.test/public/tests/notifications.json',
   },
   features_config: {
     url: process.env.RI_FEATURES_CONFIG_URL
-      // eslint-disable-next-line max-len
       || 'http://localhost:5551/remote/features-config.json',
   },
 };

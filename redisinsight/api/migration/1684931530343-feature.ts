9c7640dbf4cc90d1df5c55c1d2b2c3121fd52e57
Artem <artem.horuzhenko@softeq.com>
2023-05-24 15:35:16 +0300

    #RI-4568 fix number type to be float in local db for conrtolNumber

--- redisinsight/api/migration/1684931530343-feature.ts
+++ redisinsight/api/migration/1684931530343-feature.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class feature1684175820824 implements MigrationInterface {
-    name = 'feature1684175820824'
+export class Feature1684931530343 implements MigrationInterface {
+    name = 'Feature1684931530343'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "features" ("name" varchar PRIMARY KEY NOT NULL, "flag" boolean NOT NULL)`);
-        await queryRunner.query(`CREATE TABLE "features_config" ("id" varchar PRIMARY KEY NOT NULL, "controlNumber" integer, "data" varchar NOT NULL, "updatedAt" datetime NOT NULL DEFAULT (datetime('now')))`);
+        await queryRunner.query(`CREATE TABLE "features_config" ("id" varchar PRIMARY KEY NOT NULL, "controlNumber" float, "data" varchar NOT NULL, "updatedAt" datetime NOT NULL DEFAULT (datetime('now')))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {

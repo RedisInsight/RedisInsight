bdf02e8e215e6c731930fe1e01a334c753f5a6a4
Amir Allayarov <amir.allayarov@softeq.com>
2023-05-25 19:17:26 +0700

    #RI-4569 - add IT

--- redisinsight/api/test/api/database-analysis/POST-databases-id-analysis.test.ts
+++ redisinsight/api/test/api/database-analysis/POST-databases-id-analysis.test.ts
@@ -591,6 +591,46 @@ describe('POST /databases/:instanceId/analysis', () => {
         }
       },
       {
+        name: 'Should create new database analysis with RTS recommendation',
+        data: {
+          delimiter: '-',
+        },
+        statusCode: 201,
+        responseSchema,
+        before: async () => {
+          await rte.data.sendCommand('ZADD', [constants.TEST_ZSET_KEY_2, constants.TEST_ZSET_TIMESTAMP_SCORE, constants.TEST_ZSET_MEMBER_1]);
+        },
+        checkFn: async ({ body }) => {
+          expect(body.recommendations).to.include.deep.members([{
+            ...constants.TEST_RTS_RECOMMENDATION,
+            params: { keys: [{ data: [...Buffer.from(constants.TEST_ZSET_KEY_2)], type: "Buffer" }]}
+          }]);
+        },
+        after: async () => {
+          expect(await repository.count()).to.eq(5);
+        }
+      },
+      {
+        name: 'Should create new database analysis with RTS recommendation',
+        data: {
+          delimiter: '-',
+        },
+        statusCode: 201,
+        responseSchema,
+        before: async () => {
+          await rte.data.sendCommand('ZADD', [constants.TEST_ZSET_KEY_3, constants.TEST_ZSET_MEMBER_1_SCORE, constants.TEST_ZSET_TIMESTAMP_MEMBER]);
+        },
+        checkFn: async ({ body }) => {
+          expect(body.recommendations).to.include.deep.members([{
+            ...constants.TEST_RTS_RECOMMENDATION,
+            params: { keys: [{ data: [...Buffer.from(constants.TEST_ZSET_KEY_3)], type: "Buffer" }]}
+          }]);
+        },
+        after: async () => {
+          expect(await repository.count()).to.eq(5);
+        }
+      },
+      {
         name: 'Should create new database analysis with luaScript recommendation',
         data: {
           delimiter: '-',

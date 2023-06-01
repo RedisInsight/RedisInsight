54cf38ee7eee4f427e6fd5486db71184e094f08f
vlad-dargel <u.darhel@aisnovations.com>
2023-06-01 13:58:26 +0200

    upd

--- tests/e2e/web.runner.ts
+++ tests/e2e/web.runner.ts
@@ -5,6 +5,11 @@ import testcafe from 'testcafe';
         .then(t => {
             return t
                 .createRunner()
+                .compilerOptions({
+                    "typescript": {
+                        configPath: 'tsconfig.testcafe.json',
+                        experimentalDecorators: true
+                     }})
                 .src((process.env.TEST_FILES || 'tests/**/*.e2e.ts').split('\n'))
                 .browsers(['chromium:headless --cache --allow-insecure-localhost --ignore-certificate-errors'])
                 .filter((_testName, _fixtureName, _fixturePath, testMeta): boolean => {

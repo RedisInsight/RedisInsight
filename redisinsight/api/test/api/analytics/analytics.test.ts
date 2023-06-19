efc5e72ee661a8e237db195d939cc9d1d68ebc8e
Artem <artem.horuzhenko@softeq.com>
2023-06-19 19:30:32 +0300

    #RI-4454 fix ITests

--- redisinsight/api/test/api/analytics/analytics.test.ts
+++ redisinsight/api/test/api/analytics/analytics.test.ts
@@ -8,14 +8,14 @@ import {
 } from '../deps';
 const { analytics } = deps;
 
-
 describe('Analytics', () => {
   requirements('rte.serverType=local');
 
   it('APPLICATION_STARTED', () => {
-    // if(serverConfig.get('server').buildType !== 'ELECTRON') {
-    //   return
-    // }
+    if(serverConfig.get('server').buildType !== 'ELECTRON') {
+      return
+    }
+
     const appStarted = analytics.findEvent({
       event: 'APPLICATION_STARTED',
     })
@@ -27,7 +27,7 @@ describe('Analytics', () => {
     const found = appStarted || appFirstStarted;
 
     if (!found) {
-      fail('APPLICATION_STARTED or APPLICATION_FIRST_START events were not found');
+      expect.fail('APPLICATION_STARTED or APPLICATION_FIRST_START events were not found');
     }
 
     expect(found?.properties).to.have.all.keys('appVersion', 'osPlatform', 'buildType', 'controlNumber', 'controlGroup', 'port');

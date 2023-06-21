355135876633f199880c12c724cc01101c95c4a9
zalenskiSofteq <egor.zalenski@softeq.com>
2023-06-21 14:49:09 +0200

    good build

--- scripts/build-statics.cmd
+++ scripts/build-statics.cmd
@@ -13,47 +13,47 @@ xcopy ".\redisinsight\ui\src\assets\fonts\inconsolata" ".\vendor\fonts\" /s /e /
 if not exist %PLUGINS_VENDOR_DIR% mkdir %PLUGINS_VENDOR_DIR%
 xcopy ".\vendor\." "%PLUGINS_VENDOR_DIR%" /s /e /y
 
-@REM :: Build redisearch plugin
-@REM set REDISEARCH_DIR=".\redisinsight\ui\src\packages\redisearch"
-@REM call yarn --cwd "%REDISEARCH_DIR%"
-@REM call yarn --cwd "%REDISEARCH_DIR%" build
-@REM if not exist "%PLUGINS_DIR%\redisearch" mkdir "%PLUGINS_DIR%\redisearch"
-@REM if not exist "%PLUGINS_DIR%\redisearch\dist" mkdir "%PLUGINS_DIR%\redisearch\dist"
-@REM xcopy "%REDISEARCH_DIR%\dist" "%PLUGINS_DIR%\redisearch\dist\" /s /e /y
-@REM copy "%REDISEARCH_DIR%\package.json" "%PLUGINS_DIR%\redisearch\"
+:: Build redisearch plugin
+set REDISEARCH_DIR=".\redisinsight\ui\src\packages\redisearch"
+call yarn --cwd "%REDISEARCH_DIR%"
+call yarn --cwd "%REDISEARCH_DIR%" build
+if not exist "%PLUGINS_DIR%\redisearch" mkdir "%PLUGINS_DIR%\redisearch"
+if not exist "%PLUGINS_DIR%\redisearch\dist" mkdir "%PLUGINS_DIR%\redisearch\dist"
+xcopy "%REDISEARCH_DIR%\dist" "%PLUGINS_DIR%\redisearch\dist\" /s /e /y
+copy "%REDISEARCH_DIR%\package.json" "%PLUGINS_DIR%\redisearch\"
 
-@REM :: Build redisgraph plugin
-@REM set REDISGRAPH_DIR=".\redisinsight\ui\src\packages\redisgraph"
-@REM call yarn --cwd "%REDISGRAPH_DIR%"
-@REM call yarn --cwd "%REDISGRAPH_DIR%" build
-@REM if not exist "%PLUGINS_DIR%\redisgraph" mkdir "%PLUGINS_DIR%\redisgraph"
-@REM if not exist "%PLUGINS_DIR%\redisgraph\dist" mkdir "%PLUGINS_DIR%\redisgraph\dist"
-@REM xcopy "%REDISGRAPH_DIR%\dist" "%PLUGINS_DIR%\redisgraph\dist\" /s /e /y
-@REM copy "%REDISGRAPH_DIR%\package.json" "%PLUGINS_DIR%\redisgraph\"
+:: Build redisgraph plugin
+set REDISGRAPH_DIR=".\redisinsight\ui\src\packages\redisgraph"
+call yarn --cwd "%REDISGRAPH_DIR%"
+call yarn --cwd "%REDISGRAPH_DIR%" build
+if not exist "%PLUGINS_DIR%\redisgraph" mkdir "%PLUGINS_DIR%\redisgraph"
+if not exist "%PLUGINS_DIR%\redisgraph\dist" mkdir "%PLUGINS_DIR%\redisgraph\dist"
+xcopy "%REDISGRAPH_DIR%\dist" "%PLUGINS_DIR%\redisgraph\dist\" /s /e /y
+copy "%REDISGRAPH_DIR%\package.json" "%PLUGINS_DIR%\redisgraph\"
 
-@REM :: Build redistimeseries plugin
-@REM set REDISTIMESERSIES_DIR=".\redisinsight\ui\src\packages\redistimeseries-app"
-@REM call yarn --cwd "%REDISTIMESERSIES_DIR%"
-@REM call yarn --cwd "%REDISTIMESERSIES_DIR%" build
-@REM if not exist "%PLUGINS_DIR%\redistimeseries-app" mkdir "%PLUGINS_DIR%\redistimeseries-app"
-@REM if not exist "%PLUGINS_DIR%\redistimeseries-app\dist" mkdir "%PLUGINS_DIR%\redistimeseries-app\dist"
-@REM xcopy "%REDISTIMESERSIES_DIR%\dist" "%PLUGINS_DIR%\redistimeseries-app\dist\" /s /e /y
-@REM copy "%REDISTIMESERSIES_DIR%\package.json" "%PLUGINS_DIR%\redistimeseries-app\"
+:: Build redistimeseries plugin
+set REDISTIMESERSIES_DIR=".\redisinsight\ui\src\packages\redistimeseries-app"
+call yarn --cwd "%REDISTIMESERSIES_DIR%"
+call yarn --cwd "%REDISTIMESERSIES_DIR%" build
+if not exist "%PLUGINS_DIR%\redistimeseries-app" mkdir "%PLUGINS_DIR%\redistimeseries-app"
+if not exist "%PLUGINS_DIR%\redistimeseries-app\dist" mkdir "%PLUGINS_DIR%\redistimeseries-app\dist"
+xcopy "%REDISTIMESERSIES_DIR%\dist" "%PLUGINS_DIR%\redistimeseries-app\dist\" /s /e /y
+copy "%REDISTIMESERSIES_DIR%\package.json" "%PLUGINS_DIR%\redistimeseries-app\"
 
-@REM :: Build ri-explain plugin
-@REM set RI_EXPLIAIN_DIR=".\redisinsight\ui\src\packages\ri-explain"
-@REM call yarn --cwd "%RI_EXPLIAIN_DIR%"
-@REM call yarn --cwd "%RI_EXPLIAIN_DIR%" build
-@REM if not exist "%PLUGINS_DIR%\ri-explain" mkdir "%PLUGINS_DIR%\ri-explain"
-@REM if not exist "%PLUGINS_DIR%\ri-explain\dist" mkdir "%PLUGINS_DIR%\ri-explain\dist"
-@REM xcopy "%RI_EXPLIAIN_DIR%\dist" "%PLUGINS_DIR%\ri-explain\dist\" /s /e /y
-@REM copy "%RI_EXPLIAIN_DIR%\package.json" "%PLUGINS_DIR%\ri-explain\"
+:: Build ri-explain plugin
+set RI_EXPLIAIN_DIR=".\redisinsight\ui\src\packages\ri-explain"
+call yarn --cwd "%RI_EXPLIAIN_DIR%"
+call yarn --cwd "%RI_EXPLIAIN_DIR%" build
+if not exist "%PLUGINS_DIR%\ri-explain" mkdir "%PLUGINS_DIR%\ri-explain"
+if not exist "%PLUGINS_DIR%\ri-explain\dist" mkdir "%PLUGINS_DIR%\ri-explain\dist"
+xcopy "%RI_EXPLIAIN_DIR%\dist" "%PLUGINS_DIR%\ri-explain\dist\" /s /e /y
+copy "%RI_EXPLIAIN_DIR%\package.json" "%PLUGINS_DIR%\ri-explain\"
 
-@REM :: Build clients-list plugin
-@REM set CLIENTS_LIST_DIR=".\redisinsight\ui\src\packages\clients-list"
-@REM call yarn --cwd "%CLIENTS_LIST_DIR%"
-@REM call yarn --cwd "%CLIENTS_LIST_DIR%" build
-@REM if not exist "%PLUGINS_DIR%\clients-list" mkdir "%PLUGINS_DIR%\clients-list"
-@REM if not exist "%PLUGINS_DIR%\clients-list\dist" mkdir "%PLUGINS_DIR%\clients-list\dist"
-@REM xcopy "%CLIENTS_LIST_DIR%\dist" "%PLUGINS_DIR%\clients-list\dist\" /s /e /y
-@REM copy "%CLIENTS_LIST_DIR%\package.json" "%PLUGINS_DIR%\clients-list\"
+:: Build clients-list plugin
+set CLIENTS_LIST_DIR=".\redisinsight\ui\src\packages\clients-list"
+call yarn --cwd "%CLIENTS_LIST_DIR%"
+call yarn --cwd "%CLIENTS_LIST_DIR%" build
+if not exist "%PLUGINS_DIR%\clients-list" mkdir "%PLUGINS_DIR%\clients-list"
+if not exist "%PLUGINS_DIR%\clients-list\dist" mkdir "%PLUGINS_DIR%\clients-list\dist"
+xcopy "%CLIENTS_LIST_DIR%\dist" "%PLUGINS_DIR%\clients-list\dist\" /s /e /y
+copy "%CLIENTS_LIST_DIR%\package.json" "%PLUGINS_DIR%\clients-list\"

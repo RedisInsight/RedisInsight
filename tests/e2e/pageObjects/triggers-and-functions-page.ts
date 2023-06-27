8d32c58db52f59df1a0afdb1c11445b0271d59fa
mariasergeenko <maria.sergeenko@softeq.com>
2023-06-27 20:45:26 +0200

    add tests for RI-4591

--- tests/e2e/pageObjects/triggers-and-functions-page.ts
+++ tests/e2e/pageObjects/triggers-and-functions-page.ts
@@ -1,15 +1,22 @@
-import { Selector } from 'testcafe';
+import { Selector, t } from 'testcafe';
 import { TriggersAndFunctionLibrary } from '../interfaces/triggers-and-functions';
+import { LibrariesSections } from '../helpers/constants';
 import { InstancePage } from './instance-page';
 
 export class TriggersAndFunctionsPage extends InstancePage {
-    //Containers
-    libraryRow = Selector('[data-testid=row-]');
+    editMonacoButton = Selector('[data-testid=edit-monaco-value]');
+    acceptButton = Selector('[data-testid=apply-btn]');
+
+    inputMonaco = Selector('[class=inlineMonacoEditor]');
+    textAreaMonaco = Selector('[class^=view-lines]');
+
+    configurationLink = Selector('[data-testid=library-view-tab-config]');
+
     /**
      * Is library displayed in the table
      * @param libraryName The Library Name
      */
-    getLibraryNameSelector(libraryName: string):  Selector {
+    getLibraryNameSelector(libraryName: string): Selector {
         return Selector(`[data-testid=row-${libraryName}]`);
     }
 
@@ -17,7 +24,7 @@ export class TriggersAndFunctionsPage extends InstancePage {
      * Get library item by name
      * @param libraryName The Library Name
      */
-    async getLibraryItem(libraryName: string):  Promise<TriggersAndFunctionLibrary> {
+    async getLibraryItem(libraryName: string): Promise<TriggersAndFunctionLibrary> {
         const item = {} as TriggersAndFunctionLibrary;
         const row = this.getLibraryNameSelector(libraryName);
         item.name = await row.find('span').nth(0).textContent;
@@ -27,5 +34,41 @@ export class TriggersAndFunctionsPage extends InstancePage {
 
         return item;
     }
-}
 
+    /**
+     * Is function displayed in the list
+     * @param functionsName The functions Name
+     * @param sectionName The section Name
+     */
+    getFunctionsByName(sectionName: LibrariesSections, functionsName: string): Selector {
+        const KeySpaceSection = Selector(`[data-testid^=functions-${sectionName}]`);
+        return KeySpaceSection.find(`[data-testid=func-${functionsName}]`);
+    }
+
+    /**
+     * Send  commands in monacoEditor
+     * @param commandPart1 The command that should be on the first line
+     * @param commandPart2 command part except mandatory part
+     */
+    async sendTextToMonaco(commandPart1: string, commandPart2?: string): Promise<void> {
+
+        await t
+            // remove text since replace doesn't work here
+            .pressKey('ctrl+a')
+            .pressKey('delete')
+            .typeText(this.inputMonaco, commandPart1);
+        if (commandPart2) {
+            await t.pressKey('enter')
+                .typeText(this.inputMonaco, commandPart2);
+        }
+        await t.click(this.acceptButton);
+    }
+
+    /**
+     * Get  text from monacoEditor
+     */
+    async getTextToMonaco(): Promise<string> {
+
+        return (await this.textAreaMonaco.textContent).replace(/\s+/g, ' ');
+    }
+}
--- tests/e2e/tests/critical-path/triggers-and-functions/libraries.e2e.ts
+++ tests/e2e/tests/critical-path/triggers-and-functions/libraries.e2e.ts
@@ -2,9 +2,9 @@ import { acceptLicenseTermsAndAddDatabaseApi } from '../../../helpers/database';
 import { BrowserPage, TriggersAndFunctionsPage } from '../../../pageObjects';
 import {
     commonUrl,
-    ossStandaloneBigConfig, ossStandaloneRedisGears
+    ossStandaloneRedisGears
 } from '../../../helpers/conf';
-import { rte } from '../../../helpers/constants';
+import { rte, LibrariesSections } from '../../../helpers/constants';
 import { deleteStandaloneDatabaseApi } from '../../../helpers/api/api-database';
 import { TriggersAndFunctionLibrary } from '../../../interfaces/triggers-and-functions';
 
@@ -17,7 +17,7 @@ fixture `Triggers and Functions`
     .meta({ type: 'critical_path', rte: rte.standalone })
     .page(commonUrl)
     .beforeEach(async() => {
-        await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneRedisGears, ossStandaloneBigConfig.databaseName);
+        await acceptLicenseTermsAndAddDatabaseApi(ossStandaloneRedisGears, ossStandaloneRedisGears.databaseName);
     })
     .afterEach(async() => {
         // Delete database
@@ -39,5 +39,63 @@ test
         await t.expect(row.user).eql(item.user, 'user name is unexpected');
         await t.expect(row.pending).eql(item.pending, 'user name is unexpected');
         await t.expect(row.totalFunctions).eql(item.totalFunctions, 'user name is unexpected');
+    });
+
+test.only
+    .after(async() => {
+        await browserPage.Cli.sendCommandInCli(`TFUNCTION DELETE ${libraryName}`);
+        await deleteStandaloneDatabaseApi(ossStandaloneRedisGears);
+    })('Verify that library details is displayed', async t => {
+
+        const function1 = 'Function1';
+        const function2 = 'function2';
+        const asyncFunction = 'AsyncFunction';
+        const stream = 'StreamTrigger';
+        const cluster = 'registerClusterFunction';
+        const keySpaceTrigger = 'registerKeySpaceTrigger';
+        const command = `TFUNCTION LOAD "#!js api_version=1.0 name=${libraryName}\\n
+            redis.registerFunction('${function1}', function(){});
+            redis.registerFunction('${function2}', function(){});
+            redis.registerAsyncFunction('${asyncFunction}', function(){});
+            redis.registerStreamTrigger('${stream}', 'name', function(){});
+            redis.registerClusterFunction('${cluster}', async function(){});
+            redis.registerKeySpaceTrigger('${keySpaceTrigger}','',function(){});"`;
+
+        await browserPage.Cli.sendCommandInCli(command);
+
+        await t.click(browserPage.NavigationPanel.triggeredFunctionsButton);
+        await t.click(await triggersAndFunctionsPage.getLibraryNameSelector(libraryName));
+        await t.expect(await triggersAndFunctionsPage.getFunctionsByName(LibrariesSections.Functions, function1).exists).ok('library is not displayed in Functions section');
+        await t.expect(await triggersAndFunctionsPage.getFunctionsByName(LibrariesSections.Functions, function2).exists).ok('library is not displayed in Functions section');
+        await t.expect(await triggersAndFunctionsPage.getFunctionsByName(LibrariesSections.Functions, asyncFunction).exists).ok('library is not displayed in Functions section');
+        await t.expect(await triggersAndFunctionsPage.getFunctionsByName(LibrariesSections.StreamFunctions, stream).exists).ok('library is not displayed in Stream section');
+        await t.expect(await triggersAndFunctionsPage.getFunctionsByName(LibrariesSections.ClusterFunctions, cluster).exists).ok('library is not displayed in cluster section');
+        await t.expect(await triggersAndFunctionsPage.getFunctionsByName(LibrariesSections.KeyspaceTriggers, keySpaceTrigger).exists).ok('library is not displayed in key Space Trigger section');
+    });
+
+test
+    .after(async() => {
+        await browserPage.Cli.sendCommandInCli(`TFUNCTION DELETE ${libraryName}`);
+        await deleteStandaloneDatabaseApi(ossStandaloneRedisGears);
+    })('Verify that user can modify  code', async t => {
+        const command = `TFUNCTION LOAD "#!js api_version=1.0 name=${libraryName}\\n redis.registerFunction(\'foo\', ()=>{return \'bar\'});"`;
+        const commandUpdatedPart1 = `#!js api_version=1.0 name=${libraryName}`;
+        const commandUpdatedPart2 = ' redis.registerFunction(\'foo\', ()=>{return \'bar new\'});';
+        const configuration = '{"redisgears_2.lock-redis-timeout": 1000}';
+
+        await browserPage.Cli.sendCommandInCli(command);
+        await t.click(browserPage.NavigationPanel.triggeredFunctionsButton);
+        await t.click(await triggersAndFunctionsPage.getLibraryNameSelector(libraryName));
+        await t.click(triggersAndFunctionsPage.editMonacoButton);
+        await triggersAndFunctionsPage.sendTextToMonaco(commandUpdatedPart1, commandUpdatedPart2);
+
+        await t.expect(
+            (await triggersAndFunctionsPage.getTextToMonaco())).eql(commandUpdatedPart1 + commandUpdatedPart2), 'code was not updated';
+
+        await t.click(await triggersAndFunctionsPage.configurationLink);
+        await t.click(triggersAndFunctionsPage.editMonacoButton);
+        await triggersAndFunctionsPage.sendTextToMonaco(configuration);
+        await t.expect(
+            (await triggersAndFunctionsPage.getTextToMonaco())).eql(configuration, 'configuration was not added');
     });
 

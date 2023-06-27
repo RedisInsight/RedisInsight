f65efb16041da521030927f8d4f2690855d6a4a0
vlad-dargel <u.darhel@aisnovations.com>
2023-06-27 17:22:44 +0200

    updates for api methods to run desktop tests

---
+++ tests/e2e/helpers/api/api-common.ts
@@ -0,0 +1,45 @@
+import * as request from 'supertest';
+import { Common } from '../common';
+import { Methods } from '../constants';
+
+const endpoint = Common.getEndpoint();
+
+/**
+ * Send request using API
+ * @param resourcePath URI path segment
+ * @param body Request body
+ * @param statusCode Expected status code of the response
+ */
+export async function sendRequest(
+    method: string,
+    resourcePath: string,
+    statusCode: number,
+    body?: Record<string, unknown>
+): Promise<any> {
+    const windowId = Common.getWindowId();
+    let requestEndpoint;
+
+    if (method === Methods.post) {
+        (requestEndpoint = request(endpoint)
+            .post(resourcePath)
+            .send(body)
+            .set('Accept', 'application/json'));
+    }
+    else if (method === Methods.get) {
+        (requestEndpoint = request(endpoint)
+            .get(resourcePath)
+            .set('Accept', 'application/json'));
+    }
+    else if (method === Methods.delete) {
+        (requestEndpoint = request(endpoint)
+            .delete(resourcePath)
+            .send(body)
+            .set('Accept', 'application/json'));
+    }
+
+    if (await windowId) {
+        requestEndpoint.set('X-Window-Id', await windowId);
+    }
+
+    return await requestEndpoint.expect(statusCode);
+}

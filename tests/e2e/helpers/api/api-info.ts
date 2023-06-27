26e029ac14a8fd0300336d0692153c0213b973f4
vlad-dargel <u.darhel@aisnovations.com>
2023-06-27 18:49:53 +0200

    fix

--- tests/e2e/helpers/api/api-info.ts
+++ tests/e2e/helpers/api/api-info.ts
@@ -5,5 +5,5 @@ import { Methods } from '../constants';
  * Synchronize features
  */
 export async function syncFeaturesApi(): Promise<void> {
-    await sendRequest(Methods.post, '/features/sync', 201);
+    await sendRequest(Methods.post, '/features/sync', 200);
 }

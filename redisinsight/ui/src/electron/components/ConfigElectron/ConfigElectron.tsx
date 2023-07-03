a27610260703b77a5375743350bf1b84d80d75f4
zalenskiSofteq <egor.zalenski@softeq.com>
2023-07-03 11:44:24 +0200

    #RI-4617 - update integration

--- redisinsight/ui/src/electron/components/ConfigElectron/ConfigElectron.tsx
+++ redisinsight/ui/src/electron/components/ConfigElectron/ConfigElectron.tsx
@@ -7,18 +7,28 @@ import {
 } from 'uiSrc/slices/app/info'
 import { ipcCheckUpdates, ipcSendEvents } from 'uiSrc/electron/utils'
 import { ipcDeleteDownloadedVersion } from 'uiSrc/electron/utils/ipcDeleteStoreValues'
+import { CloudAuthResponse, CloudAuthStatus } from 'uiSrc/electron/constants'
+import { addErrorNotification } from 'uiSrc/slices/app/notifications'
+import { fetchAccountInfo, oauthCloudAccountSelector } from 'uiSrc/slices/oauth/cloud'
 
 const ConfigElectron = () => {
   let isCheckedUpdates = false
   const { identified: analyticsIdentified } = useSelector(appAnalyticsInfoSelector)
   const { isReleaseNotesViewed } = useSelector(appElectronInfoSelector)
   const serverInfo = useSelector(appServerInfoSelector)
+  const currentAccount = useSelector(oauthCloudAccountSelector)
 
   const dispatch = useDispatch()
 
   useEffect(() => {
-    window.app.cloudOauthCallback((_e: any, connected: string = '') => {
-      console.log({ connected })
+    window.app.cloudOauthCallback((_e: any, { status, message, error }: CloudAuthResponse) => {
+      if (status === CloudAuthStatus.Succeed) {
+        dispatch(fetchAccountInfo())
+      }
+
+      if (status === CloudAuthStatus.Failed) {
+        dispatch(addErrorNotification(error))
+      }
     })
   }, [])
 
@@ -40,6 +50,10 @@ const ConfigElectron = () => {
       ipcDeleteDownloadedVersion()
     }
   }, [isReleaseNotesViewed])
+
+  useEffect(() => {
+    console.log({ currentAccount })
+  }, [currentAccount])
 
   return null
 }

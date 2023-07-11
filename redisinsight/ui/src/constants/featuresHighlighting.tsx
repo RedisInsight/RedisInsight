7edfa1599780dd592a7044f57158d58ce99e0202
Roman Sergeenko <roman.sergeenko@softeq.com>
2023-07-11 10:59:24 +0200

    #RI-4600 - add highlighting for triggers & functions page

--- redisinsight/ui/src/constants/featuresHighlighting.tsx
+++ redisinsight/ui/src/constants/featuresHighlighting.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PageNames } from 'uiSrc/constants/pages'
 
 export type FeaturesHighlightingType = 'plain' | 'tooltip' | 'popover'
 
@@ -7,8 +8,15 @@ interface BuildHighlightingFeature {
   title?: string | React.ReactElement
   content?: string | React.ReactElement
   page?: string
+  asPageFeature?: boolean
 }
 
 export const BUILD_FEATURES: Record<string, BuildHighlightingFeature> = {
-
+  [PageNames.triggeredFunctions]: {
+    type: 'tooltip',
+    title: 'Triggers & Functions',
+    content: 'Triggers and Functions can execute server-side functions triggered by events or data operations to decrease latency and react in real time to database events.',
+    page: PageNames.triggeredFunctions,
+    asPageFeature: true
+  }
 } as const

1db456072e7e3f3c48e30275ef3ecc413f6c8ae9
Artem <artem.horuzhenko@softeq.com>
2023-07-17 13:47:16 +0300

    #RI-4729 - redirect to known static page. Remove redirect web strategy support

--- redisinsight/api/src/modules/cloud/cloud-sso.feature.flag.ts
+++ redisinsight/api/src/modules/cloud/cloud-sso.feature.flag.ts
@@ -12,29 +12,8 @@ export enum CloudSsoFeatureStrategy {
 }
 
 export class CloudSsoFeatureFlag {
-  static getWebEndpoint(defaultUri: string): string {
-    const { strategy } = CloudSsoFeatureFlag.getFeature();
-
-    if (strategy === CloudSsoFeatureStrategy.Web) {
-      const protocol = serverConfig.tls && serverConfig.tlsCert && serverConfig.tlsKey ? 'https' : 'http';
-      const port = parseInt(process.env.API_PORT || serverConfig.port, 10);
-      const url = new URL('/api/cloud/oauth/callback', `${protocol}://127.0.0.1:${port}`);
-      return url.toString();
-    }
-
-    return defaultUri;
-  }
-
   static getFeature(): Feature {
     if (serverConfig.buildType === BuildType.Electron) {
-      if (process.env.BUILD_PACKAGE?.toLowerCase?.() === PackageType.AppImage) {
-        return {
-          name: KnownFeatures.CloudSso,
-          flag: inRange(parseInt(process.env.API_PORT || serverConfig.port, 10), 5530, 5540),
-          strategy: CloudSsoFeatureStrategy.Web,
-        };
-      }
-
       return {
         name: KnownFeatures.CloudSso,
         flag: true,

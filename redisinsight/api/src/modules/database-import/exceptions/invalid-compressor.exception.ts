fd94b700e72a2fe615556e4d28877413222de8ba
Amir Allayarov <amir.allayarov@softeq.com>
2023-08-02 14:09:56 +0800

    #RI-4476 - update name

--- redisinsight/api/src/modules/database-import/exceptions/invalid-compressor.exception.ts
+++ redisinsight/api/src/modules/database-import/exceptions/invalid-compressor.exception.ts
@@ -2,7 +2,7 @@ import { HttpException } from '@nestjs/common';
 import ERROR_MESSAGES from 'src/constants/error-messages';
 
 export class InvalidCompressorException extends HttpException {
-  constructor(message: string = ERROR_MESSAGES.INVALID_SSH_COMPRESSOR) {
+  constructor(message: string = ERROR_MESSAGES.INVALID_COMPRESSOR) {
     const response = {
       message,
       statusCode: 400,

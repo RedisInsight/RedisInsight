46f6d0a79b90af531b2868cff735e8844e51ca27
Roman Sergeenko <roman.sergeenko@softeq.com>
2023-08-14 10:07:35 +0200

    #RI-4841 - add collapsible nav group for autodiscovery form

--- redisinsight/ui/src/pages/home/components/CloudConnection/CloudConnectionForm/CloudConnectionForm.spec.tsx
+++ redisinsight/ui/src/pages/home/components/CloudConnection/CloudConnectionForm/CloudConnectionForm.spec.tsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import { instance, mock } from 'ts-mockito'
-import { render } from 'uiSrc/utils/test-utils'
+import { render, screen } from 'uiSrc/utils/test-utils'
+import { appFeatureFlagsFeaturesSelector } from 'uiSrc/slices/app/features'
 import CloudConnectionForm, { Props } from './CloudConnectionForm'
 
 const mockedProps = mock<Props>()
+
+jest.mock('uiSrc/slices/app/features', () => ({
+  ...jest.requireActual('uiSrc/slices/app/features'),
+  appFeatureFlagsFeaturesSelector: jest.fn().mockReturnValue({
+    cloudSso: {
+      flag: false,
+    }
+  }),
+}))
 
 describe('CloudConnectionForm', () => {
   it('should render', () => {
     expect(
       render(<CloudConnectionForm {...instance(mockedProps)} />)
     ).toBeTruthy()
+  })
+
+  it('should not render cloud sso form by default', () => {
+    render(<CloudConnectionForm {...instance(mockedProps)} />)
+    expect(screen.queryByTestId('use-cloud-account-accordion')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('use-cloud-keys-accordion')).not.toBeInTheDocument()
+
+    expect(screen.getByTestId('access-key')).toBeInTheDocument()
+    expect(screen.getByTestId('secret-key')).toBeInTheDocument()
+  })
+
+  it('should render cloud sso form and collapsible nav groups with feature flag', () => {
+    (appFeatureFlagsFeaturesSelector as jest.Mock).mockReturnValue({
+      cloudSso: {
+        flag: true
+      }
+    })
+
+    render(<CloudConnectionForm {...instance(mockedProps)} />)
+
+    expect(screen.getByTestId('use-cloud-account-accordion')).toBeInTheDocument()
+    expect(screen.getByTestId('use-cloud-keys-accordion')).toBeInTheDocument()
+
+    expect(screen.getByTestId('access-key')).toBeInTheDocument()
+    expect(screen.getByTestId('secret-key')).toBeInTheDocument()
   })
 })

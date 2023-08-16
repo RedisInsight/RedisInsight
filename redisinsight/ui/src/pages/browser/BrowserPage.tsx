8b84c3aebc2580731c85eb4ad310e1042ee603d1
Roman Sergeenko <roman.sergeenko@softeq.com>
2023-08-16 14:53:18 +0200

    remove prop

--- redisinsight/ui/src/pages/browser/BrowserPage.tsx
+++ redisinsight/ui/src/pages/browser/BrowserPage.tsx
@@ -275,7 +275,6 @@ const BrowserPage = () => {
                     isBulkActionsPanelOpen={isBulkActionsPanelOpen}
                     handleAddKeyPanel={handleAddKeyPanel}
                     handleBulkActionsPanel={handleBulkActionsPanel}
-                    handleCreateIndexPanel={handleCreateIndexPanel}
                     closeRightPanels={closeRightPanels}
                   />
                 </EuiResizablePanel>

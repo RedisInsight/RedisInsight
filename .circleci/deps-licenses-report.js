8f4a01ad4eb5f19d1b07112615ffddf7acd5312a
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2023-08-17 18:35:04 +0200

    #RI-4842 - Add application version to all telemetry events

--- .circleci/deps-licenses-report.js
+++ .circleci/deps-licenses-report.js
@@ -1,50 +1,76 @@
 const fs = require('fs');
-const { exec } = require("child_process");
+const { exec } = require('child_process');
+const { last } = require('lodash');
 
-const folderName = 'licenses'
+const folderName = 'licenses';
 const packageJsons = [
   {
-    name: 'ui+electron',
+    fileName: 'ui+electron',
     path: './'
   },
   {
-    name: 'electron',
+    fileName: 'electron',
     path: './redisinsight'
   },
   {
-    name: 'api',
     path: './redisinsight/api'
   },
   {
-    name: 'e2e',
     path: './redisinsight/tests/e2e'
   },
-]
+  {
+    path: "./redisinsight/ui/src/packages/redisearch",
+  },
+  {
+    path: "./redisinsight/ui/src/packages/redisgraph",
+  },
+  {
+    path: "./redisinsight/ui/src/packages/redistimeseries-app",
+  },
+  {
+    path: "./redisinsight/ui/src/packages/ri-explain",
+  },
+  {
+    path: "./redisinsight/ui/src/packages/clients-list",
+  },
+];
 
-async function main() {
+async function createFolderIfNotExists() {
   try {
     if (!fs.existsSync(folderName)) {
       fs.mkdirSync(folderName);
     }
   } catch (err) {
-    console.error(`create a folder failed ${folderName}, error:`, err);
+    console.error(`Failed to create folder ${folderName}, error:`, err);
     process.exit(1);
   }
+}
 
-  await Promise.all(packageJsons.map(async ({name, path}) => {
-    const licenses = await new Promise((resolve, reject) => {
-      const command = `license-checker --start ${path} --csv --out ./${folderName}/licenses-${name}.csv`
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`failed command: ${command}, error`, stderr);
-          reject();
-          process.exit(1);
-        }
-        resolve();
-      })
-    })
-  }))
+async function runLicenseCheck({ fileName, path }) {
+  const name = fileName || last(path.split('/'));
 
+  const command = `license-checker --start ${path} --csv --out ./${folderName}/licenses-${name}.csv`;
+
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Failed command: ${command}, error:`, stderr);
+        reject(error);
+      }
+      resolve();
+    });
+  });
+}
+
+async function main() {
+  await createFolderIfNotExists();
+
+  try {
+    await Promise.all(packageJsons.map(runLicenseCheck));
+  } catch (error) {
+    console.error('An error occurred:', error);
+    process.exit(1);
+  }
 }
 
 main();

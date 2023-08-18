87e620ce8f7d8e137d7eaad92a8bb5479943c119
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2023-08-18 15:01:37 +0200

    #RI-3478 - Added summary

--- .circleci/deps-licenses-report.js
+++ .circleci/deps-licenses-report.js
@@ -1,20 +1,25 @@
 const fs = require('fs');
 const { join } = require('path');
-const { last } = require('lodash');
+const { last, set } = require('lodash');
 const { google } = require('googleapis');
 const { exec } = require('child_process');
 const csvParser = require('csv-parser');
+const { stringify } = require('csv-stringify');
 
 const licenseFolderName = 'licenses';
 const spreadsheetId = process.env.SPREADSHEET_ID;
 const outputFilePath = `./${licenseFolderName}/licenses.csv`;
+const summaryFilePath = `./${licenseFolderName}/summary.csv`;
+const allData = [];
+let csvFiles = [];
+
 
 // Main function
 async function main() {
   const folderPath = './';
   const packageJsons = findPackageJsonFiles(folderPath); // Find all package.json files in the given folder
 
-  console.log('All package.jsons was found', packageJsons);
+  console.log('All package.jsons was found:', packageJsons);
 
   // Create the folder if it doesn't exist
   if (!fs.existsSync(licenseFolderName)) {
@@ -24,6 +29,7 @@ async function main() {
   try {
     await Promise.all(packageJsons.map(runLicenseCheck));
     console.log('All csv files was generated');
+    await generateSummary()
     await sendLicensesToGoogleSheet()
   } catch (error) {
     console.error('An error occurred:', error);
@@ -95,10 +101,7 @@ async function sendLicensesToGoogleSheet() {
 
     const sheets = google.sheets('v4');
 
-
     // Read all .csv files in the 'licenses' folder
-    const csvFiles = fs.readdirSync(licenseFolderName).filter(file => file.endsWith('.csv')).sort();
-
     csvFiles.forEach((csvFile) => {
       // Extract sheet name from file name
       const sheetName = csvFile.replace('.csv', '').replaceAll('_', ' ');
@@ -131,17 +134,7 @@ async function sendLicensesToGoogleSheet() {
               await sheets.spreadsheets.batchUpdate({
                 auth: jwtClient,
                 spreadsheetId,
-                resource: {
-                  requests: [
-                    {
-                      addSheet: {
-                        properties: {
-                          title: sheetName,
-                        },
-                      },
-                    },
-                  ],
-                },
+                resource: set({}, 'requests[0].addSheet.properties.title', sheetName),
               });
             }
           } catch (error) {
@@ -173,4 +166,87 @@ async function sendLicensesToGoogleSheet() {
   } catch (error) {
     console.error('Error loading service account key:', error);
   }
+}
+
+// Function to read and process each CSV file
+const processCSVFile = (file) => {
+  return new Promise((resolve, reject) => {
+    const parser = csvParser({ columns: true, trim: true });
+    const input = fs.createReadStream(`./${licenseFolderName}/${file}`);
+
+    parser.on('data', (record) => {
+      allData.push(record);
+    });
+
+    parser.on('end', () => {
+      resolve();
+    });
+
+    parser.on('error', (err) => {
+      reject(err);
+    });
+
+    input.pipe(parser);
+  });
+};
+
+// Process and aggregate license data
+const processLicenseData = () => {
+  const licenseCountMap = {};
+  for (const record of allData) {
+    const license = record.license;
+    licenseCountMap[license] = (licenseCountMap[license] || 0) + 1;
+  }
+  return licenseCountMap;
+};
+
+// Create summary CSV data
+const createSummaryData = (licenseCountMap) => {
+  const summaryData = [['License', 'Count']];
+  for (const license in licenseCountMap) {
+    summaryData.push([license, licenseCountMap[license]]);
+  }
+  return summaryData;
+};
+
+// Write summary CSV file
+const writeSummaryCSV = async (summaryData) => {
+  try {
+    const summaryCsvString = await stringifyPromise(summaryData);
+    fs.writeFileSync(summaryFilePath, summaryCsvString);
+    csvFiles.push(last(summaryFilePath.split('/')));
+    console.log(`Summary CSV saved as ${summaryFilePath}`);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+};
+
+// Stringify as a promise
+const stringifyPromise = (data) => {
+  return new Promise((resolve, reject) => {
+    stringify(data, (err, csvString) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(csvString);
+      }
+    });
+  });
+};
+
+async function generateSummary() {
+  csvFiles = fs.readdirSync(licenseFolderName).filter(file => file.endsWith('.csv')).sort();
+
+  for (const file of csvFiles) {
+    try {
+      await processCSVFile(file);
+    } catch (err) {
+      console.error(`Error processing ${file}: ${err}`);
+    }
+  }
+
+  const licenseCountMap = processLicenseData();
+  const summaryData = createSummaryData(licenseCountMap);
+
+  await writeSummaryCSV(summaryData);
 }

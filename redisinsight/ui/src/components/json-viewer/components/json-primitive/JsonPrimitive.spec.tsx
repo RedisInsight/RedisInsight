0d6477610c289b6261d283fc0b4884bcac6ac20d
Amir Allayarov <amir.allayarov@softeq.com>
2023-08-18 15:22:12 +0800

    #RI-4795 - fix test

--- redisinsight/ui/src/components/json-viewer/components/json-primitive/JsonPrimitive.spec.tsx
+++ redisinsight/ui/src/components/json-viewer/components/json-primitive/JsonPrimitive.spec.tsx
@@ -12,49 +12,49 @@ describe('JsonPrimitive', () => {
     render(<JsonPrimitive data={null} />)
 
     expect(screen.getByTestId('json-primitive-value')).toHaveTextContent('null')
-    expect(screen.getByTestId('json-primitive-value')).toHaveClass('null')
+    expect(screen.getByTestId('json-primitive-value')).toHaveClass('json-pretty__null-value')
   })
 
   it('should render proper text and value for number', () => {
     render(<JsonPrimitive data={1} />)
 
     expect(screen.getByTestId('json-primitive-value')).toHaveTextContent('1')
-    expect(screen.getByTestId('json-primitive-value')).toHaveClass('number')
+    expect(screen.getByTestId('json-primitive-value')).toHaveClass('json-pretty__number-value')
   })
 
   it('should render proper text and value for boolean', () => {
     render(<JsonPrimitive data={false} />)
 
     expect(screen.getByTestId('json-primitive-value')).toHaveTextContent('false')
-    expect(screen.getByTestId('json-primitive-value')).toHaveClass('boolean')
+    expect(screen.getByTestId('json-primitive-value')).toHaveClass('json-pretty__boolean-value')
   })
 
   it('should render proper text and value for string', () => {
     render(<JsonPrimitive data="123" />)
 
     expect(screen.getByTestId('json-primitive-value')).toHaveTextContent('"123"')
-    expect(screen.getByTestId('json-primitive-value')).toHaveClass('string')
+    expect(screen.getByTestId('json-primitive-value')).toHaveClass('json-pretty__string-value')
   })
 
   it('should render proper text and value for bigint', () => {
     render(<JsonPrimitive data={BigInt(123)} />)
 
     expect(screen.getByTestId('json-primitive-value')).toHaveTextContent('123')
-    expect(screen.getByTestId('json-primitive-value')).toHaveClass('bigint')
+    expect(screen.getByTestId('json-primitive-value')).toHaveClass('json-pretty__bigint-value')
   })
 
   it('should render proper text and value for other types', () => {
     render(<JsonPrimitive data={new Map()} />)
 
     expect(screen.getByTestId('json-primitive-value')).toHaveTextContent('[object Map]')
-    expect(screen.getByTestId('json-primitive-value')).toHaveClass('otherValue')
+    expect(screen.getByTestId('json-primitive-value')).toHaveClass('json-pretty__other-value')
   })
 
   it('should render proper text and value for true', () => {
     render(<JsonPrimitive data />)
 
     expect(screen.getByTestId('json-primitive-value')).toHaveTextContent('true')
-    expect(screen.getByTestId('json-primitive-value')).toHaveClass('boolean')
+    expect(screen.getByTestId('json-primitive-value')).toHaveClass('json-pretty__boolean-value')
   })
 
   it('should render coma', () => {

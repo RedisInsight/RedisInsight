301cddf6c5f9ccd99f16ed3d2550cc9886b88269
mariasergeenko <maria.sergeenko@softeq.com>
2023-08-18 15:14:28 +0200

    remove debug

--- tests/e2e/test-data/formatters-data.ts
+++ tests/e2e/test-data/formatters-data.ts
@@ -4,7 +4,8 @@
 export const formatters = [{
     format: 'JSON',
     fromText: '{ "field": "value" }',
-    fromTextEdit: '{ "field": "value123" }'
+    fromTextEdit: '{ "field": "value123" }',
+    fromBigInt: '{ "field": 248480010225057793 }'
 },
 {
     format: 'Msgpack',

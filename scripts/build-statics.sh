a8b7fd82043e0be728bae53116d0a858411eebda
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2023-08-18 15:58:44 +0200

    #RI-3478 - not build plugins for licenses check

--- scripts/build-statics.sh
+++ scripts/build-statics.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-echo $pluginsOnlyInstall
-
-if [ $pluginsOnlyInstall == 1 ]; then
-  exit 1
-fi
-
-
 # =============== Plugins ===============
 PLUGINS_DIR="./redisinsight/api/static/plugins"
 PLUGINS_VENDOR_DIR="./redisinsight/api/static/resources/plugins"
@@ -25,7 +18,7 @@ cp -R "./vendor/." "${PLUGINS_VENDOR_DIR}"
 # Build redisearch plugin
 REDISEARCH_DIR="./redisinsight/ui/src/packages/redisearch"
 yarn --cwd "${REDISEARCH_DIR}"
-if [ $pluginsOnlyInstall != "1" ]; then
+if [ $pluginsOnlyInstall != 1 ]; then
   yarn --cwd "${REDISEARCH_DIR}" build
   mkdir -p "${PLUGINS_DIR}/redisearch"
   cp -R "${REDISEARCH_DIR}/dist" "${REDISEARCH_DIR}/package.json" "${PLUGINS_DIR}/redisearch"
@@ -35,7 +28,7 @@ fi
 # Build redisgraph plugin
 REDISGRAPH_DIR="./redisinsight/ui/src/packages/redisgraph"
 yarn --cwd "${REDISGRAPH_DIR}"
-if [ $pluginsOnlyInstall != "1" ]; then
+if [ $pluginsOnlyInstall != 1 ]; then
   yarn --cwd "${REDISGRAPH_DIR}" build
   mkdir -p "${PLUGINS_DIR}/redisgraph"
   cp -R "${REDISGRAPH_DIR}/dist" "${REDISGRAPH_DIR}/package.json" "${PLUGINS_DIR}/redisgraph"
@@ -44,7 +37,7 @@ fi
 # Build timeseries plugin
 REDISTIMESERIES_DIR="./redisinsight/ui/src/packages/redistimeseries-app"
 yarn --cwd "${REDISTIMESERIES_DIR}"
-if [ $pluginsOnlyInstall != "1" ]; then
+if [ $pluginsOnlyInstall != 1 ]; then
   yarn --cwd "${REDISTIMESERIES_DIR}" build
   mkdir -p "${PLUGINS_DIR}/redistimeseries-app"
   cp -R "${REDISTIMESERIES_DIR}/dist" "${REDISTIMESERIES_DIR}/package.json" "${PLUGINS_DIR}/redistimeseries-app"
@@ -53,7 +46,7 @@ fi
 # Build ri-explain plugin
 RI_EXPLIAIN_DIR="./redisinsight/ui/src/packages/ri-explain"
 yarn --cwd "${RI_EXPLIAIN_DIR}"
-if [ $pluginsOnlyInstall != "1" ]; then
+if [ $pluginsOnlyInstall != 1 ]; then
   yarn --cwd "${RI_EXPLIAIN_DIR}" build
   mkdir -p "${PLUGINS_DIR}/ri-explain"
   cp -R "${RI_EXPLIAIN_DIR}/dist" "${RI_EXPLIAIN_DIR}/package.json" "${PLUGINS_DIR}/ri-explain"
@@ -62,7 +55,7 @@ fi
 # Build clients-list and json plugin
 CLIENTS_LIST_DIR="./redisinsight/ui/src/packages/clients-list"
 yarn --cwd "${CLIENTS_LIST_DIR}"
-if [ $pluginsOnlyInstall != "1" ]; then
+if [ $pluginsOnlyInstall != 1 ]; then
   yarn --cwd "${CLIENTS_LIST_DIR}" build
   mkdir -p "${PLUGINS_DIR}/clients-list"
   cp -R "${CLIENTS_LIST_DIR}/dist" "${CLIENTS_LIST_DIR}/package.json" "${PLUGINS_DIR}/clients-list"

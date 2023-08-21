6cb66600e468255cb073336836d99807a035b9d4
Amir Allayarov <amir.allayarov@softeq.com>
2023-08-21 15:51:12 +0800

    #RI-4869 - resolve comments

--- redisinsight/ui/src/components/json-viewer/components/json-object/JsonObject.spec.tsx
+++ redisinsight/ui/src/components/json-viewer/components/json-object/JsonObject.spec.tsx
@@ -27,4 +27,17 @@ describe('JsonObject', () => {
 
     expect(screen.getByTestId('json-object-component')).toHaveTextContent('{ "value": "{}" }')
   })
+
+  it('should not render empty space and line break', () => {
+    render(<JsonObject data={{}} lastElement />)
+
+    expect(screen.getByTestId('json-object-component')).toHaveTextContent('{}', { normalizeWhitespace: false })
+  })
+
+  it('should render empty space and line break', () => {
+    const renderedObject = '{\n  "value": "{}"\n}'
+    render(<JsonObject data={mockJson} lastElement />)
+
+    expect(screen.getByTestId('json-object-component')).toHaveTextContent(renderedObject, { normalizeWhitespace: false })
+  })
 })

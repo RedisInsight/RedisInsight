4fc29fed14e4dad6e05a80a29170f1318c6d8e37
Amir Allayarov <amir.allayarov@softeq.com>
2023-08-21 15:13:57 +0800

    #RI-4869 - fix space in empty array and object

--- redisinsight/ui/src/packages/clients-list/src/components/json-view/components/json-object/JsonObject.tsx
+++ redisinsight/ui/src/packages/clients-list/src/components/json-view/components/json-object/JsonObject.tsx
@@ -3,29 +3,33 @@ import React, { Fragment } from 'react'
 import JsonPretty from '../json-pretty'
 import { IJsonObjectProps } from '../../interfaces'
 
-const JsonObject = ({ data, space = 2, gap = 0, lastElement = true }: IJsonObjectProps) => (
-  <span data-testid="json-object-component">
-    {'{\n'}
-    {Object.keys(data).map((key, idx) => (
-      <Fragment key={`${key}-{idx}`}>
-        {!!space && Array.from({ length: space + gap }, () => ' ')}
-        <span className="json-pretty__key">
-          {`"${key}"`}
-        </span>
-        {': '}
-        <JsonPretty
-          data={data[key]}
-          lastElement={idx === Object.keys(data).length - 1}
-          space={space}
-          gap={gap + space}
-        />
-      </Fragment>
-    ))}
-    {!!gap && Array.from({ length: gap }, () => ' ')}
-    {'}'}
-    {!lastElement && ','}
-    {'\n'}
-  </span>
-)
+const JsonObject = ({ data, space = 2, gap = 0, lastElement = true }: IJsonObjectProps) => {
+  const keys = Object.keys(data)
+  return (
+    <span data-testid="json-object-component">
+      {'{'}
+      {!!keys.length && '\n'}
+      {keys.map((key, idx) => (
+        <Fragment key={`${key}-{idx}`}>
+          {!!space && Array.from({ length: space + gap }, () => ' ')}
+          <span className="json-pretty__key">
+            {`"${key}"`}
+          </span>
+          {': '}
+          <JsonPretty
+            data={data[key]}
+            lastElement={idx === Object.keys(data).length - 1}
+            space={space}
+            gap={gap + space}
+          />
+        </Fragment>
+      ))}
+      {!!keys.length && !!gap && Array.from({ length: gap }, () => ' ')}
+      {'}'}
+      {!lastElement && ','}
+      {'\n'}
+    </span>
+  )
+}
 
 export default JsonObject

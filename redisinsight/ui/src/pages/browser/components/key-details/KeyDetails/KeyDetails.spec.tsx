5665d169e39d0819232ad83e3a85284af7d065f6
Roman Sergeenko <roman.sergeenko@softeq.com>
2023-08-22 09:26:34 +0200

    #RI-4879 - fix explore guides message showing

--- redisinsight/ui/src/pages/browser/components/key-details/KeyDetails/KeyDetails.spec.tsx
+++ redisinsight/ui/src/pages/browser/components/key-details/KeyDetails/KeyDetails.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { instance, mock } from 'ts-mockito'
-import { render } from 'uiSrc/utils/test-utils'
+import { render, screen } from 'uiSrc/utils/test-utils'
 import KeyDetails, { Props } from './KeyDetails'
 
 const mockedProps = mock<Props>()
@@ -8,5 +8,17 @@ const mockedProps = mock<Props>()
 describe('KeyDetails', () => {
   it('should render', () => {
     expect(render(<KeyDetails {...instance(mockedProps)} />)).toBeTruthy()
+  })
+
+  it('should render explore-guides when there are no keys', () => {
+    render(<KeyDetails {...instance(mockedProps)} totalKeys={0} />)
+
+    expect(screen.getByTestId('explore-guides')).toBeInTheDocument()
+  })
+
+  it('should render proper message when there are keys', () => {
+    render(<KeyDetails {...instance(mockedProps)} totalKeys={10} />)
+
+    expect(screen.getByTestId('select-key-message')).toBeInTheDocument()
   })
 })

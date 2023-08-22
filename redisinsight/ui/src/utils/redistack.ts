9e420e1e548d84c8799291d86aa136c95c528742
Amir Allayarov <amir.allayarov@softeq.com>
2023-08-22 22:59:18 +0800

    #RI-4880 - update condition

--- redisinsight/ui/src/utils/redistack.ts
+++ redisinsight/ui/src/utils/redistack.ts
@@ -1,47 +1,51 @@
-import { isArray, map, concat } from 'lodash'
+import { isArray, map, concat, remove, find } from 'lodash'
 import { Instance, RedisDefaultModules } from 'uiSrc/slices/interfaces'
 import { isVersionHigherOrEquals, Nullable } from 'uiSrc/utils'
 
-const REDISTACK_LOW_VERSION = '6.2.6'
-const REDISTACK_HIGH_VERSION = '7.1'
+const REDISTACK_VERSION = '6.2.5'
 
-const REDISTACK_LOW_VERSION_REQUIRE_MODULES: Array<string | Array<string>> = [
+const REDISTACK_REQUIRE_MODULES: Array<string | Array<string>> = [
   RedisDefaultModules.ReJSON,
   RedisDefaultModules.Bloom,
+  [RedisDefaultModules.Search, RedisDefaultModules.SearchLight],
+  RedisDefaultModules.TimeSeries,
+]
+
+const REDISTACK_OPTIONAL_MODULES: Array<string | Array<string>> = [
   RedisDefaultModules.Graph,
-  [RedisDefaultModules.Search, RedisDefaultModules.SearchLight],
-  RedisDefaultModules.TimeSeries,
-]
-
-const REDISTACK_HIGH_VERSION_REQUIRE_MODULES: Array<string | Array<string>> = [
-  RedisDefaultModules.ReJSON,
-  RedisDefaultModules.Bloom,
-  [RedisDefaultModules.Search, RedisDefaultModules.SearchLight],
-  RedisDefaultModules.TimeSeries,
-]
-
-const REDISTACK_HIGH_VERSION_OPTIONAL_MODULES: Array<string[]> = [
   [RedisDefaultModules.RedisGears, RedisDefaultModules.RedisGears2]
 ]
 
-const checkRediStackModules = (modules: any[], required: any[], optional: any[] = []) => {
+const checkRediStackModules = (modules: any[]) => {
   if (!modules?.length) return false
+  const moduleNames = map(modules, 'name').sort()
 
-  if (modules.length === required.length) {
-    return map(modules, 'name')
-      .sort()
-      .every((m, index) => (isArray(required[index])
-        ? (required[index] as Array<string>).some((rm) => rm === m)
-        : required[index] === m))
+  if (modules.length === REDISTACK_REQUIRE_MODULES.length) {
+    return moduleNames
+      .every((m, index) => (isArray(REDISTACK_REQUIRE_MODULES[index])
+        ? (REDISTACK_REQUIRE_MODULES[index] as Array<string>).some((rm) => rm === m)
+        : REDISTACK_REQUIRE_MODULES[index] === m))
   }
 
-  if (modules.length === (required.length + optional.length)) {
-    const rediStackModules = concat(required, optional).sort()
-    return map(modules, 'name')
-      .sort()
-      .every((m, index) => (isArray(rediStackModules[index])
-        ? (rediStackModules[index] as Array<string>).some((rm) => rm === m)
-        : rediStackModules[index] === m))
+  if (modules.length > REDISTACK_REQUIRE_MODULES.length
+    && modules.length <= (REDISTACK_REQUIRE_MODULES.length + REDISTACK_OPTIONAL_MODULES.length)) {
+    let isCustomModule = false
+    const rediStackModules = concat(REDISTACK_REQUIRE_MODULES, REDISTACK_OPTIONAL_MODULES).sort()
+
+    const diff = rediStackModules.reduce((acc: Array<string>, current: string | Array<string>) => {
+      const moduleName = isArray(current)
+        ? (current as Array<string>).find((item) => find(moduleNames, (m) => item === m))
+        : find(moduleNames, (name) => name === current)
+
+      if (moduleName) {
+        remove(acc, (name) => moduleName === name)
+        return acc
+      }
+      isCustomModule = true
+      return acc
+    }, moduleNames)
+
+    return isCustomModule && !diff.length
   }
 
   return false
@@ -49,19 +53,11 @@ const checkRediStackModules = (modules: any[], required: any[], optional: any[] 
 
 const isRediStack = (modules: any[], version?: Nullable<string>): boolean => {
   if (!version) {
-    return checkRediStackModules(modules, REDISTACK_LOW_VERSION_REQUIRE_MODULES)
+    return checkRediStackModules(modules)
   }
 
-  if (isVersionHigherOrEquals(version, REDISTACK_HIGH_VERSION)) {
-    return checkRediStackModules(
-      modules,
-      REDISTACK_HIGH_VERSION_REQUIRE_MODULES,
-      REDISTACK_HIGH_VERSION_OPTIONAL_MODULES
-    )
-  }
-
-  if (isVersionHigherOrEquals(version, REDISTACK_LOW_VERSION)) {
-    return checkRediStackModules(modules, REDISTACK_LOW_VERSION_REQUIRE_MODULES)
+  if (isVersionHigherOrEquals(version, REDISTACK_VERSION)) {
+    return checkRediStackModules(modules)
   }
 
   return false

588d654cc0059086da7b8a3053e555beb824bb87
Artem <artem.horuzhenko@softeq.com>
2023-09-15 11:58:59 +0300

    #RI-4935 fix + speedup tests

--- redisinsight/api/src/modules/cloud/job/transformers/cloud-job-data.transformer.ts
+++ redisinsight/api/src/modules/cloud/job/transformers/cloud-job-data.transformer.ts
@@ -3,13 +3,16 @@ import { TypeHelpOptions } from 'class-transformer';
 import { CreateDatabaseCloudJobDataDto } from 'src/modules/cloud/job/dto/create-database.cloud-job.data.dto';
 import { CloudJobName } from 'src/modules/cloud/job/constants';
 import {
-  CreateSubscriptionAndDatabaseCloudJobDataDto
+  CreateSubscriptionAndDatabaseCloudJobDataDto,
 } from 'src/modules/cloud/job/dto/create-subscription-and-database.cloud-job.data.dto';
+import { ImportDatabaseCloudJobDataDto } from 'src/modules/cloud/job/dto/import-database.cloud-job.data.dto';
 
 export const cloudJobDataTransformer = (data: TypeHelpOptions) => {
   const jobName = get(data?.object, 'name');
 
   switch (jobName) {
+    case CloudJobName.ImportFreeDatabase:
+      return ImportDatabaseCloudJobDataDto;
     case CloudJobName.CreateFreeDatabase:
       return CreateDatabaseCloudJobDataDto;
     case CloudJobName.CreateFreeSubscription:

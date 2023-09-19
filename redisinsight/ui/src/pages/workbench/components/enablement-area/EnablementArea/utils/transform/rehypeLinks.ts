025d829b1b579fbc805083d7d16be8f2a3b389b6
Roman Sergeenko <roman.sergeenko@softeq.com>
2023-09-19 22:02:31 +0200

    #RI-4877 - uncomment deleting title from links

--- redisinsight/ui/src/pages/workbench/components/enablement-area/EnablementArea/utils/transform/rehypeLinks.ts
+++ redisinsight/ui/src/pages/workbench/components/enablement-area/EnablementArea/utils/transform/rehypeLinks.ts
@@ -13,7 +13,7 @@ export const rehypeLinks = (config?: IConfig): (tree: Node) => void => (tree: an
       if (IS_ABSOLUTE_PATH.test(url)) { // External link
         node.properties.rel = ['nofollow', 'noopener', 'noreferrer']
         node.properties.target = '_blank'
-        // delete node.properties.title
+        delete node.properties.title
       }
       if (url.startsWith('#') && config?.history) {
         const { location: currentLocation } = config.history

1153b028e19afa880c44a81829edc2fbfabdb905
mariasergeenko <maria.sergeenko@softeq.com>
2023-11-20 13:53:27 +0100

    firts test

--- tests/e2e/web.runner.ts
+++ tests/e2e/web.runner.ts
@@ -39,7 +39,8 @@ import testcafe from 'testcafe';
                     assertionTimeout: 5000,
                     speed: 1,
                     quarantineMode: { successThreshold: 1, attemptLimit: 3 },
-                    pageRequestTimeout: 8000
+                    pageRequestTimeout: 8000,
+                    disableMultipleWindows: true
                 });
         })
         .then((failedCount) => {

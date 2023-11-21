fe6ad91a7e9b60b78c54b1cf44adb6c602e73034
mariasergeenko <maria.sergeenko@softeq.com>
2023-11-21 14:52:18 +0100

    remove redundant method

--- tests/e2e/helpers/utils.ts
+++ tests/e2e/helpers/utils.ts
@@ -1,5 +1,3 @@
 import { ClientFunction } from 'testcafe';
-import { commonUrl } from './conf';
 
 export const goBackHistory = ClientFunction(() => window.history.back());
-export const openRedisHomePage =  ClientFunction(() => window.open(commonUrl, '_blank'));

fec4e829f6624e6d4791ec4aa17835e3b95fa9e1
Artem <artem.horuzhenko@softeq.com>
2023-11-24 11:38:05 +0200

    #RI-5072 add env key encryption strategy

---
+++ redisinsight/api/src/modules/encryption/exceptions/key-encryption-error.exception.ts
@@ -0,0 +1,13 @@
+import {
+  EncryptionServiceErrorException,
+} from 'src/modules/encryption/exceptions/encryption-service-error.exception';
+
+export class KeyEncryptionErrorException extends EncryptionServiceErrorException {
+  constructor(message = 'Unable to encrypt data') {
+    super({
+      message,
+      name: 'KeyEncryptionError',
+      statusCode: 500,
+    }, 500);
+  }
+}

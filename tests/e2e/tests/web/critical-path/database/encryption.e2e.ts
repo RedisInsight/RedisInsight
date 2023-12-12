3d2cf13d49dfb324cdab2276666be52f9e35eb4c
vlad-dargel <u.darhel@aisnovations.com>
2023-12-12 17:43:43 +0100

    change db to tls

--- tests/e2e/tests/web/critical-path/database/encryption.e2e.ts
+++ tests/e2e/tests/web/critical-path/database/encryption.e2e.ts
@@ -2,7 +2,7 @@ import { rte } from '../../../../helpers/constants';
 import { MyRedisDatabasePage } from '../../../../pageObjects';
 import {
     commonUrl,
-    ossStandaloneConfig
+    ossStandaloneTlsConfig
 } from '../../../../helpers/conf';
 import { DatabaseHelper } from '../../../../helpers/database';
 import { DatabaseAPIRequests } from '../../../../helpers/api/api-database';
@@ -17,7 +17,7 @@ const dbTableParams: DbTableParameters = {
     columnName: 'caCertId',
     rowValue: 'invalid',
     conditionColumnName: 'name',
-    conditionColumnValue: ossStandaloneConfig.databaseName
+    conditionColumnValue: ossStandaloneTlsConfig.databaseName
 };
 
 fixture `Encryption`
@@ -25,21 +25,22 @@ fixture `Encryption`
     .page(commonUrl)
     .beforeEach(async() => {
         await databaseHelper.acceptLicenseTerms();
-        await databaseAPIRequests.addNewStandaloneDatabaseApi(ossStandaloneConfig);
+        await databaseAPIRequests.addNewStandaloneDatabaseApi(ossStandaloneTlsConfig);
         await myRedisDatabasePage.reloadPage();
     })
     .afterEach(async() => {
-        await databaseHelper.deleteDatabase(ossStandaloneConfig.databaseName);
+        await databaseHelper.deleteDatabase(ossStandaloneTlsConfig.databaseName);
     });
 test('Verify that data encrypted using KEY', async t => {
     const decryptionError = 'Unable to decrypt data';
     // Connect to DB
-    await myRedisDatabasePage.clickOnDBByName(ossStandaloneConfig.databaseName);
+    await myRedisDatabasePage.clickOnDBByName(ossStandaloneTlsConfig.databaseName);
+    // Return back to db list page
     await t.click(myRedisDatabasePage.NavigationPanel.myRedisDBButton);
 
     await DatabaseScripts.updateColumnValueInDBTable(dbTableParams);
     // Verify that Encription by KEY applied for connection if RI_ENCRYPTION_KEY variable exists
-    await t.expect(await DatabaseScripts.getColumnValueFromTableInDB({ ...dbTableParams, columnName: 'encryption' })).eql('KEY', 'Encription is not applied by Key');
-    await databaseHelper.clickOnEditDatabaseByName(ossStandaloneConfig.databaseName);
+    await t.expect(await DatabaseScripts.getColumnValueFromTableInDB({ ...dbTableParams, columnName: 'encryption' })).eql('KEY', 'Encription is not applied by RI_ENCRYPTION_KEY');
+    await databaseHelper.clickOnEditDatabaseByName(ossStandaloneTlsConfig.databaseName);
     await t.expect(myRedisDatabasePage.Toast.toastError.textContent).contains(decryptionError, 'Invalid encrypted field is decrypted');
 });

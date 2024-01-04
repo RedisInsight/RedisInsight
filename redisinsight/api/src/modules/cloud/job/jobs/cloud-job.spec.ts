18e7f2756ceb7a51c475720d3f5f1b15f67fee22
irina-abramova <iryna.abramava@gmail.com>
2024-01-04 10:30:59 +0300

    upd unit tests

--- redisinsight/api/src/modules/cloud/job/jobs/cloud-job.spec.ts
+++ redisinsight/api/src/modules/cloud/job/jobs/cloud-job.spec.ts
@@ -3,39 +3,19 @@ import { BadRequestException } from '@nestjs/common';
 import {
   mockDatabaseService,
   mockRedisClientInstance,
-  mockRedisConnectionFactory,
   mockClientMetadata,
   mockStandaloneRedisClient,
   mockDatabaseClientFactory,
 } from 'src/__mocks__';
-import { RedisService } from 'src/modules/redis/redis.service';
 import { DatabaseService } from 'src/modules/database/database.service';
-import { RedisConnectionFactory } from 'src/modules/redis/redis-connection.factory';
 import { DatabaseClientFactory } from 'src/modules/database/providers/database.client.factory';
 
 describe('CloudJob', () => {
-  let redisService;
-  let redisConnectionFactory;
   let consumerInstance;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        {
-          provide: RedisService,
-          useFactory: () => ({
-            getClientInstance: jest.fn(),
-            selectDatabase: jest.fn(),
-            setClientInstance: jest.fn(),
-            isClientConnected: jest.fn(),
-            removeClientInstance: jest.fn(),
-            connectToDatabaseInstance: jest.fn(),
-          }),
-        },
-        {
-          provide: RedisConnectionFactory,
-          useFactory: mockRedisConnectionFactory,
-        },
         {
           provide: DatabaseService,
           useFactory: mockDatabaseService,
@@ -47,11 +27,7 @@ describe('CloudJob', () => {
       ],
     }).compile();
 
-    redisService = await module.get<RedisService>(RedisService);
     consumerInstance = await module.get<DatabaseClientFactory>(DatabaseClientFactory);
-    redisConnectionFactory = await module.get(RedisConnectionFactory);
-
-    redisService.setClientInstance.mockReturnValue(mockRedisClientInstance);
   });
 
   describe('getRedisClient', () => {
@@ -59,7 +35,6 @@ describe('CloudJob', () => {
       consumerInstance.createClient = jest.fn();
     });
     it('create new redis client', async () => {
-      redisService.getClientInstance.mockReturnValue(null);
       consumerInstance.getOrCreateClient.mockResolvedValue(
         mockStandaloneRedisClient,
       );
@@ -70,18 +45,12 @@ describe('CloudJob', () => {
       expect(consumerInstance.getOrCreateClient).toHaveBeenCalled();
     });
     it('existing client has connection', async () => {
-      redisService.getClientInstance.mockReturnValue(mockRedisClientInstance);
-      redisService.isClientConnected.mockReturnValue(true);
-
       const result = await consumerInstance.getOrCreateClient(mockClientMetadata);
 
       expect(result).toEqual(mockStandaloneRedisClient);
       expect(consumerInstance.createClient).not.toHaveBeenCalled();
-      expect(redisService.selectDatabase).not.toHaveBeenCalled();
     });
     it('existing client has no connection', async () => {
-      redisService.getClientInstance.mockReturnValue(mockRedisClientInstance);
-      redisService.isClientConnected.mockReturnValue(false);
       consumerInstance.getOrCreateClient.mockResolvedValue(
         mockStandaloneRedisClient,
       );
@@ -92,9 +61,6 @@ describe('CloudJob', () => {
       expect(consumerInstance.getOrCreateClient).toHaveBeenCalled();
     });
     it('select redis database by number', async () => {
-      redisService.getClientInstance.mockReturnValue(mockRedisClientInstance);
-      redisService.isClientConnected.mockReturnValue(true);
-
       await expect(
         consumerInstance.getOrCreateClient({
           ...mockClientMetadata,
@@ -107,7 +73,6 @@ describe('CloudJob', () => {
       const error = new BadRequestException(
         ' Could not connect to localhost, please check the connection details.',
       );
-      redisService.getClientInstance.mockReturnValue(null);
       consumerInstance.getOrCreateClient.mockRejectedValue(error);
 
       await expect(
@@ -139,12 +104,11 @@ describe('CloudJob', () => {
 
   describe('createNewClient', () => {
     it('create new redis client', async () => {
-      redisConnectionFactory.createRedisConnection.mockResolvedValue(
+      consumerInstance.createClient.mockResolvedValue(
         mockStandaloneRedisClient,
       );
 
       const result = await consumerInstance.createClient(mockRedisClientInstance.clientMetadata);
-
       expect(result).toEqual(mockStandaloneRedisClient);
     });
     it("can't create redis client", async () => {

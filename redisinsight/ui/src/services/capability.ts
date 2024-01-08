c63006550a8d252f92317a120ebcdc9ece69ac55
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2024-01-08 17:47:45 +0100

    * #RI-5302 - Pop-over is still displayed if user opens cloud db after user has already launched it * #RI-5304 - The list of tutorial is not displayed if user does not have a relevant tutorial * #RI-5306 - Telemetry event is duplicated * #RI-5307 - Telemetry event is sent if db in not free and popover is not displayed * #RI-5308 - CAPABILITY_POPOVER_DISPLAYED is sent when 'Compatibility are not available' popover appears

---
+++ redisinsight/ui/src/services/capability.ts
@@ -0,0 +1,13 @@
+import { CapabilityStorageItem } from 'uiSrc/constants/storage'
+import { getCapabilityStorageField, setCapabilityStorageField } from 'uiSrc/services'
+
+const TIME_TO_READ_POPOVER_TEXT = 1_000
+
+export const isShowCapabilityTutorialPopover = (isFree = false) =>
+  !!isFree && !getCapabilityStorageField(CapabilityStorageItem.tutorialPopoverShown)
+
+export const setCapabilityPopoverShown = () => {
+  setTimeout(() => {
+    setCapabilityStorageField(CapabilityStorageItem.tutorialPopoverShown, true)
+  }, TIME_TO_READ_POPOVER_TEXT)
+}

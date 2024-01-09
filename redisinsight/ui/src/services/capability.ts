fdf2579aeb2fb95c578595a81846a43f8e86058c
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2024-01-09 14:55:51 +0100

    #RI-5311 - Popover appears after Triggers and Functions

--- redisinsight/ui/src/services/capability.ts
+++ redisinsight/ui/src/services/capability.ts
@@ -1,10 +1,13 @@
 import { CapabilityStorageItem } from 'uiSrc/constants/storage'
 import { getCapabilityStorageField, setCapabilityStorageField } from 'uiSrc/services'
+import { getTutorialCapability } from 'uiSrc/utils'
 
 const TIME_TO_READ_POPOVER_TEXT = 1_000
 
 export const isShowCapabilityTutorialPopover = (isFree = false) =>
-  !!isFree && !getCapabilityStorageField(CapabilityStorageItem.tutorialPopoverShown)
+  !!isFree
+  && !getCapabilityStorageField(CapabilityStorageItem.tutorialPopoverShown)
+  && getTutorialCapability(getCapabilityStorageField(CapabilityStorageItem.source))?.name
 
 export const setCapabilityPopoverShown = () => {
   setTimeout(() => {

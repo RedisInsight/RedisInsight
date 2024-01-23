5326b5310394626863602acf313010845dc37a47
Amir Allayarov <amir.allayarov@softeq.com>
2024-01-23 15:53:33 +0400

    #RI- 5199 - resolve comments

--- redisinsight/ui/src/templates/rdi-pipeline-page-template/RdiPipelinePageTemplate.tsx
+++ redisinsight/ui/src/templates/rdi-pipeline-page-template/RdiPipelinePageTemplate.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Formik, Form } from 'formik'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
@@ -8,11 +8,17 @@ import Navigation from 'uiSrc/pages/rdi/pipeline/components/navigation'
 import { deployPipelineAction, rdiPipelineSelector } from 'uiSrc/slices/rdi/pipeline'
 
 import { IPipeline } from 'uiSrc/slices/interfaces'
+import { Nullable } from 'uiSrc/utils'
 import styles from './styles.module.scss'
 
 export interface Props {
   children: React.ReactNode
 }
+
+const getInitialValues = (data: Nullable<IPipeline>): IPipeline => ({
+  config: data?.config ?? '',
+  jobs: data?.jobs ?? [],
+})
 
 const RdiPipelinePageTemplate = (props: Props) => {
   const { children } = props
@@ -22,17 +28,20 @@ const RdiPipelinePageTemplate = (props: Props) => {
   const dispatch = useDispatch()
   const { rdiInstanceId } = useParams<{ rdiInstanceId: string }>()
 
+  const [initialFormValues, setInitialFormValues] = useState<IPipeline>(getInitialValues(data))
+
   const onSubmit = (values: IPipeline) => {
     dispatch(deployPipelineAction(rdiInstanceId, values))
   }
 
+  useEffect(() => {
+    setInitialFormValues(getInitialValues(data))
+  }, [data])
+
   // TODO add side panel logs
   return (
     <Formik
-      initialValues={{
-        config: data?.config ?? '',
-        jobs: data?.jobs ?? [],
-      }}
+      initialValues={initialFormValues}
       enableReinitialize
       onSubmit={onSubmit}
     >

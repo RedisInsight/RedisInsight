5a2a6b3d637a4b439994da2879bf28e00be75b28
kyle-marcum <kyle.marcum@redis.com>
2024-01-25 14:58:16 -0600

    RI-5142: Changed Download .zip file on RDI page from using redux to using formik values

--- redisinsight/ui/src/pages/rdi/pipeline/components/download/Download.tsx
+++ redisinsight/ui/src/pages/rdi/pipeline/components/download/Download.tsx
@@ -1,33 +1,37 @@
 import { EuiButtonIcon } from '@elastic/eui'
 import { saveAs } from 'file-saver'
+import { useFormikContext } from 'formik'
 import JSZip from 'jszip'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { IPipeline } from 'uiSrc/slices/interfaces'
 
 import { rdiPipelineSelector } from 'uiSrc/slices/rdi/pipeline'
 import { sendEventTelemetry, TelemetryEvent } from 'uiSrc/telemetry'
 
 const Download = () => {
-  const { loading, data } = useSelector(rdiPipelineSelector)
+  const { loading } = useSelector(rdiPipelineSelector)
 
   const { rdiInstanceId } = useParams<{ rdiInstanceId: string }>()
+
+  const { values } = useFormikContext<IPipeline>()
 
   const handleDownloadClick = async () => {
     sendEventTelemetry({
       event: TelemetryEvent.RDI_PIPELINE_DOWNLOAD_CLICKED,
       eventData: {
         id: rdiInstanceId,
-        jobsNumber: data?.jobs?.length
+        jobsNumber: values?.jobs?.length
       }
     })
 
     // zip config and job contents
     const zip = new JSZip()
-    zip.file('config.yaml', data?.config || '')
+    zip.file('config.yaml', values?.config || '')
 
     const jobs = zip.folder('jobs')
-    data?.jobs.forEach(({ name, value }) => jobs?.file(`${name}.yaml`, value))
+    values?.jobs.forEach(({ name, value }) => jobs?.file(`${name}.yaml`, value))
 
     const content = await zip.generateAsync({ type: 'blob' })
     saveAs(content, 'RDI_pipeline.zip')

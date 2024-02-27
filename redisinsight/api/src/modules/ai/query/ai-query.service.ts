48168495cf2a87e6d3571f1862be97171956ee99
Artem <artem.horuzhenko@softeq.com>
2024-02-27 09:22:52 +0200

    prepare to merge from wrapper

--- redisinsight/api/src/modules/ai/query/ai-query.service.ts
+++ redisinsight/api/src/modules/ai/query/ai-query.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { ClientContext, SessionMetadata } from 'src/common/models';
-import { ConvAiProvider } from 'src/modules/ai/chat/providers/conv-ai.provider';
-import { plainToClass } from 'class-transformer';
-import { AiChat } from 'src/modules/ai/chat/models';
-import { SendAiChatMessageDto } from 'src/modules/ai/chat/dto/send.ai-chat.message.dto';
 import { AiQueryProvider } from 'src/modules/ai/query/providers/ai-query.provider';
 import { SendAiQueryMessageDto } from 'src/modules/ai/query/dto/send.ai-query.message.dto';
 import { DatabaseConnectionService } from 'src/modules/database/database-connection.service';
@@ -27,21 +23,21 @@ export class AiQueryService {
         context: ClientContext.AI,
       });
 
-      const indexes = await client.call('ft._list');
-      console.log('___ indexes', indexes);
+      const indexes = await client.call('ft._list') as string[];
 
       if (!indexes?.length) {
         return context;
       }
 
-      const attributes = await Promise.all(indexes.map(async (index) => {
+      await Promise.all(indexes.map(async (index) => {
         const attrResponse = convertStringsArrayToObject(await client.call('ft.info', index));
-        console.log('___ attrResponse', attrResponse)
 
-        context[index] = attrResponse?.attributes.map((attrs) => convertStringsArrayToObject(attrs));
-      }))
+        context[index] = {
+          index_name: index,
+          attributes: attrResponse?.attributes.map((attrs) => convertStringsArrayToObject(attrs)),
+        };
+      }));
 
-      console.log('___ context', require('util').inspect(context, {depth: null}))
       return context;
     } catch (e) {
       throw e;
@@ -50,15 +46,9 @@ export class AiQueryService {
 
   async generateQuery(sessionMetadata: SessionMetadata, dto: SendAiQueryMessageDto) {
     try {
-      // dto.databaseId = '03762280-b1d2-462e-8074-4f97ffdd7aaa'
-      console.log('___ dto', dto)
       const context = await this.getContext(sessionMetadata, dto);
 
-      console.log('___ context', context)
       const aiResponse = await this.aiQueryProvider.generateQuery(sessionMetadata, dto.content, context);
-
-      console.log('___ aiResponse', aiResponse)
-      let response = 'response';
 
       return aiResponse;
     } catch (e) {

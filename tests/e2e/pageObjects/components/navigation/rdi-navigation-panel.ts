96ceee6d009bf8c9097000217c7f7371890e255f
mariasergeenko <maria.sergeenko@softeq.com>
2024-03-04 15:08:22 +0100

    add missing rdi tests

--- tests/e2e/pageObjects/components/navigation/rdi-navigation-panel.ts
+++ tests/e2e/pageObjects/components/navigation/rdi-navigation-panel.ts
@@ -2,6 +2,7 @@ import { Selector, t } from 'testcafe';
 import { BaseNavigationPanel } from './base-navigation-panel';
 
 export class RdiNavigationPanel extends BaseNavigationPanel{
-    rdiPageButton = Selector('[data-testid=pipeline-management-page-btn]');
+    managementPageButton = Selector('[data-testid=pipeline-management-page-btn]');
+    statusPageButton = Selector('[data-testid=pipeline-status-page-btn]');
 
 }
--- tests/e2e/tests/web/critical-path/rdi/configuration.e2e.ts
+++ tests/e2e/tests/web/critical-path/rdi/configuration.e2e.ts
@@ -1,4 +1,4 @@
-import { t } from 'testcafe';
+import { ClientFunction, t } from 'testcafe';
 import { RdiInstancePage } from '../../../../pageObjects/rdi-instance-page';
 import { AddNewRdiParameters, RdiApiRequests } from '../../../../helpers/api/api-rdi';
 import { commonUrl } from '../../../../helpers/conf';
@@ -6,6 +6,7 @@ import { MyRedisDatabasePage } from '../../../../pageObjects';
 import { RdiInstancesListPage } from '../../../../pageObjects/rdi-instances-list-page';
 import { RedisOverviewPage, TextConnectionSection } from '../../../../helpers/constants';
 import { DatabaseHelper } from '../../../../helpers';
+import { goBackHistory } from '../../../../helpers/utils';
 
 const myRedisDatabasePage = new MyRedisDatabasePage();
 const rdiInstancePage = new RdiInstancePage();
@@ -20,16 +21,18 @@ const rdiInstance: AddNewRdiParameters = {
     password: 'password'
 };
 
+const getPageUrl = ClientFunction(() => window.location.href);
+
 //skip the tests until rdi integration is added
 fixture.skip `Pipeline`
-    .meta({ type: 'critical_path' })
+    .meta({ type: 'critical_path', feature: 'rdi' })
     .page(commonUrl)
     .beforeEach(async() => {
         await databaseHelper.acceptLicenseTerms();
         await rdiApiRequests.addNewRdiApi(rdiInstance);
         await myRedisDatabasePage.setActivePage(RedisOverviewPage.Rdi);
         await rdiInstancesListPage.clickRdiByName(rdiInstance.name);
-        await t.click(rdiInstancePage.configurationTab);
+        await t.click(rdiInstancePage.PipelineManagementPanel.configurationTab);
 
     })
     .afterEach(async() => {
@@ -51,4 +54,12 @@ test('Verify that user can test connection', async() => {
     await t.expect(await rdiInstancePage.TestConnectionPanel.getSectionRowTextByIndex(TextConnectionSection.Failed, 0)).contains('redis', 'endpoint is not empty');
     await t.click(rdiInstancePage.TestConnectionPanel.closeSection);
     await t.expect(rdiInstancePage.TestConnectionPanel.sidePanel.exists).notOk('the panel is not closed');
+});
+test('Verify that link on configuration is valid', async() => {
+
+    const link = 'https://docs.redis.com/latest/rdi/quickstart/';
+    // Verify the link
+    await t.click(rdiInstancePage.configurationLink);
+    await t.expect(getPageUrl()).eql(link, 'Build from homebrew page is not valid');
+    await goBackHistory();
 });

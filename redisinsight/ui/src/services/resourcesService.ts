da1606d5ba05eab11a040f6f7bd46de9f7c6061b
Gnanesh <gnaneshkunal@outlook.com>
2024-03-25 16:42:43 +0530

    check for window.__RIPROXYPATH__ field truthfulness

--- redisinsight/ui/src/services/resourcesService.ts
+++ redisinsight/ui/src/services/resourcesService.ts
@@ -8,7 +8,7 @@ const isWebApp = process.env.RI_APP_TYPE === 'web'
 
 let BASE_URL = !isDevelopment && isWebApp ? '/' : `${baseApiUrl}:${apiPort}/`
 
-if (window.__RIPROXYPATH__ !== '') {
+if (window.__RIPROXYPATH__) {
   BASE_URL = `${BASE_URL}${window.__RIPROXYPATH__}/`
 }
 

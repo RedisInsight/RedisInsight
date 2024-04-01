c108f6bb313742847a3214e947481c9e6257e73b
Roman Sergeenko <roman.sergeenko@softeq.com>
2024-04-01 10:05:24 +0200

    #RI-5546 - refactor

--- redisinsight/ui/src/components/hightlighted-feature/HighlightedFeature.tsx
+++ redisinsight/ui/src/components/hightlighted-feature/HighlightedFeature.tsx
@@ -59,12 +59,7 @@ const HighlightedFeature = (props: Props) => {
     </EuiToolTip>
   )
 
-  if (type === 'dialog') {
-    if (!isHighlight) return null
-    return (<>{children}</>)
-  }
-
-  if (!isHighlight) return (<>{children}</>)
+  if (!isHighlight) return type === 'dialog' ? null : (<>{children}</>)
 
   return (
     <div

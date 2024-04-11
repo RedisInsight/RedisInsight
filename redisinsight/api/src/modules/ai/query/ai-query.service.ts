7493966e0f5b8183963516ec861eb03e1166f19b
arthosofteq <artem.horuzhenko@softeq.com>
2024-04-11 10:18:53 +0300

    update context util

--- redisinsight/api/src/modules/ai/query/ai-query.service.ts
+++ redisinsight/api/src/modules/ai/query/ai-query.service.ts
@@ -3,19 +3,18 @@ import { ClientContext, SessionMetadata } from 'src/common/models';
 import { AiQueryProvider } from 'src/modules/ai/query/providers/ai-query.provider';
 import { SendAiQueryMessageDto } from 'src/modules/ai/query/dto/send.ai-query.message.dto';
 import { AiQueryInternalServerErrorException } from 'src/modules/ai/query/exceptions';
-import { AiContextProvider } from 'src/modules/ai/query/providers/ai-context.provider';
 import { DatabaseClientFactory } from 'src/modules/database/providers/database.client.factory';
+import { getFullDbContext } from 'src/modules/ai/query/utils/context.util';
 
 @Injectable()
 export class AiQueryService {
   constructor(
     private readonly aiQueryProvider: AiQueryProvider,
-    private readonly contextProvider: AiContextProvider,
     private readonly databaseClientFactory: DatabaseClientFactory,
   ) {
   }
 
-  async getContext(sessionMetadata: SessionMetadata, dto: SendAiQueryMessageDto) {
+  async generateQuery(sessionMetadata: SessionMetadata, dto: SendAiQueryMessageDto) {
     try {
       const client = await this.databaseClientFactory.getOrCreateClient({
         sessionMetadata,
@@ -23,15 +22,8 @@ export class AiQueryService {
         context: ClientContext.AI,
       });
 
-      return await this.contextProvider.getDbContext(client, { topValues: true });
-    } catch (e) {
-      return {};
-    }
-  }
-
-  async generateQuery(sessionMetadata: SessionMetadata, dto: SendAiQueryMessageDto) {
-    try {
-      const context = await this.getContext(sessionMetadata, dto);
+      // todo: improve if full db context is not enough
+      const context = await getFullDbContext(client);
 
       return await this.aiQueryProvider.generateQuery(sessionMetadata, dto.content, context);
     } catch (e) {

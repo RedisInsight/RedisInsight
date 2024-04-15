50fd91dc8a88df6882abd889ae6068f2be64e609
kyle-marcum <kyle.marcum@redis.com>
2024-04-15 09:11:25 -0500

    CR-30: Added temporary code for getting/setting session data

--- redisinsight/ui/src/constants/storage.ts
+++ redisinsight/ui/src/constants/storage.ts
@@ -31,6 +31,7 @@ enum BrowserStorageItem {
   OAuthAgreement = 'OAuthAgreement',
   insightsPanel = 'insightsPanel',
   capability = 'capability',
+  session = 'session', // TODO: [USER_CONTEXT] add session to browser storage?
 }
 
 export default BrowserStorageItem

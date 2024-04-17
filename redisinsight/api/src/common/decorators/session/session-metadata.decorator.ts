1905e2db25c1bc4380a0afcd717f81cfef98c4b1
kyle-marcum <112435081+kyle-marcum@users.noreply.github.com>
2024-04-17 14:59:49 -0500

    CR-30: Added temporary code for getting/setting session data (#3280)

--- redisinsight/api/src/common/decorators/session/session-metadata.decorator.ts
+++ redisinsight/api/src/common/decorators/session/session-metadata.decorator.ts
@@ -6,8 +6,15 @@ import { SessionMetadata } from 'src/common/models';
 const validator = new Validator();
 
 export const sessionMetadataFromRequest = (request): SessionMetadata => {
+  // TODO: [USER_CONTEXT] What if request session is undefined?
+  const requestSession = {
+    sessionId: request.headers['session-id'],
+    userId: request.headers['user-id'],
+    uniqueId: request.headers['unique-id'],
+  };
+
   // todo: do not forget to deal with session vs sessionMetadata property
-  const session = plainToClass(SessionMetadata, request.session);
+  const session = plainToClass(SessionMetadata, request.session || requestSession);
 
   const errors = validator.validateSync(session, {
     whitelist: false, // we need this to allow additional fields if needed for flexibility

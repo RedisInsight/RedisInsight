e11a7439ca693d897cb4b7e5beaf8ca87e6c8d61
Nelson Wells <nelson.wells@redis.com>
2024-04-18 16:25:57 -0500

    CR-31: Make statics management module dynamic so that...
    auto-updated can be turned on or off

--- redisinsight/api/src/app.module.ts
+++ redisinsight/api/src/app.module.ts
@@ -84,7 +84,7 @@ const PATH_CONFIG = config.get('dir_path') as Config['dir_path'];
         fallthrough: false,
       },
     }),
-    StaticsManagementModule,
+    StaticsManagementModule.register(),
   ],
   controllers: [],
   providers: [],

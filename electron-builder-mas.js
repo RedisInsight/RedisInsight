469fc1eb5ffb8f6711914abf0449730bcf74a75c
Gnanesh <gnaneshkunal@outlook.com>
2024-04-21 18:23:25 +0530

    Remove explict notarize option

--- electron-builder-mas.js
+++ electron-builder-mas.js
@@ -4,12 +4,6 @@ const config = {
   ...electronBuilder,
   appId: 'com.redis.RedisInsight',
   productName: 'RedisInsight',
-  mac: {
-    ...electronBuilder.mac,
-    notarize: {
-      teamId: process.env.APPLE_TEAM_ID,
-    },
-  },
 };
 
 module.exports = config;

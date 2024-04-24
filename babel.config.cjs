af1fe2645670203e2451174006041658af0080a6
zalenskiSofteq <egor.zalenski@softeq.com>
2024-04-24 12:18:31 +0200

    fix ui tests

---
+++ babel.config.cjs
@@ -0,0 +1,13 @@
+/* eslint global-require: off, import/no-extraneous-dependencies: off */
+const developmentEnv = ['development', 'test']
+module.exports = (api) => {
+  const development = api.env(developmentEnv)
+
+  return {
+    presets: [
+      require('@babel/preset-env'),
+      require('@babel/preset-typescript'),
+      [require('@babel/preset-react'), { development }],
+    ],
+  }
+}

7c74d076f26d1d3cf41a71e40868a220e476cab7
Nelson Wells <nelson.wells@redis.com>
2024-04-26 09:20:02 -0500

    CR-28: code review; move socket session metadata default values from decorator to adapter

--- redisinsight/api/src/modules/auth/session-metadata/adapters/session-metadata.adapter.ts
+++ redisinsight/api/src/modules/auth/session-metadata/adapters/session-metadata.adapter.ts
@@ -1,29 +1,27 @@
 /* eslint-disable no-param-reassign */
-import { INestApplication, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { MessageMappingProperties } from '@nestjs/websockets';
 import { Observable } from 'rxjs';
 import { Socket } from 'socket.io';
+import { DEFAULT_SESSION_ID, DEFAULT_USER_ID } from 'src/common/constants';
+import { SessionMetadata } from 'src/common/models';
 
 @Injectable()
 export class SessionMetadataAdapter extends IoAdapter {
   private logger = new Logger('SessionMetadataAdapter');
-
-  constructor(private app: INestApplication) {
-    super(app);
-  }
 
   async bindMessageHandlers(
     socket: Socket,
     handlers: MessageMappingProperties[],
     transform: (data: any) => Observable<any>,
   ) {
-    // TODO: [USER_CONTEXT]
-    // Assuming that we'll figure out session data from a JWT token
-    // but that is up to discovery later on
-    // eslint-disable-next-line no-param-reassign
-    const jwt = socket.handshake.headers.authorization?.split(' ')[1];
-    socket.data['jwt'] = jwt || null;
+    const sessionMetadata: SessionMetadata = {
+      userId: DEFAULT_USER_ID,
+      sessionId: DEFAULT_SESSION_ID,
+    };
+
+    socket.data['sessionMetadata'] = sessionMetadata;
 
     super.bindMessageHandlers(socket, handlers, transform);
   }
--- redisinsight/api/src/modules/auth/session-metadata/decorators/ws-session-metadata.decorator.ts
+++ redisinsight/api/src/modules/auth/session-metadata/decorators/ws-session-metadata.decorator.ts
@@ -1,16 +1,9 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { SessionMetadata } from 'src/common/models';
-import { DEFAULT_SESSION_ID, DEFAULT_USER_ID } from 'src/common/constants';
 
 export const WSSessionMetadata = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): SessionMetadata => {
     const socket = ctx.switchToWs().getClient();
-    // TODO: [USER_CONTEXT] - decode the JWT and return a session metadata for the user
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { jwt } = socket.data;
-    return {
-      userId: DEFAULT_SESSION_ID,
-      sessionId: DEFAULT_USER_ID,
-    };
+    return socket.data.sessionMetadata;
   },
 );

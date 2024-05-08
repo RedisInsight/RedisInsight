67f4a45c9e33255d04863bbefa56735661af5a2b
vlad-dargel <u.darhel@aisnovations.com>
2024-05-08 15:42:25 +0200

    return headless

--- tests/e2e/web.runner.ts
+++ tests/e2e/web.runner.ts
@@ -11,7 +11,7 @@ import testcafe from 'testcafe';
                         experimentalDecorators: true
                     } })
                 .src((process.env.TEST_FILES || 'tests/web/**/*.e2e.ts').split('\n'))
-                .browsers(['chromium --headless=new --cache --allow-insecure-localhost --ignore-certificate-errors'])
+                .browsers(['chromium:headless --cache --allow-insecure-localhost --ignore-certificate-errors'])
                 .screenshots({
                     path: 'report/screenshots/',
                     takeOnFails: true,

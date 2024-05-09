18d231e0801b21f81846eb7cb7668a2b0bf9bc64
Amir Allayarov <amir.allayarov@softeq.com>
2024-05-09 11:33:11 +0400

    #RI-5660 - update endpoints

--- redisinsight/api/src/modules/rdi/constants/index.ts
+++ redisinsight/api/src/modules/rdi/constants/index.ts
@@ -1,11 +1,11 @@
 export enum RdiUrl {
-  GetSchema = '/api/v1/schemas',
-  GetPipeline = '/api/v1/pipelines',
+  GetSchema = 'api/v1/schemas',
+  GetPipeline = 'api/v1/pipelines',
   GetStrategies = 'api/v1/pipelines/strategies',
   GetTemplate = 'deployments/templates',
-  DryRunJob = '/api/v1/pipelines/jobs/dry-run',
+  DryRunJob = 'api/v1/pipelines/jobs/dry-run',
   Deploy = 'api/v1/pipelines',
   TestConnections = 'api/v1/pipelines/targets/dry-run',
-  GetStatistics = '/api/v1/monitoring/statistics',
+  GetStatistics = 'api/v1/monitoring/statistics',
   GetPipelineStatus = 'api/v1/status',
 }

c3b161424cc245885c18c786835606511865f33f
vlad-dargel <u.darhel@aisnovations.com>
2024-05-15 18:21:59 +0200

    e2e/feature/RI-5154_empty_db_screen

--- tests/e2e/tests/web/critical-path/database/connecting-to-the-db.e2e.ts
+++ tests/e2e/tests/web/critical-path/database/connecting-to-the-db.e2e.ts
@@ -181,3 +181,23 @@ test
         await myRedisDatabasePage.clickOnDBByName(sshDbPass.databaseName);
         await Common.checkURLContainsText('browser');
     });
+test
+    .meta({ rte: rte.none })
+    .before(async() => {
+        await databaseAPIRequests.deleteAllDatabasesApi();
+        await databaseHelper.acceptLicenseTerms();
+    })('Verify that user can see the No databases message on the empty databases list', async t => {
+        const notDatabasesMessage = 'No databases yet, let\'s add one!';
+        // const externalPageLink = 'https://redis.io/try-free/?utm_source=redisinsight&utm_medium=main&utm_campaign=main'
+
+        await t.expect(myRedisDatabasePage.emptyListMessage.withText(notDatabasesMessage).exists).ok('Empty databases list message not displayed');
+
+        await t.click(myRedisDatabasePage.addDbFromEmptyListBtn);
+        await t.expect(myRedisDatabasePage.AddRedisDatabase.testConnectionBtn.exists).ok('Add database form not opened');
+        await t.click(myRedisDatabasePage.AddRedisDatabase.cancelButton);
+
+        // Unskip after updating testcafe with opening links support https://redislabs.atlassian.net/browse/RI-5565
+        // await t.click(myRedisDatabasePage.emptyDbCloudBtn);
+        // await Common.checkURL(externalPageLink);
+        // await goBackHistory();
+    });

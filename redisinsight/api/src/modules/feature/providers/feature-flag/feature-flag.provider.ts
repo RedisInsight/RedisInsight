83aa7f91d53eecf453a400c7964d0a38ed6506b2
ArtemHoruzhenko <artem.horuzhenko@redis.com>
2024-06-06 12:10:52 +0300

    fix typo

--- redisinsight/api/src/modules/feature/providers/feature-flag/feature-flag.provider.ts
+++ redisinsight/api/src/modules/feature/providers/feature-flag/feature-flag.provider.ts
@@ -57,7 +57,7 @@ export class FeatureFlagProvider {
       this.settingsService,
     ));
   }
-a
+
   getStrategy(name: string): FeatureFlagStrategy {
     return this.strategies.get(name) || this.getStrategy('default');
   }

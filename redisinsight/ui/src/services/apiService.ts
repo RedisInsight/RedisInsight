8139f6376ba3a57af46c09cdec836075aa387027
Nelson Wells <nelson.wells@redis.com>
2024-06-13 14:43:22 -0500

    CR-72: Add withCredentials to apiService so that cookies are sent

--- redisinsight/ui/src/services/apiService.ts
+++ redisinsight/ui/src/services/apiService.ts
@@ -24,6 +24,7 @@ export const getBaseUrl = () => (!isDevelopment && isWebApp
 
 const mutableAxiosInstance: AxiosInstance = axios.create({
   baseURL: hostedApiBaseUrl || getBaseUrl(),
+  withCredentials: true,
 })
 
 export const requestInterceptor = (config: InternalAxiosRequestConfig) => {

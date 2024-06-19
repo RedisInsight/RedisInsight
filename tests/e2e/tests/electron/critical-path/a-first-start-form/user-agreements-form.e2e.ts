b158b8fec6aafea94271c00972b9b839d902dd36
vlad-dargel <u.darhel@aisnovations.com>
2024-06-19 17:54:31 +0200

    fixes for desktop

--- tests/e2e/tests/electron/critical-path/a-first-start-form/user-agreements-form.e2e.ts
+++ tests/e2e/tests/electron/critical-path/a-first-start-form/user-agreements-form.e2e.ts
@@ -1,4 +1,4 @@
-import { commonUrl } from '../../../../helpers/conf';
+import { commonUrl, workingDirectory } from '../../../../helpers/conf';
 import { MyRedisDatabasePage } from '../../../../pageObjects';
 import { Common } from '../../../../helpers/common';
 import { rte } from '../../../../helpers/constants';
@@ -7,16 +7,21 @@ import { UserAgreementDialog } from '../../../../pageObjects/dialogs';
 const userAgreementDialog = new UserAgreementDialog();
 const myRedisDatabasePage = new MyRedisDatabasePage();
 
+const dbPath = `${workingDirectory}/redisinsight.db`;
+
 fixture `Agreements Verification`
     .meta({ type: 'critical_path', rte: rte.none })
     .page(commonUrl)
-    .requestHooks(Common.mockSettingsResponse())
-test('Verify that user should accept User Agreements to continue working with the application', async t => {
-    await t.expect(userAgreementDialog.userAgreementsPopup.exists).ok('User Agreements Popup is shown');
-    // Verify that I still has agreements popup & cannot add a database
-    await t.expect(userAgreementDialog.submitButton.hasAttribute('disabled')).ok('Submit button not disabled by default');
-    await t.expect(myRedisDatabasePage.AddRedisDatabase.addDatabaseManually.exists).notOk('User can\'t add a database');
-});
+    .requestHooks(Common.mockSettingsResponse());
+test
+    .before(async() => {
+        await Common.deleteFileFromFolder(dbPath);
+    })('Verify that user should accept User Agreements to continue working with the application', async t => {
+        await t.expect(userAgreementDialog.userAgreementsPopup.exists).ok('User Agreements Popup is shown');
+        // Verify that I still has agreements popup & cannot add a database
+        await t.expect(userAgreementDialog.submitButton.hasAttribute('disabled')).ok('Submit button not disabled by default');
+        await t.expect(myRedisDatabasePage.AddRedisDatabase.addDatabaseManually.exists).notOk('User can\'t add a database');
+    });
 test('Verify that the encryption enabled by default and specific message', async t => {
     const expectedPluginText = 'To avoid automatic execution of malicious code, when adding new Workbench plugins, use files from trusted authors only.';
     // Verify that section with plugin warning is displayed

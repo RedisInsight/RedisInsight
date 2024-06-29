1bd09e8f864efcfa776f064a4c9a6109c08eb34c
Gnanesh <gnaneshkunal@outlook.com>
2024-06-30 00:10:57 +0530

    Disable code transforms

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
+  transform: {},
   testEnvironmentOptions: {
     url: 'http://localhost/'
   },

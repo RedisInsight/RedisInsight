ba71e159544e5c449d56f3827784a569f4d4bfe8
mariasergeenko <maria.sergeenko@softeq.com>
2024-07-29 14:49:42 +0200

    create a base class for the pages

--- tests/e2e/tests/web/regression/search-and-query/search-and-query-tab.ts
+++ tests/e2e/tests/web/regression/search-and-query/search-and-query-tab.ts
@@ -1,8 +1,8 @@
-import { ExploreTabs, KeysInteractionTabs, rte } from '../../../../helpers/constants';
-import { DatabaseHelper } from '../../../../helpers/database';
-import { BrowserPage } from '../../../../pageObjects';
-import { commonUrl, ossStandaloneConfig } from '../../../../helpers/conf';
+import { DatabaseHelper } from '../../../../helpers';
 import { DatabaseAPIRequests } from '../../../../helpers/api/api-database';
+import { BrowserPage } from '../../../../pageObjects';
+import { ExploreTabs, KeysInteractionTabs, rte } from '../../../../helpers/constants';
+import { commonUrl, ossStandaloneConfig } from '../../../../helpers/conf';
 
 const databaseHelper = new DatabaseHelper();
 const databaseAPIRequests = new DatabaseAPIRequests();
@@ -19,9 +19,9 @@ fixture `Autocomplete for entered commands`
         await databaseAPIRequests.deleteStandaloneDatabaseApi(ossStandaloneConfig);
     });
 test('Verify that tutorials can be opened from Workbench', async t => {
-    const workbench =  await browserPage.KeysInteractionPanel.setActiveTab(KeysInteractionTabs.Workbench);
-    await t.click(workbench.getTutorialLinkLocator('redis_use_cases_basic'));
-    await t.expect(workbench.InsightsPanel.sidePanel.exists).ok('Insight panel is not opened');
+    const search =  await browserPage.KeysInteractionPanel.setActiveTab(KeysInteractionTabs.SearchAndQuery);
+    await t.click(search.getTutorialLinkLocator('sq-exact-match'));
+    await t.expect(search.InsightsPanel.sidePanel.exists).ok('Insight panel is not opened');
     const tab = await browserPage.InsightsPanel.setActiveTab(ExploreTabs.Tutorials);
-    await t.expect(tab.preselectArea.textContent).contains('BASIC REDIS USE CASES', 'the tutorial page is incorrect');
+    await t.expect(tab.preselectArea.textContent).contains('EXACT MATCH', 'the tutorial page is incorrect');
 });

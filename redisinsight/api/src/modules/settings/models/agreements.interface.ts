43538e394e89fe52a8b97697ed4ce0f02c45b2cf
ArtemHoruzhenko <artem.horuzhenko@redis.com>
2024-08-19 13:10:51 +0300

    add custom agreements spec file + possibility to disable keytar encryption

--- redisinsight/api/src/modules/settings/models/agreements.interface.ts
+++ redisinsight/api/src/modules/settings/models/agreements.interface.ts
@@ -1,16 +1,24 @@
 export interface IAgreement {
-  defaultValue: boolean;
-  displayInSetting: boolean;
-  required: boolean;
+  defaultValue?: boolean;
+  displayInSetting?: boolean;
+  required?: boolean;
   category?: string;
-  since: string;
-  editable: boolean;
-  disabled: boolean;
-  title: string;
-  label: string;
+  since?: string;
+  editable?: boolean;
+  disabled?: boolean;
+  title?: string;
+  label?: string;
   description?: string;
+  options?: {
+    [key: string]: IAgreement;
+  }
 }
 
 export interface IAgreementSpec {
   [key: string]: IAgreement;
+}
+
+export interface IAgreementSpecFile {
+  version: string;
+  agreements: IAgreementSpec;
 }

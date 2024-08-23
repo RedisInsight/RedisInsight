3d8d242582bad7d99ee258d289fcaca24b092259
Roman Sergeenko <roman.sergeenko@softeq.com>
2024-08-23 09:03:49 +0200

    #RI-5684 - udd tests

--- redisinsight/ui/src/pages/search/components/query/utils.spec.ts
+++ redisinsight/ui/src/pages/search/components/query/utils.spec.ts
@@ -1,7 +1,10 @@
-import { getGeneralSuggestions } from 'uiSrc/pages/search/components/query/utils'
+import { getGeneralSuggestions, isIndexComplete } from 'uiSrc/pages/search/components/query/utils'
 import { MOCKED_SUPPORTED_COMMANDS } from 'uiSrc/pages/search/mocks/mocks'
+import { buildSuggestion } from 'uiSrc/pages/search/utils'
+import { SearchCommand } from 'uiSrc/pages/search/types'
 
-const ftAggregate = MOCKED_SUPPORTED_COMMANDS['FT.SEARCH']
+const ftAggregate = MOCKED_SUPPORTED_COMMANDS['FT.AGGREGATE']
+const ftProfile = MOCKED_SUPPORTED_COMMANDS['FT.PROFILE']
 
 const getCursorContext = () => ({
   currentOffsetArg: undefined,
@@ -25,14 +28,100 @@ const getGeneralSuggestionsTests = [
     },
     result: {
       helpWidgetData: expect.any(Object),
-      suggestions: expect.any(Array)
+      suggestions: ftAggregate.arguments
+        .slice(2)
+        .map((arg) => ({
+          ...buildSuggestion(arg as SearchCommand, {} as any),
+          sortText: expect.any(String),
+          kind: undefined,
+          detail: expect.any(String)
+        }))
+    }
+  },
+  {
+    input: {
+      commandContext: {
+        allArgs: ['FT.AGGREGATE', '""', '""', 'APPLY', 'expression'],
+        command: ftAggregate,
+        commandName: 'FT.AGGREGATE',
+        currentCommandArg: null,
+        prevArgs: ['""', '""', 'APPLY', 'expression']
+      },
+      cursorContext: getCursorContext()
+    },
+    result: {
+      helpWidgetData: expect.any(Object),
+      suggestions: [
+        {
+          label: 'AS',
+          insertText: 'AS ',
+          insertTextRules: 4,
+          range: expect.any(Object),
+          kind: undefined,
+          detail: 'APPLY expression AS name',
+        }
+      ]
+    }
+  },
+  {
+    input: {
+      commandContext: {
+        allArgs: ['FT.PROFILE', '""'],
+        command: ftProfile,
+        commandName: 'FT.PROFILE',
+        currentCommandArg: null,
+        prevArgs: ['""']
+      },
+      cursorContext: getCursorContext()
+    },
+    result: {
+      helpWidgetData: expect.any(Object),
+      suggestions: [
+        {
+          label: 'SEARCH',
+          insertText: 'SEARCH ',
+          insertTextRules: 4,
+          range: expect.any(Object),
+          kind: undefined,
+          detail: '',
+        },
+        {
+          label: 'AGGREGATE',
+          insertText: 'AGGREGATE ',
+          insertTextRules: 4,
+          range: expect.any(Object),
+          kind: undefined,
+          detail: '',
+        }
+      ]
     }
   }
 ]
 
 describe('getGeneralSuggestions', () => {
-  it.each(getGeneralSuggestionsTests)('dawd', ({ input, result }) => {
+  it.each(getGeneralSuggestionsTests)('should properly return suggestions', ({ input, result }) => {
     const testResult = getGeneralSuggestions(input.commandContext, input.cursorContext, [])
+
+    expect(testResult).toEqual(result)
+  })
+})
+
+const isIndexCompleteTests: Array<[string, boolean]> = [
+  ['', false],
+  ['"', false],
+  ['\"\\"', false],
+  ['""', true],
+  ["'", false],
+  ["''", true],
+  ["'index\\'", false],
+  ["'index'", true],
+  ['"index \\\\"', true],
+  ['index', true],
+]
+
+describe('isIndexComplete', () => {
+  it.each(isIndexCompleteTests)('should properly return value for %s', (index, result) => {
+    const testResult = isIndexComplete(index)
 
     expect(testResult).toEqual(result)
   })

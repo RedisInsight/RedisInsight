df69064c3ae3fffa1f665068e850d958425a3091
Roman Sergeenko <roman.sergeenko@softeq.com>
2024-08-23 09:24:50 +0200

    #RI-6031 - remove copilot beta label, remove rdi feature highlighting

--- redisinsight/ui/src/components/home-tabs/HomeTabs.tsx
+++ redisinsight/ui/src/components/home-tabs/HomeTabs.tsx
@@ -1,22 +1,17 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { EuiTab, EuiTabs } from '@elastic/eui'
 import { useHistory, useLocation } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { FeatureFlags, Pages, PageValues } from 'uiSrc/constants'
 import { FeatureFlagComponent } from 'uiSrc/components'
 import { sendEventTelemetry, TelemetryEvent } from 'uiSrc/telemetry'
-import { BUILD_FEATURES } from 'uiSrc/constants/featuresHighlighting'
-import HighlightedFeature from 'uiSrc/components/hightlighted-feature/HighlightedFeature'
-import { getHighlightingFeatures } from 'uiSrc/utils/features'
-import { appFeatureHighlightingSelector, removeFeatureFromHighlighting } from 'uiSrc/slices/app/features'
+import { removeFeatureFromHighlighting } from 'uiSrc/slices/app/features'
 import { tabs } from './constants'
 
 import styles from './styles.module.scss'
 
 const HomeTabs = () => {
   const [activeTab, setActiveTab] = useState('')
-  const { features } = useSelector(appFeatureHighlightingSelector)
-  const { [FeatureFlags.rdi]: rdiHighlighting } = getHighlightingFeatures(features)
 
   const history = useHistory()
   const { pathname } = useLocation()
@@ -52,13 +47,7 @@ const HomeTabs = () => {
           className={styles.tab}
           data-testid={`home-tab-${id}`}
         >
-          <HighlightedFeature
-            isHighlight={rdiHighlighting}
-            /* highlighting will remove in next release, do not need cover multiple tabs */
-            {...(BUILD_FEATURES.redisDataIntegration || {})}
-          >
-            {title}
-          </HighlightedFeature>
+          {title}
         </EuiTab>
       </FeatureFlagComponent>
     ) : (

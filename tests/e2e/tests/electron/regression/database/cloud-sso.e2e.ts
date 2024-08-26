27e31e4636eabe0ae21ea9f20c35608c6d773071
vlad-dargel <u.darhel@aisnovations.com>
2024-08-26 20:06:08 +0200

    fix 6

--- tests/e2e/tests/electron/regression/database/cloud-sso.e2e.ts
+++ tests/e2e/tests/electron/regression/database/cloud-sso.e2e.ts
@@ -44,6 +44,7 @@ test('Verify that user can see SSO feature if it is enabled in feature config', 
     await t.expect(myRedisDatabasePage.AuthorizationDialog.ssoAuth.exists).ok('SSO auth button not displayed in Sigh in modal');
     // await t.click(myRedisDatabasePage.Modal.closeModalButton);
     await t.pressKey('esc');
+    await t.wait(1000);
 
     await t.click(myRedisDatabasePage.AddRedisDatabase.addDatabaseButton);
     await t.click(myRedisDatabasePage.AddRedisDatabase.addAutoDiscoverDatabase);

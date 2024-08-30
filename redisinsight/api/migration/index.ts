2714e8027e24a50152c332c077ad5764ecb7f7bf
zalenskiSofteq <egor.zalenski@softeq.com>
2024-08-30 16:51:49 +0200

    #RI-6032 - [BE] Unify ai chats

--- redisinsight/api/migration/index.ts
+++ redisinsight/api/migration/index.ts
@@ -42,6 +42,7 @@ import { AiHistory1713515657364 } from './1713515657364-ai-history';
 import { AiHistorySteps1714501203616 } from './1714501203616-ai-history-steps';
 import { Rdi1716370509836 } from './1716370509836-rdi';
 import { AiHistory1718260230164 } from './1718260230164-ai-history';
+import { AiUnifyHistory1725029452055 } from './1725029452055-ai-unify-history';
 
 export default [
   initialMigration1614164490968,
@@ -88,4 +89,5 @@ export default [
   AiHistorySteps1714501203616,
   Rdi1716370509836,
   AiHistory1718260230164,
+  AiUnifyHistory1725029452055,
 ];

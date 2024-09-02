af7c499c0d5e26cbe89bacea3f646ced3a1b1697
vlad-dargel <u.darhel@aisnovations.com>
2024-09-02 14:06:24 +0200

    e2e-fixes-regression

--- tests/e2e/tests/web/regression/settings/settings.e2e.ts
+++ tests/e2e/tests/web/regression/settings/settings.e2e.ts
@@ -1,11 +1,11 @@
 import { rte } from '../../../../helpers/constants';
 import { BrowserPage, MemoryEfficiencyPage, SettingsPage, WorkbenchPage } from '../../../../pageObjects';
 import {
-    commonUrl, ossClusterConfig
+    commonUrl, ossClusterConfig,
 } from '../../../../helpers/conf';
 import { DatabaseAPIRequests } from '../../../../helpers/api/api-database';
+import { Common, DatabaseHelper } from '../../../../helpers';
 import { deleteAllKeysFromDB } from '../../../../helpers/keys';
-import { DatabaseHelper } from '../../../../helpers';
 
 const browserPage = new BrowserPage();
 const databaseAPIRequests = new DatabaseAPIRequests();
@@ -13,6 +13,8 @@ const workbenchPage = new WorkbenchPage();
 const settingsPage = new SettingsPage();
 const memoryEfficiencyPage = new MemoryEfficiencyPage();
 const databaseHelper = new DatabaseHelper();
+
+let keyName = Common.generateWord(20);
 
 fixture `DataTime format setting`
     .meta({
@@ -23,14 +25,19 @@ fixture `DataTime format setting`
     .beforeEach(async() => {
         await databaseHelper.acceptLicenseTermsAndAddOSSClusterDatabase(ossClusterConfig);
     })
-    .afterEach(async() => {
+    .afterEach(async t => {
         await deleteAllKeysFromDB(ossClusterConfig.ossClusterPort, ossClusterConfig.ossClusterPort);
+        await t.click(workbenchPage.NavigationPanel.settingsButton);
+        await t.click(settingsPage.accordionAppearance);
+        await t.click(settingsPage.commonRadioButton);
+        await settingsPage.selectDataFormatDropdown('HH\\:mm\\:ss');
         await databaseAPIRequests.deleteAllDatabasesApi();
+        await settingsPage.selectTimeZoneDropdown('local');
     });
 test('Verify that user can select date time format', async t => {
-    const defaultDateRegExp = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d\.\d{3} \d{1,2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}$/;
+    const defaultDateRegExp = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d \d{1,2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}$/;
     const selectedDateReqExp = /^(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.\d{4} ([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
-    const keyName = 'DateTimeTestKey';
+    keyName = `DateTimeTestKey-${Common.generateWord(5)}`;
     const hashField = '1724674140';
 
     const selectorForOption = 'dd\\.MM\\.yyyy';
@@ -40,7 +47,7 @@ test('Verify that user can select date time format', async t => {
     await browserPage.addHashKey(keyName, '100000', hashField, hashField);
     await browserPage.openKeyDetails(keyName);
     await browserPage.selectFormatter('DateTime');
-    await t.expect(defaultDateRegExp.test(await browserPage.getHashKeyValue())).ok('date is not in default format HH:mm:ss.SSS d MMM yyyy');
+    await t.expect(defaultDateRegExp.test(await browserPage.getHashKeyValue())).ok('date is not in default format HH:mm:ss d MMM yyyy');
 
     await t.click(workbenchPage.NavigationPanel.settingsButton);
     await t.click(settingsPage.accordionAppearance);
@@ -62,12 +69,11 @@ test('Verify that user can select date time format', async t => {
     await workbenchPage.sendCommandInWorkbench('info');
     const dateTime = await workbenchPage.queryCardContainer.nth(0).find(workbenchPage.cssCommandExecutionDateTime).textContent;
     await t.expect(selectedDateReqExp.test(dateTime)).ok('date is not in default format HH:mm:ss.SSS d MMM yyyy');
-
 });
 
 test('Verify that user can set custom date time format', async t => {
     const enteredFormat = 'MMM dd yyyy/ HH.mm.ss';
-    const enteredDateReqExp = /^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ([0-2]\d|3[01]) \d{4}\/ ([01]\d|2[0-3])\.[0-5]\d\.[0-5]\d$/;
+    const enteredDateReqExp = /^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0[1-9]|[12]\d|3[01]) \d{4}\/ ([01]\d|2[0-3])\.[0-5]\d\.[0-5]\d$/;
 
     await t.click(workbenchPage.NavigationPanel.settingsButton);
     await t.click(settingsPage.accordionAppearance);
@@ -79,5 +85,5 @@ test('Verify that user can set custom date time format', async t => {
     await t.click(settingsPage.NavigationPanel.analysisPageButton);
     await t.click(memoryEfficiencyPage.databaseAnalysisTab);
     await t.click(memoryEfficiencyPage.newReportBtn);
-    await t.expect(enteredDateReqExp.test(await memoryEfficiencyPage.selectedReport.textContent)).ok(`custom format is not working ${enteredFormat}`);
+    await t.expect(enteredDateReqExp.test((await memoryEfficiencyPage.selectedReport.textContent).trim())).ok(`custom format is not working ${enteredFormat}`);
 });

254bb9b0a88adc348b7f14c1b6f56e7a18f56ff3
seth-riedel <seth.riedel@redis.com>
2024-10-16 17:16:16 -0500

    CR-172: Hide "Change database" button in tutorials by feature flag

--- redisinsight/ui/src/components/markdown/CodeButtonBlock/components/RunConfirmationPopover.spec.tsx
+++ redisinsight/ui/src/components/markdown/CodeButtonBlock/components/RunConfirmationPopover.spec.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import { render, screen } from 'uiSrc/utils/test-utils'
-
+import { cloneDeep, set } from 'lodash'
+import { initialStateDefault, mockStore, render, screen } from 'uiSrc/utils/test-utils'
+import { FeatureFlags } from 'uiSrc/constants'
 import RunConfirmationPopover from './RunConfirmationPopover'
 
 describe('RunConfirmationPopover', () => {
   it('should render', () => {
     expect(render(<RunConfirmationPopover onApply={jest.fn()} />)).toBeTruthy()
+  })
+
+  it('should hide "Change Database" button when feature flag is off', async () => {
+    const initialStoreState = set(
+      cloneDeep(initialStateDefault),
+      `app.features.featureFlags.features.${FeatureFlags.envDependent}`,
+      { flag: true }
+    )
+
+    render(<RunConfirmationPopover onApply={jest.fn()} />, {
+      store: mockStore(initialStoreState)
+    })
+    expect(screen.queryByRole('button', { name: 'Change Database' })).toBeInTheDocument()
+  })
+
+  it('should hide "Change Database" button when feature flag is on', async () => {
+    const initialStoreState = set(
+      cloneDeep(initialStateDefault),
+      `app.features.featureFlags.features.${FeatureFlags.envDependent}`,
+      { flag: false }
+    )
+
+    render(<RunConfirmationPopover onApply={jest.fn()} />, {
+      store: mockStore(initialStoreState)
+    })
+    expect(screen.queryByRole('button', { name: 'Change Database' })).not.toBeInTheDocument()
   })
 })

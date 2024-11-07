aca82b55275e7eb8ed0bc03ac1cad4d7d6b14e9f
vlad-dargel <u.darhel@aisnovations.com>
2024-11-07 16:47:24 +0100

    return back long string

--- tests/e2e/helpers/scripts/browser-scripts.ts
+++ tests/e2e/helpers/scripts/browser-scripts.ts
@@ -63,7 +63,7 @@ export function openChromeWindow(): void {
         console.log('Opening Chrome on Linux...');
         try {
             console.log("Attempting to open Chrome with execSync");
-            const output = execSync(`google-chrome ${chromeArgs}`, { stdio: 'inherit', timeout: 10000 });
+            const output = execSync(`google-chrome --remote-debugging-port=9223 --disable-gpu --disable-search-engine-choice-screen --disable-dev-shm-usage --disable-software-rasterizer --enable-logging --disable-extensions --no-default-browser-check --disable-default-apps --disable-domain-reliability --disable-web-security --remote-allow-origins=* --disable-popup-blocking about:blank`, { stdio: 'inherit', timeout: 10000 });
             console.log("Chrome opened successfully with execSync:", output);
         } catch (error) {
             console.error("Error occurred in execSync:", error);

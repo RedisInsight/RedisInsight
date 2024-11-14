de13502189209ec71d56c33a1c2101b385d02e61
Egor Zalenski <egor.zalenski@softeq.com>
2024-11-14 11:33:14 +0100

    revert e2e appimage sso sh script

--- .github/e2e/test.app-image.sh
+++ .github/e2e/test.app-image.sh
@@ -1,54 +1,25 @@
 #!/bin/bash
 set -e
 
-yarn --cwd tests/e2e install
+ARCH=${ARCH:-x86_64}
+WORKING_DIRECTORY=$(pwd)
+SOURCE_APP=${SOURCE_APP:-"Redis-Insight-linux-$ARCH.AppImage"}
+RI_APP_FOLDER_NAME="Redis-Insight-linux"
+TAR_NAME="Redis-Insight-app-linux.$ARCH.tar.gz"
+TMP_FOLDER="/tmp/Redis-Insight-app-$ARCH"
 
-# Create the ri-test directory if it doesn't exist
-mkdir -p ri-test
+rm -rf "$TMP_FOLDER"
 
-# Extract the AppImage
-chmod +x ./release/*.AppImage
-./release/*.AppImage --appimage-extract
+mkdir -p "$WORKING_DIRECTORY/release/redisstack"
+mkdir -p "$TMP_FOLDER"
 
-# Move contents of squashfs-root to ri-test and remove squashfs-root folder
-mv squashfs-root/* ri-test/
-rm -rf squashfs-root
+cp "./release/$SOURCE_APP" "$TMP_FOLDER"
+cd "$TMP_FOLDER" || exit 1
 
-# Export custom XDG_DATA_DIRS with ri-test
-export XDG_DATA_DIRS="$(pwd)/ri-test:$XDG_DATA_DIRS"
+./"$SOURCE_APP" --appimage-extract
+mv squashfs-root "$RI_APP_FOLDER_NAME"
 
-# create folder before tests run to prevent permissions issue
-mkdir -p tests/e2e/remote
-mkdir -p tests/e2e/rdi
+tar -czvf "$TAR_NAME" "$RI_APP_FOLDER_NAME"
 
-# Create a custom .desktop file for RedisInsight
-cat > ri-test/redisinsight.desktop <<EOL
-[Desktop Entry]
-Version=1.0
-Name=RedisInsight
-Exec=$(pwd)/ri-test/redisinsight %u
-Icon=$(pwd)/ri-test/resources/app.asar/img/icon.png
-Type=Application
-Terminal=false
-MimeType=x-scheme-handler/redisinsight;
-EOL
-
-# Copy the .desktop file to the local applications directory
-cp ri-test/redisinsight.desktop "$HOME/.local/share/applications"
-
-# Update the desktop database with custom directory
-update-desktop-database "$(pwd)/ri-test/"
-
-# Register the RedisInsight deeplink protocol
-xdg-mime default redisinsight.desktop x-scheme-handler/redisinsight
-
-# Run rte
-docker compose -f tests/e2e/rte.docker-compose.yml build
-docker compose -f tests/e2e/rte.docker-compose.yml up --force-recreate -d -V
-./tests/e2e/wait-for-redis.sh localhost 12000 && \
-
-# Run tests
-COMMON_URL=$(pwd)/ri-test/resources/app.asar/dist/renderer/index.html \
-ELECTRON_PATH=$(pwd)/ri-test/redisinsight \
-RI_SOCKETS_CORS=true \
-yarn --cwd tests/e2e dotenv -e .desktop.env yarn --cwd tests/e2e test:desktop:ci
+cp "$TAR_NAME" "$WORKING_DIRECTORY/release/redisstack/"
+cd "$WORKING_DIRECTORY" || exit 1

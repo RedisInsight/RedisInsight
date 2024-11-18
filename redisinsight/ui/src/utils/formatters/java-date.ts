397d46664ce2a00b7eec3373d7157be65dfcfa8d
Gnanesh <gnaneshkunal@outlook.com>
2024-11-18 15:02:40 +0530

    Move the constants to the class level and use them in the code

--- redisinsight/ui/src/utils/formatters/java-date.ts
+++ redisinsight/ui/src/utils/formatters/java-date.ts
@@ -7,6 +7,12 @@ export default class JavaDate implements JavaSerializable {
   // The serial version UID followed for 'java.util.Date'
   static readonly SerialVersionUID = '7523967970034938905'
 
+  // The maximum value for a Java long
+  readonly JAVA_MAX_LONG = 9223372036854775807n // 2^63 - 1
+
+  // The maximum value for a two's complement long
+  readonly TWO_COMPLEMENT_MAX_LONG = 18446744073709551616n // 2^64
+
   time: bigint = 0n
 
   readObject(stream: ObjectInputStream): void {
@@ -17,9 +23,9 @@ export default class JavaDate implements JavaSerializable {
     let timeValue: number
 
     // Handle two's complement conversion for negative numbers
-    if (this.time > 9223372036854775807n) {
+    if (this.time > this.JAVA_MAX_LONG) {
       // If the number is larger than MAX_LONG, it's a negative number in two's complement
-      timeValue = Number(this.time - 18446744073709551616n)
+      timeValue = Number(this.time - this.TWO_COMPLEMENT_MAX_LONG)
     } else {
       timeValue = Number(this.time)
     }

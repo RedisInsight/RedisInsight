2ef88a4c2daae6936dd90b02e3f55079221b3692
zalenskiSofteq <egor.zalenski@softeq.com>
2024-11-22 12:02:50 +0100

    #RI-6325 - Fix e2e tests for SSH

--- tests/e2e/web.runner.ci.ts
+++ tests/e2e/web.runner.ci.ts
@@ -34,7 +34,7 @@ import testcafe from 'testcafe';
                 ])
                 .run({
                     skipJsErrors: true,
-                    browserInitTimeout: 120000,
+                    browserInitTimeout: 240000,
                     selectorTimeout: 5000,
                     assertionTimeout: 5000,
                     speed: 1,

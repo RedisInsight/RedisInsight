d3ce57dd5ee5e5c427017e0a07609df7253cd5c3
zalenskiSofteq <egor.zalenski@softeq.com>
2024-11-22 15:55:17 +0100

    test

--- tests/e2e/web.runner.ci.ts
+++ tests/e2e/web.runner.ci.ts
@@ -11,7 +11,7 @@ import testcafe from 'testcafe';
                         experimentalDecorators: true
                     } })
                 .src((process.env.TEST_FILES || 'tests/web/**/*.e2e.ts').split('\n'))
-                .browsers(['chrome --cache --allow-insecure-localhost --disable-search-engine-choice-screen --ignore-certificate-errors'])
+                .browsers(['chrome:headless'])
                 // .browsers(['chromium:headless --cache --allow-insecure-localhost --disable-search-engine-choice-screen --ignore-certificate-errors'])
                 .screenshots({
                     path: 'report/screenshots/',

f9fc4d9abfab0e1c5abdc85903d1bf100d54203d
pd-redis <petar.dzhambazov@redis.com>
2024-12-11 17:22:13 +0200

    test log

--- redisinsight/ui/src/App.tsx
+++ redisinsight/ui/src/App.tsx
@@ -40,6 +40,7 @@ const AppWrapper = ({ children }: { children?: ReactElement[] }) => (
 )
 const App = ({ children }: { children?: ReactElement[] }) => {
   const { loading: serverLoading } = useSelector(appInfoSelector)
+  console.log('Test');
   return (
     <div className="main-container">
       <ThemeComponent />

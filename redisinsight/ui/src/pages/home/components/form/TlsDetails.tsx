45e18f794a1b90f2794d0e15a7714b068bfa6365
Pavel Angelov <pavel.angelov@redis.com>
2024-12-18 15:27:30 +0200

    RI-6114: prevent long text overlapping in SuperSelect

--- redisinsight/ui/src/pages/home/components/form/TlsDetails.tsx
+++ redisinsight/ui/src/pages/home/components/form/TlsDetails.tsx
@@ -322,6 +322,7 @@ const TlsDetails = (props: Props) => {
                 <EuiSuperSelect
                   placeholder="Select certificate"
                   valueOfSelected={formik.values.selectedTlsClientCertId}
+                  className={styles.selectWithLongTextSupport}
                   options={optionsCertsClient}
                   onChange={(value) => {
                     formik.setFieldValue('selectedTlsClientCertId', value)

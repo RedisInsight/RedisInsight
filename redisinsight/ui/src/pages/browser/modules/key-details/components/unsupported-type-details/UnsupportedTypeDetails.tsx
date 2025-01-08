e43b7d39f67e285d6282da39aa90ecf1d5e424a7
Pavel Angelov <pavel.angelov@redis.com>
2025-01-08 11:15:47 +0200

    update message

--- redisinsight/ui/src/pages/browser/modules/key-details/components/unsupported-type-details/UnsupportedTypeDetails.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/unsupported-type-details/UnsupportedTypeDetails.tsx
@@ -9,21 +9,19 @@ const UnsupportedTypeDetails = () => (
     <EuiFlexGroup alignItems="center" justifyContent="center">
       <EuiFlexItem className={styles.textWrapper}>
         <EuiTitle>
-          <h4>This data type is coming soon!</h4>
+          <h4>This key type is not currently supported.</h4>
         </EuiTitle>
         <EuiText size="s">
-          We are constantly working to launch support for more data types.
-          If you have any ideas or suggestions, please
-          {' '}
+          See{' '}
           <a
-            href={EXTERNAL_LINKS.githubIssues}
+            href={EXTERNAL_LINKS.githubRepo}
             className={styles.link}
             target="_blank"
             rel="noreferrer"
           >
-            contact us
+            our repository
           </a>
-          .
+          {' '}for the list of supported key types.
         </EuiText>
       </EuiFlexItem>
     </EuiFlexGroup>

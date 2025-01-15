5050425d21a4704cfc1d07cff3f3285094835fb9
pd-redis <petar.dzhambazov@redis.com>
2025-01-15 12:02:23 +0200

    - add one-to-one relations to database  - remove DatabaseSetting dto

--- redisinsight/api/src/modules/database-settings/models/database-settings.ts
+++ redisinsight/api/src/modules/database-settings/models/database-settings.ts
@@ -1,14 +1,23 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class DatabaseSettingsData {
+  @Expose()
+  key: any;
+}
 
 export class DatabaseSettings {
+  @ApiProperty({
+    description: 'Database id',
+    type: String,
+    default: '123',
+  })
   @Expose()
   databaseId: string;
 
+  @ApiProperty({
+    description: 'Applied settings by user, by database',
+  })
   @Expose()
   data: DatabaseSettingsData;
-}
-
-class DatabaseSettingsData {
-  @Expose()
-  key: string;
 }

ffbaf89d8e33a5dc351c4e0b38148798f111d2a8
Pavel Angelov <pavel.angelov@redis.com>
2025-01-18 23:21:35 +0200

    some more updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,91 +9,78 @@ import eslintPluginImportX from 'eslint-plugin-import-x';
 import tsParser from '@typescript-eslint/parser';
 import path from 'path';
 
-const ignored = {
-  ignores: [
-    '**/logs',
-    '**/*.log',
-    '**/pids',
-    '**/*.pid',
-    '**/*.seed',
-    '**/lib-cov',
-    '**/coverage',
-    '**/.grunt',
-    '**/.lock-wscript',
-    'build/Release',
-    '**/.eslintcache',
-    '**/node_modules',
-    '**/.DS_Store',
-    '**/release',
-    '**/*.main.prod.js',
-    '**/*.renderer.prod.js',
-    '**/scripts',
-    '**/configs',
-    '**/dist',
-    '**/dll',
-    '**/*.main.js',
-    '**/.idea',
-    '**/npm-debug.log.*',
-    '**/__snapshots__',
-    '**/package.json',
-    '**/.travis.yml',
-    '**/*.css.d.ts',
-    '**/*.sass.d.ts',
-    '**/*.scss.d.ts',
-
-    'report',
-    '**/__mocks__',
-    'tests/e2e',
-
-    'redisinsight/ui/src/packages/redisgraph',
-    'redisinsight/ui/src/packages/redistimeseries-app',
-    'redisinsight/api/test/test-runs/coverage',
-  ],
-};
+const ignored = [
+  '**/logs',
+  '**/*.log',
+  '**/pids',
+  '**/*.pid',
+  '**/*.seed',
+  '**/lib-cov',
+  '**/coverage',
+  '**/.grunt',
+  '**/.lock-wscript',
+  'build/Release',
+  '**/.eslintcache',
+  '**/node_modules',
+  '**/.DS_Store',
+  '**/release',
+  '**/*.main.prod.js',
+  '**/*.renderer.prod.js',
+  '**/scripts',
+  '**/configs',
+  '**/dist',
+  '**/dll',
+  '**/*.main.js',
+  '**/.idea',
+  '**/npm-debug.log.*',
+  '**/__snapshots__',
+  '**/package.json',
+  '**/.travis.yml',
+  '**/*.css.d.ts',
+  '**/*.sass.d.ts',
+  '**/*.scss.d.ts',
+  'report',
+  '**/__mocks__',
+  'tests/e2e',
+  'redisinsight/ui/src/packages/redisgraph',
+  'redisinsight/ui/src/packages/redistimeseries-app',
+  'redisinsight/api/test/test-runs/coverage',
+];
 
 const rules = {
-  rules: {
-    '@stylistic/indent': ['error', 2],
-    '@stylistic/no-multiple-empty-lines': ['error'],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    'max-len': [
-      'error',
-      {
-        ignoreComments: true,
-        ignoreStrings: true,
-        ignoreRegExpLiterals: true,
-        code: 120,
-      },
-    ],
-    'class-methods-use-this': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    semi: ['error', 'never'],
-    'object-curly-newline': 'off',
-    'import/prefer-default-export': 'off',
-    'comma-dangle': 'off',
-    'no-unused-vars': 'error',
-    'no-undef': 'error',
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          'external',
-          'builtin',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        pathGroups: [
-          { pattern: 'desktopSrc/**', group: 'internal', position: 'after' },
-          { pattern: 'uiSrc/**', group: 'internal', position: 'after' },
-          { pattern: 'apiSrc/**', group: 'internal', position: 'after' },
-        ],
-        warnOnUnassignedImports: true,
-        pathGroupsExcludedImportTypes: ['builtin'],
-      },
-    ],
-  },
+  '@stylistic/indent': ['error', 2],
+  '@stylistic/no-multiple-empty-lines': ['error'],
+  quotes: ['error', 'single', { avoidEscape: true }],
+  'max-len': [
+    'error',
+    {
+      ignoreComments: true,
+      ignoreStrings: true,
+      ignoreRegExpLiterals: true,
+      code: 120,
+    },
+  ],
+  'class-methods-use-this': 'off',
+  'import/no-extraneous-dependencies': 'off',
+  semi: ['error', 'never'],
+  'object-curly-newline': 'off',
+  'import/prefer-default-export': 'off',
+  'comma-dangle': 'off',
+  'no-unused-vars': 'error',
+  'no-undef': 'error',
+  'import/order': [
+    'warn',
+    {
+      groups: ['external', 'builtin', 'internal', 'parent', 'sibling', 'index'],
+      pathGroups: [
+        { pattern: 'desktopSrc/**', group: 'internal', position: 'after' },
+        { pattern: 'uiSrc/**', group: 'internal', position: 'after' },
+        { pattern: 'apiSrc/**', group: 'internal', position: 'after' },
+      ],
+      warnOnUnassignedImports: true,
+      pathGroupsExcludedImportTypes: ['builtin'],
+    },
+  ],
 };
 
 const resolverSettings = {
@@ -106,35 +93,14 @@ const resolverSettings = {
   },
 };
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  ignored,
-  eslintConfigPrettier,
-  importPlugin.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
-  {
-    plugins: { '@stylistic': stylistic },
-    ...resolverSettings,
-  },
-  rules,
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parser: tsParser,
-      globals: { ...globals.browser, ...globals.node },
-    },
-  },
-  pluginJs.configs.recommended,
-  {
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
+const languageOptions = {
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+  parser: tsParser,
+  globals: { ...globals.browser, ...globals.node },
+};
+
+const fileSpecificConfigs = [
   {
     files: ['**/*.ts', '**/*.tsx'],
     rules: { semi: 'off' },
@@ -143,6 +109,26 @@ export default [
     files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
     rules: { semi: ['error', 'always'] },
   },
+];
+
+export default [
+  { ignores: ignored },
+  eslintConfigPrettier,
+  importPlugin.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.typescript,
+  { plugins: { '@stylistic': stylistic }, ...resolverSettings },
+  { rules },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions },
+  pluginJs.configs.recommended,
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  ...fileSpecificConfigs,
   ...configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],

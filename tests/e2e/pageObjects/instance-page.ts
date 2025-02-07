7df790266ddfa0ba94ec72d8796324acf602639f
seth-riedel <seth.riedel@redis.com>
2025-02-07 07:45:46 -0600

    RI-6427: Add additional selectors (#4361)

--- tests/e2e/pageObjects/instance-page.ts
+++ tests/e2e/pageObjects/instance-page.ts
@@ -1,13 +1,16 @@
 import { BasePage } from './base-page';
-import { Profiler, Cli, CommandHelper } from './components/bottom-panel';
+import { Profiler, Cli, CommandHelper, SurveyLink } from './components/bottom-panel';
 import { OverviewPanel } from './components/overview-panel';
 import { InsightsPanel } from './components/insights-panel';
 import { MonacoEditor } from './components/monaco-editor';
 import { NavigationHeader } from './components/navigation/navigation-header';
+import { DatabaseOverview } from './components/top-panel';
 export class InstancePage extends BasePage {
     Profiler = new Profiler();
     Cli = new Cli();
     CommandHelper = new CommandHelper();
+    DatabaseOverview = new DatabaseOverview();
+    SurveyLink = new SurveyLink();
     OverviewPanel = new OverviewPanel();
     InsightsPanel = new InsightsPanel();
     MonacoEditor = new MonacoEditor();

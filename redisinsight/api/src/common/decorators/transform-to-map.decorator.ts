aa3c1651f0481543a787240ec3d8f5d40a77cc3a
Pavel Angelov <pavel.angelov@redis.com>
2025-02-24 15:46:52 +0200

    support missing data

--- redisinsight/api/src/common/decorators/transform-to-map.decorator.ts
+++ redisinsight/api/src/common/decorators/transform-to-map.decorator.ts
@@ -1,10 +1,15 @@
 import { Transform, plainToClass } from 'class-transformer';
 
 export function TransformToMap<T>(cls: new (...args: any[]) => T) {
-  return Transform(({ value }) => Object.fromEntries(
-    Object.entries(value || {}).map(([key, val]) => [
-      key,
-      plainToClass(cls, val),
-    ]),
-  ));
+  return Transform(({ value }) => {
+    if (value === undefined || value === null) {
+      return value;
+    }
+    return Object.fromEntries(
+      Object.entries(value).map(([key, val]) => [
+        key,
+        plainToClass(cls, val),
+      ]),
+    );
+  });
 }

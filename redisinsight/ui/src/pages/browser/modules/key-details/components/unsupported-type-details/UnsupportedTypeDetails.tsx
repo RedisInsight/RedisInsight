9fba2f95ae17caa758081f1a0fd148261ac4be93
Dijana Antovska <dijana.antovska@redis.com>
2025-02-27 10:53:27 +0200

    wrap unsupported type details with text details wrapper to reuse functionality

--- redisinsight/ui/src/pages/browser/modules/key-details/components/unsupported-type-details/UnsupportedTypeDetails.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/unsupported-type-details/UnsupportedTypeDetails.tsx
@@ -1,47 +1,29 @@
 import React from 'react'
-import { EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiText, EuiTitle, EuiToolTip } from '@elastic/eui'
+import { EuiText, EuiTitle } from '@elastic/eui'
 
 import { EXTERNAL_LINKS } from 'uiSrc/constants/links'
+import TextDetailsWrapper from '../text-details-wrapper/TextDetailsWrapper';
 
 import styles from './styles.module.scss'
 
-const UnsupportedTypeDetails = ({ onClose }: 
-      { onClose: () => void }) => (
-  <div className={styles.container} data-testid="unsupported-type-details">
-    <EuiToolTip
-        content="Close"
-        position="left"
-        anchorClassName={styles.closeRightPanel}
-    >
-      <EuiButtonIcon
-          iconType="cross"
-          color="primary"
-          aria-label="Close key"
-          className={styles.closeBtn}
-          onClick={() => onClose()}
-          data-testid="unsuported-type-close-key-btn"
-      />
-    </EuiToolTip>
-    <EuiFlexGroup alignItems="center" justifyContent="center">
-      <EuiFlexItem className={styles.textWrapper}>
-        <EuiTitle>
-          <h4>This key type is not currently supported.</h4>
-        </EuiTitle>
-        <EuiText size="s">
-          See{' '}
-          <a
-            href={EXTERNAL_LINKS.githubRepo}
-            className={styles.link}
-            target="_blank"
-            rel="noreferrer"
-          >
-            our repository
-          </a>
-          {' '}for the list of supported key types.
-        </EuiText>
-      </EuiFlexItem>
-    </EuiFlexGroup>
-  </div>
+const UnsupportedTypeDetails = ({ onClose }: { onClose: () => void }) => (
+  <TextDetailsWrapper onClose={onClose}>
+    <EuiTitle>
+      <h4>This key type is not currently supported.</h4>
+    </EuiTitle>
+    <EuiText size="s">
+      See{' '}
+      <a
+        href={EXTERNAL_LINKS.githubRepo}
+        className={styles.link}
+        target="_blank"
+        rel="noreferrer"
+      >
+        our repository
+      </a>
+      {' '}for the list of supported key types.
+    </EuiText>
+  </TextDetailsWrapper>
 )
 
 export default UnsupportedTypeDetails

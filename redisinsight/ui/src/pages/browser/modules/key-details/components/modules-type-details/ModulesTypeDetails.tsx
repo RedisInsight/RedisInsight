ffc00eb847a17b32c0c0f17221d9b85065aff9c5
Dijana Antovska <dijana.antovska@redis.com>
2025-02-27 10:52:56 +0200

    wrap module type details with text details wrapper to reuse functionality

--- redisinsight/ui/src/pages/browser/modules/key-details/components/modules-type-details/ModulesTypeDetails.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/modules-type-details/ModulesTypeDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiText, EuiTitle, EuiToolTip } from '@elastic/eui'
+import { EuiText, EuiTitle } from '@elastic/eui'
 import { useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
@@ -7,6 +7,7 @@ import { Pages } from 'uiSrc/constants'
 import { connectedInstanceSelector } from 'uiSrc/slices/instances/instances'
 
 import styles from './styles.module.scss'
+import TextDetailsWrapper from '../text-details-wrapper/TextDetailsWrapper'
 
 const ModulesTypeDetails = (
   { moduleName = 'unsupported', 
@@ -25,44 +26,26 @@ const ModulesTypeDetails = (
   }
 
   return (
-    <div className={styles.container} data-testid="modules-type-details">
-      <EuiToolTip
-        content="Close"
-        position="left"
-        anchorClassName={styles.closeRightPanel}
-      >
-        <EuiButtonIcon
-          iconType="cross"
-          color="primary"
-          aria-label="Close key"
-          className={styles.closeBtn}
-          onClick={() => onClose()}
-          data-testid="module-type-close-key-btn"
-        />
-      </EuiToolTip>
-      <EuiFlexGroup alignItems="center" justifyContent="center" >
-        <EuiFlexItem className={styles.textWrapper}>
-          <EuiTitle>
-            <h4>{`This is a ${moduleName} key.`}</h4>
-          </EuiTitle>
-          <EuiText size="s">
-            {'Use Redis commands in the '}
-            <a
-              tabIndex={0}
-              onClick={handleGoWorkbenchPage}
-              className={styles.link}
-              data-testid="internal-workbench-link"
-              onKeyDown={() => ({})}
-              role="link"
-              rel="noreferrer"
-            >
-              Workbench
-            </a>
-            {' tool to view the value.'}
-          </EuiText>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    </div>
+    <TextDetailsWrapper onClose={onClose}>
+      <EuiTitle>
+        <h4>{`This is a ${moduleName} key.`}</h4>
+      </EuiTitle>
+      <EuiText size="s">
+        {'Use Redis commands in the '}
+        <a
+          tabIndex={0}
+          onClick={handleGoWorkbenchPage}
+          className={styles.link}
+          data-testid="internal-workbench-link"
+          onKeyDown={() => ({})}
+          role="link"
+          rel="noreferrer"
+        >
+          Workbench
+        </a>
+        {' tool to view the value.'}
+      </EuiText>
+    </TextDetailsWrapper>
   )
 }
 

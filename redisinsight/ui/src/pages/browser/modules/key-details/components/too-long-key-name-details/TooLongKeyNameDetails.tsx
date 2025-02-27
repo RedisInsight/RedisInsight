8220f4c806c2050d0fe360fd8f3916894fdc0411
Dijana Antovska <dijana.antovska@redis.com>
2025-02-27 10:53:14 +0200

    wrap too long key name details with text details wrapper to reuse functionality

--- redisinsight/ui/src/pages/browser/modules/key-details/components/too-long-key-name-details/TooLongKeyNameDetails.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/too-long-key-name-details/TooLongKeyNameDetails.tsx
@@ -1,35 +1,17 @@
 import React from 'react'
-import { EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiText, EuiTitle, EuiToolTip } from '@elastic/eui'
+import { EuiText, EuiTitle } from '@elastic/eui'
 
-import styles from './styles.module.scss'
+import TextDetailsWrapper from '../text-details-wrapper/TextDetailsWrapper'
 
 const TooLongKeyNameDetails = ({ onClose }: { onClose: () => void}) => (
-  <div className={styles.container} data-testid="too-long-key-name-details">
-    <EuiToolTip
-      content="Close"
-      position="left"
-      anchorClassName={styles.closeRightPanel}
-    >
-      <EuiButtonIcon
-        iconType="cross"
-        color="primary"
-        aria-label="Close key"
-        className={styles.closeBtn}
-        onClick={() => onClose()}
-        data-testid="too-long-key-name-close-key-btn"
-      />
-    </EuiToolTip>
-    <EuiFlexGroup alignItems="center" justifyContent="center">
-      <EuiFlexItem className={styles.textWrapper}>
-        <EuiTitle>
-          <h4>The key name is too long</h4>
-        </EuiTitle>
-        <EuiText size="s">
-          Details cannot be displayed.
-        </EuiText>
-      </EuiFlexItem>
-    </EuiFlexGroup>
-  </div>
+  <TextDetailsWrapper onClose={onClose}>
+    <EuiTitle>
+      <h4>The key name is too long</h4>
+    </EuiTitle>
+    <EuiText size="s">
+      Details cannot be displayed.
+    </EuiText>
+  </TextDetailsWrapper>
 )
 
 export default TooLongKeyNameDetails

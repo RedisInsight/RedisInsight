6a3efd21979e1c40f87b4d23d611e036a9ed7b53
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-03-10 16:08:17 +0200

    RI-6928 fix sessionId calculation to match schema requirements

--- redisinsight/api/src/modules/analytics/analytics.service.spec.ts
+++ redisinsight/api/src/modules/analytics/analytics.service.spec.ts
@@ -14,6 +14,7 @@ import { AppType } from 'src/modules/server/models/server';
 import { SettingsService } from 'src/modules/settings/settings.service';
 import { ConstantsProvider } from 'src/modules/constants/providers/constants.provider';
 import { LocalConstantsProvider } from 'src/modules/constants/providers/local.constants.provider';
+import { convertAnyStringToPositiveInteger } from 'src/utils';
 import {
   AnalyticsService,
   Telemetry,
@@ -178,7 +179,7 @@ describe('AnalyticsService', () => {
 
       expect(service.getSessionId()).toEqual(-1);
       expect(service.getSessionId(mockSessionMetadata))
-        .toEqual(Number(BigInt(`0x${Buffer.from(mockSessionMetadata.sessionId).toString('hex')}`)));
+        .toEqual(convertAnyStringToPositiveInteger(mockSessionMetadata.sessionId));
     });
   });
 

c1b75e8b45e064e9b6030ec949b04907ec6c67d3
ttsvtkov <tsvetan.tsvetkov@redis.com>
2025-03-16 12:19:44 +0200

    full test passing db delete call failing

--- tests/playwright/tests/example.spec.ts
+++ tests/playwright/tests/example.spec.ts
@@ -27,8 +27,16 @@ test('basic test', async ({ basePage}) => {
 
     console.log('WE ARE IN TEST')
     await browserPage.addHashKey(keyName)
-    // checks that the notification is displayed
+
+    // checks that the notification is displayed (should be in a different test)
     await expect(await basePage.getByText('Key has been added')).toBeVisible()
+
+
+    // Check that new key is displayed in the list
+    await browserPage.searchByKeyName(keyName)
+    const isKeyIsDisplayedInTheList = await browserPage.isKeyIsDisplayedInTheList(keyName)
+    await expect(isKeyIsDisplayedInTheList).toBe(true)
+
 
 
 })

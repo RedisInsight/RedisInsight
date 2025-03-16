a5003312158148e796b4373965ed2a313291cb0a
ttsvtkov <tsvetan.tsvetkov@redis.com>
2025-03-16 16:55:40 +0200

    simplify http client

--- tests/playwright/helpers/api/api-databases.ts
+++ tests/playwright/helpers/api/api-databases.ts
@@ -1,215 +1,22 @@
-//
-// import { faker } from '@faker-js/faker'
-// import { HttpClient } from './http-client'
-// import { AddNewDatabaseParameters } from '../../pageObjects/dialogs/add-redis-database-dialog'
-// import { ResourcePath } from '../constants'
-//
-// export class DatabaseAPIRequests {
-//     private httpClient: HttpClient
-//
-//     constructor(baseURL: string) {
-//         this.httpClient = new HttpClient(baseURL)
-//     }
-//
-//     /**
-//      * Add a new standalone database using the HTTP client.
-//      * @param databaseParameters The database parameters
-//      * @param isCloud Whether the database is cloud-based
-//      */
-//     async addNewStandaloneDatabaseApi(
-//         databaseParameters: AddNewDatabaseParameters,
-//         isCloud = false
-//     ): Promise<void> {
-//         const uniqueId = faker.string.alphanumeric({ length: 10 })
-//         const uniqueIdNumber = faker.number.int({ min: 1, max: 1000 })
-//
-//         const requestBody: any = {
-//             name: databaseParameters.databaseName,
-//             host: databaseParameters.host,
-//             port: Number(databaseParameters.port),
-//             username: databaseParameters.databaseUsername,
-//             password: databaseParameters.databasePassword,
-//         }
-//
-//         if (databaseParameters.caCert && databaseParameters.clientCert) {
-//             requestBody.tls = true
-//             requestBody.verifyServerCert = false
-//             requestBody.caCert = {
-//                 name: `ca-${uniqueId}`,
-//                 certificate: databaseParameters.caCert.certificate,
-//             }
-//             requestBody.clientCert = {
-//                 name: `client-${uniqueId}`,
-//                 certificate: databaseParameters.clientCert.certificate,
-//                 key: databaseParameters.clientCert.key,
-//             }
-//         }
-//
-//         if (isCloud) {
-//             requestBody.cloudDetails = {
-//                 cloudId: uniqueIdNumber,
-//                 subscriptionType: 'fixed',
-//                 planMemoryLimit: 100,
-//                 memoryLimitMeasurementUnit: 'mb',
-//                 free: true,
-//             }
-//         }
-//
-//         const response = await this.httpClient.post<any>(ResourcePath.Databases, requestBody)
-//
-//         if (!response || Object.keys(response).length === 0) {
-//             throw new Error('The response body is empty')
-//         }
-//
-//         if (response.name !== databaseParameters.databaseName) {
-//             throw new Error(
-//                 `Database name mismatch. Expected: ${databaseParameters.databaseName}, Received: ${response.name}`
-//             )
-//         }
-//
-//         console.log('Database created successfully:', response)
-//     }
-// }
 import { faker } from '@faker-js/faker'
-import axios from 'axios'
-import { Chance } from 'chance'
-import { HttpClient } from './http-client'
-import { AddNewDatabaseParameters, OSSClusterParameters,  ClusterNodes, databaseParameters } from '../../types'
-import { ResourcePath } from '../constants'
+import {AxiosInstance} from 'axios'
 
-// export class DatabaseAPIRequests {
-//     private httpClient: HttpClient
-//
-//     constructor(baseURL: string) {
-//         this.httpClient = new HttpClient(baseURL)
-//     }
-//
-//     async addNewStandaloneDatabaseApi(databaseParameters: AddNewDatabaseParameters, isCloud = false): Promise<void> {
-//         const uniqueId = faker.string.alphanumeric({ length: 10 })
-//         const uniqueIdNumber = faker.number.int({ min: 1, max: 1000 })
-//
-//         const requestBody: any = {
-//             name: databaseParameters.databaseName,
-//             host: databaseParameters.host,
-//             port: Number(databaseParameters.port),
-//             username: databaseParameters.databaseUsername,
-//             password: databaseParameters.databasePassword,
-//         }
-//
-//         if (databaseParameters.caCert && databaseParameters.clientCert) {
-//             requestBody.tls = true
-//             requestBody.verifyServerCert = false
-//             requestBody.caCert = {
-//                 name: `ca-${uniqueId}`,
-//                 certificate: databaseParameters.caCert.certificate,
-//             }
-//             requestBody.clientCert = {
-//                 name: `client-${uniqueId}`,
-//                 certificate: databaseParameters.clientCert.certificate,
-//                 key: databaseParameters.clientCert.key,
-//             }
-//         }
-//
-//         if (isCloud) {
-//             requestBody.cloudDetails = {
-//                 cloudId: uniqueIdNumber,
-//                 subscriptionType: 'fixed',
-//                 planMemoryLimit: 100,
-//                 memoryLimitMeasurementUnit: 'mb',
-//                 free: true,
-//             }
-//         }
-//
-//         const response = await this.httpClient.post<any>(ResourcePath.Databases, requestBody)
-//
-//         if (!response || Object.keys(response).length === 0) {
-//             throw new Error('The response body is empty')
-//         }
-//
-//         if (response.name !== databaseParameters.databaseName) {
-//             throw new Error(`Database name mismatch. Expected: ${databaseParameters.databaseName}, Received: ${response.name}`)
-//         }
-//     }
-//
-//     async getAllDatabases(): Promise<databaseParameters[]> {
-//         return this.httpClient.get<databaseParameters[]>(ResourcePath.Databases)
-//     }
-//
-//     async getDatabaseIdByName(databaseName?: string): Promise<string> {
-//         if (!databaseName) throw new Error('Error: Missing databaseName')
-//
-//         const allDatabases = await this.getAllDatabases()
-//         const db = allDatabases.find(db => db.name === databaseName)
-//         if (!db) throw new Error(`Database ${databaseName} not found`)
-//
-//         return db.id
-//     }
-//
-//     async deleteStandaloneDatabaseApi(databaseParameters: AddNewDatabaseParameters): Promise<void> {
-//         const databaseId = await this.getDatabaseIdByName(databaseParameters.databaseName)
-//         if (!databaseId) throw new Error('Error: Missing databaseId')
-//
-//         await this.httpClient.delete(ResourcePath.Databases, { ids: [`${databaseId}`] })
-//     }
-//
-//     async deleteAllDatabasesApi(): Promise<void> {
-//         const allDatabases = await this.getAllDatabases()
-//         const databaseIds = allDatabases.map(db => db.id)
-//
-//         if (databaseIds.length) {
-//             await this.httpClient.delete(ResourcePath.Databases, { ids: databaseIds })
-//             await this.deleteAllDatabasesByConnectionTypeApi('SENTINEL')
-//         }
-//     }
-//
-//     async deleteAllDatabasesByConnectionTypeApi(connectionType: string): Promise<void> {
-//         const allDatabases = await this.getAllDatabases()
-//         const databaseIds = allDatabases.filter(db => db.connectionType === connectionType).map(db => db.id)
-//
-//         if (databaseIds.length) {
-//             await this.httpClient.delete(ResourcePath.Databases, { ids: databaseIds })
-//         }
-//     }
-//
-//     async getClusterNodesApi(databaseParameters: OSSClusterParameters): Promise<string[]> {
-//         const databaseId = await this.getDatabaseIdByName(databaseParameters.ossClusterDatabaseName)
-//         const resourcePath = `${ResourcePath.Databases}/${databaseId}${ResourcePath.ClusterDetails}`
-//         const response = await this.httpClient.get<{ nodes: ClusterNodes[] }>(resourcePath)
-//
-//         return response.nodes.map(node => `${node.host}:${node.port}`)
-//     }
-// }
+import { HttpClient } from './http-client'
+import { AddNewDatabaseParameters,  databaseParameters } from '../../types'
+import { ResourcePath } from '../constants'
 
 import { asyncFilter, doAsyncStuff } from '../async-helper'
 
-const apiUrl = 'https://localhost:5540/api'
-
-
-
-const apiClient = axios.create({
-    baseURL: apiUrl, // Change to your API base URL
-    httpsAgent: new (require('https').Agent)({
-        rejectUnauthorized: false // Allows self-signed/invalid SSL certs
-    })
-})
-
-// Enable logging if DEBUG=1 is set
-if (process.env.DEBUG === '1') {
-    apiClient.interceptors.request.use(request => {
-        console.log('Starting Request', request)
-        return request
-    })
-
-    apiClient.interceptors.response.use(response => {
-        console.log('Response:', response)
-        return response
-    }, error => {
-        console.error('Error Response:', error.response)
-        return Promise.reject(error)
-    })
-}
 
 export class DatabaseAPIRequests {
+
+    private apiClient: AxiosInstance
+
+    constructor(apiUrl: string) {
+        this.apiClient = new HttpClient(apiUrl).getClient()
+
+    }
+
     async addNewStandaloneDatabaseApi(databaseParameters: AddNewDatabaseParameters, isCloud = false): Promise<void> {
         const uniqueId = faker.string.alphanumeric({ length: 10 })
         const uniqueIdNumber = faker.number.int({ min: 1, max: 1000 })
@@ -245,12 +52,12 @@ export class DatabaseAPIRequests {
             }
         }
 
-        const response = await apiClient.post(ResourcePath.Databases, requestBody)
+        const response = await this.apiClient.post(ResourcePath.Databases, requestBody)
         if (response.status !== 201) throw new Error(`Database creation failed for ${databaseParameters.databaseName}`)
     }
 
     async getAllDatabases(): Promise<string[]> {
-        const response = await apiClient.get(ResourcePath.Databases)
+        const response = await this.apiClient.get(ResourcePath.Databases)
         if (response.status !== 200) throw new Error('Failed to retrieve databases')
         return response.data
     }
@@ -273,7 +80,7 @@ export class DatabaseAPIRequests {
         if (!databaseId) throw new Error('Error: Missing databaseId')
 
         const requestBody = { ids: [databaseId] }
-        const response = await apiClient.delete(ResourcePath.Databases, { data: requestBody })
+        const response = await this.apiClient.delete(ResourcePath.Databases, { data: requestBody })
         if (response.status !== 200) throw new Error(`Failed to delete database ${databaseParameters.databaseName}`)
     }
 }
--- tests/playwright/helpers/api/http-client.ts
+++ tests/playwright/helpers/api/http-client.ts
@@ -1,66 +1,39 @@
-import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios'
-import https from 'https'
-
-// Logging interceptor for requests
-const requestLogger = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-    console.log('Request:', config.method?.toUpperCase(), config.url)
-    console.log('Headers:', config.headers)
-    console.log('Body:', config.data)
-    return config
-}
-
-// Logging interceptor for responses
-const responseLogger = (response: AxiosResponse): AxiosResponse => {
-    console.log('Response:', response.status, response.data)
-    return response
-}
+import axios, { AxiosInstance } from 'axios'
 
 export class HttpClient {
-    private instance: AxiosInstance
 
-    constructor(baseURL: string) {
-        // if (!baseURL) {
-        //     throw new Error('No URL must be passed in')
-        // }
-        this.instance = axios.create({
-            baseURL,
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false // allow self-signed or invalid certificates
+    private apiUrl: string
+    private apiClient: AxiosInstance
+    private static instance: HttpClient
+
+    constructor(apiUrl: string) {
+        this.apiUrl = apiUrl
+        this.apiClient = axios.create({
+            baseURL: this.apiUrl, // Change to your API base URL
+            httpsAgent: new (require('https').Agent)({
+                rejectUnauthorized: false // Allows self-signed/invalid SSL certs
             })
         })
 
-        // Setup interceptors for logging
-        this.instance.interceptors.request.use(
-            requestLogger,
-            error => Promise.reject(error)
-        )
-        this.instance.interceptors.response.use(
-            responseLogger,
-            error => Promise.reject(error)
-        )
+        // Enable logging if DEBUG=1 is set
+        if (process.env.DEBUG === '1') {
+            this.apiClient.interceptors.request.use(request => {
+                console.log('Starting Request', request)
+                return request
+            })
+            this.apiClient.interceptors.response.use(response => {
+                console.log('Response:', response)
+                return response
+            }, error => {
+                console.error('Error Response:', error.response)
+                return Promise.reject(error)
+            })
+        }
     }
 
-    async get<T>(url: string, headers?: Record<string, string>): Promise<T> {
-        const response = await this.instance.get<T>(url, { headers })
-        if (!response.data) throw new Error('GET request returned empty response')
-        return response.data
+    getClient(): AxiosInstance {
+        return this.apiClient
     }
 
-    async post<T>(url: string, body: unknown, headers?: Record<string, string>): Promise<T> {
-        const response = await this.instance.post<T>(url, body, { headers })
-        if (!response.data) throw new Error('Empty response body on POST request')
-        return response.data
-    }
 
-    async put<T>(url: string, body: unknown, headers?: Record<string, string>): Promise<T> {
-        const response = await this.instance.put<T>(url, body, { headers })
-        if (!response.data) throw new Error('Empty response body on PUT request')
-        return response.data
-    }
-
-    async delete<T>(url: string, headers?: Record<string, string>): Promise<T> {
-        const response = await this.instance.delete<T>(url, { headers })
-        if (!response.data) throw new Error('Empty response body from DELETE request')
-        return response.data
-    }
 }

15e8ff32d8c0a3b03d10db8c18ab159471416590
Dijana Antovska <dijana.antovska@redis.com>
2025-03-26 17:45:25 +0200

    apply the actual column show / hide; looks ugly when few columns are left, to be fixed

--- redisinsight/ui/src/pages/home/components/database-list-component/DatabasesListWrapper.tsx
+++ redisinsight/ui/src/pages/home/components/database-list-component/DatabasesListWrapper.tsx
@@ -39,6 +39,7 @@ import {
   checkConnectToInstanceAction,
   deleteInstancesAction,
   exportInstancesAction,
+  instancesSelector,
   setConnectedInstanceId,
 } from 'uiSrc/slices/instances/instances'
 import {
@@ -94,6 +95,7 @@ const DatabasesListWrapper = (props: Props) => {
     [FeatureFlags.cloudSso]: cloudSsoFeature,
     [FeatureFlags.databaseManagement]: databaseManagementFeature,
   } = useSelector(appFeatureFlagsFeaturesSelector)
+  const { shownColumns } = useSelector(instancesSelector)
 
   const [width, setWidth] = useState(0)
   const [, forceRerender] = useState({})
@@ -287,7 +289,7 @@ const DatabasesListWrapper = (props: Props) => {
     />
   )
 
-  const columns: EuiTableFieldDataColumnType<Instance>[] = [
+  const initialColumns: EuiTableFieldDataColumnType<Instance>[] = [
     {
       field: 'name',
       className: 'column_name',
@@ -513,6 +515,15 @@ const DatabasesListWrapper = (props: Props) => {
       },
     },
   ]
+
+  const [columns, setColumns] = useState<
+    EuiTableFieldDataColumnType<Instance>[]
+  >(initialColumns.filter((c) => shownColumns.includes(c.field)))
+
+  useEffect(() => {
+    const filteredColumns = initialColumns.filter((column) => shownColumns.includes(column.field))
+    setColumns([...filteredColumns])
+  }, [shownColumns])
 
   const onTableChange = ({ sort, page }: Criteria<Instance>) => {
     // calls also with page changing

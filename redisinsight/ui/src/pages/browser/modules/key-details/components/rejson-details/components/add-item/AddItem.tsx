c11f1fcfec361645d93ea92688d302d8e79c9811
Pavel Angelov <pavel.angelov@redis.com>
2025-04-07 16:19:48 +0300

    hook ui

--- redisinsight/ui/src/pages/browser/modules/key-details/components/rejson-details/components/add-item/AddItem.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/rejson-details/components/add-item/AddItem.tsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import cx from 'classnames'
 import {
+  EuiButton,
   EuiButtonIcon,
   EuiFieldText,
   EuiFlexItem,
   EuiFocusTrap,
   EuiForm,
   EuiOutsideClickDetector,
+  EuiPopover,
+  EuiText,
   EuiWindowEvent,
-  keys
+  keys,
 } from '@elastic/eui'
+import { useSelector } from 'react-redux'
 
+import { rejsonDataSelector } from 'uiSrc/slices/browser/rejson'
+import { checkExistingPath } from 'uiSrc/utils/rejson'
 import FieldMessage from 'uiSrc/components/field-message/FieldMessage'
 import { Nullable } from 'uiSrc/utils'
-import { isValidJSON, isValidKey } from '../../utils'
+import { isValidJSON, isValidKey, parseJsonData, wrapPath } from '../../utils'
 import { JSONErrors } from '../../constants'
 
 import styles from '../../styles.module.scss'
@@ -21,17 +27,17 @@ import styles from '../../styles.module.scss'
 export interface Props {
   isPair: boolean
   onCancel: () => void
-  onSubmit: (pair: { key?: string, value: string }) => void
+  onSubmit: (pair: { key?: string; value: string }) => void
   leftPadding?: number
 }
 
 const AddItem = (props: Props) => {
-  const {
-    isPair,
-    leftPadding = 0,
-    onCancel,
-    onSubmit
-  } = props
+  const { isPair, leftPadding = 0, onCancel, onSubmit } = props
+  const [isConfirmationVisible, setIsConfirmationVisible] =
+    useState<boolean>(false)
+
+  const { data } = useSelector(rejsonDataSelector)
+  const jsonContent = parseJsonData(data)
 
   const [key, setKey] = useState<string>('')
   const [value, setValue] = useState<string>('')
@@ -61,12 +67,29 @@ const AddItem = (props: Props) => {
       return
     }
 
+    const wrappedKey = wrapPath(key) || ''
+    if (checkExistingPath(wrappedKey, jsonContent)) {
+      setIsConfirmationVisible(true)
+      return
+    }
+
+    onSubmit({ key, value })
+  }
+
+  const confirmApply = () => {
     onSubmit({ key, value })
   }
 
   return (
-    <div className={styles.row} style={{ display: 'flex', flexDirection: 'row', paddingLeft: `${leftPadding}em` }}>
-      <EuiOutsideClickDetector onOutsideClick={onCancel}>
+    <div
+      className={styles.row}
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        paddingLeft: `${leftPadding}em`,
+      }}
+    >
+      <EuiOutsideClickDetector onOutsideClick={() => {}}>
         <div>
           <EuiWindowEvent event="keydown" handler={(e) => handleOnEsc(e)} />
           <EuiFocusTrap>
@@ -84,7 +107,9 @@ const AddItem = (props: Props) => {
                     value={key}
                     isInvalid={!!error}
                     placeholder="Enter JSON key"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setKey(e.target.value)}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setKey(e.target.value)
+                    }
                     data-testid="json-key"
                   />
                 </EuiFlexItem>
@@ -95,7 +120,9 @@ const AddItem = (props: Props) => {
                   value={value}
                   placeholder="Enter JSON value"
                   isInvalid={!!error}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setValue(e.target.value)
+                  }
                   data-testid="json-value"
                 />
               </EuiFlexItem>
@@ -108,15 +135,63 @@ const AddItem = (props: Props) => {
                   className={styles.declineBtn}
                   onClick={() => onCancel?.()}
                 />
-                <EuiButtonIcon
-                  iconSize="m"
-                  iconType="check"
-                  color="primary"
-                  type="submit"
-                  aria-label="Apply"
-                  className={styles.applyBtn}
-                  data-testid="apply-btn"
-                />
+
+                <EuiPopover
+                  ownFocus
+                  initialFocus={false}
+                  anchorPosition="downRight"
+                  isOpen={isConfirmationVisible}
+                  closePopover={() => {}}
+                  panelClassName={cx(
+                    'euiToolTip',
+                    'popoverLikeTooltip',
+                    styles.popover,
+                  )}
+                  button={
+                    <EuiButtonIcon
+                      iconSize="m"
+                      iconType="check"
+                      color="primary"
+                      type="submit"
+                      aria-label="Apply"
+                      className={styles.applyBtn}
+                      data-testid="apply-btn"
+                    />
+                  }
+                >
+                  <EuiText size="m" style={{ fontWeight: 'bold' }}>
+                    Duplicate JSON key detected
+                  </EuiText>
+                  <EuiText size="s">
+                    You already have the same JSON key. If you proceed, a value
+                    of the existing JSON key will be overwritten.
+                  </EuiText>
+
+                  <div className={styles.confirmDialogActions}>
+                    <EuiButton
+                      color="secondary"
+                      aria-label="Cancel"
+                      size="s"
+                      className={cx(styles.btn)}
+                      onClick={onCancel}
+                      data-testid="cancel-confirmation-btn"
+                    >
+                      Cancel
+                    </EuiButton>
+
+                    <EuiButton
+                      fill
+                      color="warning"
+                      aria-label="Save"
+                      size="s"
+                      className={cx(styles.btn, styles.saveBtn)}
+                      onClick={confirmApply}
+                      data-testid="save-btn"
+                    >
+                      Save
+                    </EuiButton>
+                  </div>
+                </EuiPopover>
               </div>
             </EuiForm>
             {!!error && (

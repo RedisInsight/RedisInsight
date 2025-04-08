316d7df0954db66633722b28389679a2e0246636
pd-redis <petar.dzhambazov@redis.com>
2025-04-08 17:29:58 +0300

    add useWindowEvent hook

--- redisinsight/ui/src/components/base/utilities/WindowEvent.tsx
+++ redisinsight/ui/src/components/base/utilities/WindowEvent.tsx
@@ -7,15 +7,42 @@ interface WindowEventProps<Ev extends EventNames> {
   handler: (ev: WindowEventMap[Ev]) => any
 }
 
-export function WindowEvent<E extends EventNames>({
-  event,
-  handler,
-}: WindowEventProps<E>) {
+/**
+ * Adds an event listener to the window object and cleans it up when the component
+ * is unmounted.
+ *
+ * @example
+ *
+ * ```tsx
+ * useWindowEvent('resize', handleResize)
+ * ```
+ */
+export function useWindowEvent<EName extends EventNames>(
+  event: WindowEventProps<EName>['event'],
+  handler: WindowEventProps<EName>['handler'],
+) {
   useEffect(() => {
     window.addEventListener(event, handler)
     return () => {
       window.removeEventListener(event, handler)
     }
   }, [event, handler])
+}
+
+/**
+ * A component that adds an event listener to the window object and cleans it up when it is unmounted.
+ *
+ * Added for convenience, replacing EuiWindowEvent, but for future uses, the useWindowEvent hook is recommended.
+ * @example
+ *
+ * ```tsx
+ * <WindowEvent event="resize" handler={handleResize} />
+ * ```
+ */
+export function WindowEvent<EName extends EventNames>({
+  event,
+  handler,
+}: WindowEventProps<EName>) {
+  useWindowEvent(event, handler)
   return null
 }

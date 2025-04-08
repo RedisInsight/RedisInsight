a8ff458cd1c750f7213813d00e5edfa4633fabea
pd-redis <petar.dzhambazov@redis.com>
2025-04-08 14:48:04 +0300

    fix ordering of grow prop options

--- redisinsight/ui/src/components/base/layout/Flex.tsx
+++ redisinsight/ui/src/components/base/layout/Flex.tsx
@@ -165,7 +165,7 @@ const VALID_GROW_VALUES = [
   10,
 ] as const
 export type FlexItemProps = React.HTMLAttributes<HTMLDivElement> & {
-  grow?: boolean | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | null
+  grow?: (typeof VALID_GROW_VALUES)[number]
   inline?: boolean
   className?: string
 }

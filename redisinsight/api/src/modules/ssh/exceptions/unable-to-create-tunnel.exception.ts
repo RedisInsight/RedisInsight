8bb5c85727436283d20e94c6dd70e13463aef680
Dijana Antovska <dijana.antovska@redis.com>
2025-04-08 12:56:24 +0300

    use the sanitizeMessage method to sanitize error messages in all types of ssh exceptions

--- redisinsight/api/src/modules/ssh/exceptions/unable-to-create-tunnel.exception.ts
+++ redisinsight/api/src/modules/ssh/exceptions/unable-to-create-tunnel.exception.ts
@@ -1,19 +1,17 @@
 import { HttpException } from '@nestjs/common';
+import { sanitizeMessage } from '../utils';
 
 export class UnableToCreateTunnelException extends HttpException {
   constructor(message = '') {
     const prepend = 'Unable to create tunnel.';
-
-    let msg = message;
-
-    if (message.includes('Cannot parse privateKey')) {
-      msg = 'Cannot parse privateKey';
-    }
-
-    super({
-      message: `${prepend} ${msg}`,
-      name: 'UnableToCreateTunnelException',
-      statusCode: 500,
-    }, 500);
+    const sanitizedMessage = sanitizeMessage(message);
+    super(
+      {
+        message: `${prepend} ${sanitizedMessage}`,
+        name: 'UnableToCreateTunnelException',
+        statusCode: 500,
+      },
+      500,
+    );
   }
 }

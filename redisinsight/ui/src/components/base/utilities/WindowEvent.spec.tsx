f5601d00e16a58f6103f1d223f9305a32973b714
pd-redis <petar.dzhambazov@redis.com>
2025-04-09 12:56:35 +0300

    add test for handler invocation

--- redisinsight/ui/src/components/base/utilities/WindowEvent.spec.tsx
+++ redisinsight/ui/src/components/base/utilities/WindowEvent.spec.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { render } from 'uiSrc/utils/test-utils'
+import { render, fireEvent } from 'uiSrc/utils/test-utils'
 import { WindowEvent } from './WindowEvent'
 
 describe('WindowEvent', () => {
   let windowAddCount = 0
   let windowRemoveCount = 0
+  let windowAddEventListener: typeof window.addEventListener
+  let windowRemoveEventListener: typeof window.removeEventListener
 
   beforeAll(() => {
+    windowAddEventListener = window.addEventListener
+    windowRemoveEventListener = window.removeEventListener
     // React 16 and 17 register a bunch of error listeners which we don't need to capture
     window.addEventListener = jest.fn((event: string) => {
       if (event !== 'error') {
@@ -30,14 +34,14 @@ describe('WindowEvent', () => {
     jest.restoreAllMocks()
   })
 
-  test('attaches handler to window event on mount', () => {
+  it('should attach handler to window event on mount', () => {
     const handler = () => null
     render(<WindowEvent event="click" handler={handler} />)
     expect(window.addEventListener).toHaveBeenCalledWith('click', handler)
     expect(windowAddCount).toEqual(1)
   })
 
-  test('removes handler on unmount', () => {
+  it('should remove handler on unmount', () => {
     const handler = () => null
     const { unmount } = render(<WindowEvent event="click" handler={handler} />)
     unmount()
@@ -45,7 +49,7 @@ describe('WindowEvent', () => {
     expect(windowRemoveCount).toEqual(1)
   })
 
-  test('removes and re-attaches handler to window event on update', () => {
+  it('should remove and re-attach handler to window event on update', () => {
     const handler1 = () => null
     const handler2 = () => null
     const { rerender } = render(
@@ -60,7 +64,7 @@ describe('WindowEvent', () => {
     expect(window.addEventListener).toHaveBeenCalledWith('keydown', handler2)
   })
 
-  test('does not remove or re-attach handler if update is irrelevant', () => {
+  it('should not remove or re-attach handler if update is irrelevant', () => {
     const handler = () => null
     const { rerender } = render(<WindowEvent event="click" handler={handler} />)
     expect(windowAddCount).toEqual(1)
@@ -70,5 +74,15 @@ describe('WindowEvent', () => {
     )
     expect(windowAddCount).toEqual(1)
     expect(windowRemoveCount).toEqual(0)
+  })
+
+  it('should call handler on window event', () => {
+    window.addEventListener = windowAddEventListener
+    window.removeEventListener = windowRemoveEventListener
+    const handler = jest.fn()
+    render(<WindowEvent event="click" handler={handler} />)
+    fireEvent.click(window)
+    expect(handler).toHaveBeenCalled()
+    expect(handler).toHaveBeenCalledTimes(1)
   })
 })

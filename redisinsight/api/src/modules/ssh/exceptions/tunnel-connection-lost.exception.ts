0ed93e05c56c36af999a1e6e9d2aa049954eec73
dantovska <dijana.antovska@redis.com>
2025-04-09 13:54:12 +0300

    RI-6878: Sanitize ssh tunnel error messages (#4485)
    * create error message sanitizer for the ssh / tunnel errors to avoid displaying sensitive data
    * use the sanitizeMessage method to sanitize error messages in all types of ssh exceptions

--- redisinsight/api/src/modules/ssh/exceptions/tunnel-connection-lost.exception.ts
+++ redisinsight/api/src/modules/ssh/exceptions/tunnel-connection-lost.exception.ts
@@ -1,12 +1,17 @@
 import { HttpException } from '@nestjs/common';
+import { sanitizeMessage } from '../utils';
 
 export class TunnelConnectionLostException extends HttpException {
   constructor(message = '') {
     const prepend = 'Tunnel connection was lost.';
-    super({
-      message: `${prepend} ${message}`,
-      name: 'TunnelConnectionLostException',
-      statusCode: 500,
-    }, 500);
+    const sanitizedMessage = sanitizeMessage(message);
+    super(
+      {
+        message: `${prepend} ${sanitizedMessage}`,
+        name: 'TunnelConnectionLostException',
+        statusCode: 500,
+      },
+      500,
+    );
   }
 }

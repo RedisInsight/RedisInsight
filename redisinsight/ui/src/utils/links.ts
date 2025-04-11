80882a5332d55524b2b16b808a42fc6991ae842a
Dijana Antovska <dijana.antovska@redis.com>
2025-04-11 12:56:51 +0300

    WIP: create the link builder function

--- redisinsight/ui/src/utils/links.ts
+++ redisinsight/ui/src/utils/links.ts
@@ -1,4 +1,6 @@
 import { UTM_MEDIUMS } from 'uiSrc/constants/links'
+import { Instance, RedisCloudSubscription } from 'uiSrc/slices/interfaces'
+import { Nullable } from '.'
 
 export interface UTMParams {
   source?: string
@@ -18,3 +20,74 @@ export const getUtmExternalLink = (baseUrl: string, params: UTMParams) => {
     return baseUrl
   }
 }
+
+export interface Bdb {
+  id: number
+  name: string
+  access_control?: {
+    redis_password?: string
+    has_ssl_auth?: boolean
+    enforce_client_authentication?: boolean
+  }
+}
+export interface Plan {
+  plan_type: string
+  size: number
+}
+export interface BuildRedisInsightUrlParams {
+  endpoint: string
+  bdb: Instance
+  plan: CloudSubscriptionPlan
+  subscription: Nullable<RedisCloudSubscription>
+}
+const RI_PROTOCOL_SCHEMA = 'redisinsight://'
+export const buildRedisInsightUrl = ({
+  endpoint,
+  bdb,
+  subscription,
+  plan,
+}: BuildRedisInsightUrlParams) => {
+  if (!bdb) {
+    return ''
+  }
+  const defaultPassword = bdb.password
+
+  const dbUrl = defaultPassword
+    ? `redis://default:${defaultPassword}@${endpoint}`
+    : `redis://@${endpoint}`
+
+  const params: Record<string, string> = {
+    redisUrl: dbUrl,
+    cloudBdbId: bdb.id.toString(),
+    databaseAlias: bdb.nameFromProvider || '',
+  }
+
+  if (bdb.tls) {
+    // requiredTls is for backwards compatibility, in the future we may remove it
+    params.requiredTls = 'true'
+    params.requiredCaCert = 'true'
+  }
+
+  if (bdb.tlsClientAuthRequired) {
+    params.requiredClientCert = 'true'
+  }
+
+  if (plan) {
+    if (subscription.type === 'fixed') {
+      params.planMemoryLimit = planMemoryLimit;
+      params.memoryLimitMeasurementUnit = memoryLimitMeasurementUnit
+      if (subscription.free === true) {
+        params.free = 'true'
+      }
+    }
+  }
+
+  return `${RI_PROTOCOL_SCHEMA}databases/connect${appendParams(params)}`
+}
+
+const appendParams = (params: Record<string, string>) =>
+  Object.entries(params).reduce(
+    (acc, [key, value], index) =>
+      acc.concat(`${index === 0 ? '?' : '&'}${key}=${encodeURIComponent(value)}`),
+    ''
+  )

dafe654599885dea33b9b1c44d73832cdc783d59
Pavel Angelov <pavel.angelov@redis.com>
2025-04-15 10:51:31 +0300

    wrap in useCallback

--- redisinsight/ui/src/pages/browser/modules/key-details/components/change-editor-type-button/useChangeEditorType.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/change-editor-type-button/useChangeEditorType.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { rejsonSelector, setEditorType } from 'uiSrc/slices/browser/rejson'
 import { EditorType } from 'uiSrc/slices/interfaces'
@@ -5,12 +6,12 @@ import { EditorType } from 'uiSrc/slices/interfaces'
 export const useChangeEditorType = () => {
   const dispatch = useDispatch()
   const { editorType } = useSelector(rejsonSelector)
-  const opposite =
-    editorType === EditorType.Default ? EditorType.Text : EditorType.Default
 
-  const switchEditorType = () => {
+  const switchEditorType = useCallback(() => {
+    const opposite =
+      editorType === EditorType.Default ? EditorType.Text : EditorType.Default
     dispatch(setEditorType(opposite))
-  }
+  }, [dispatch, editorType])
 
   return { switchEditorType, editorType }
 }

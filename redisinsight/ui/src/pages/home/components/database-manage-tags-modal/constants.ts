c17f993e718f700cec6e68e389fc153a6d37ac02
Krum Tyukenov <krum.tyukenov@redis.com>
2025-04-16 16:29:34 +0300

    RI-6384: add unique tag validation errors

--- redisinsight/ui/src/pages/home/components/database-manage-tags-modal/constants.ts
+++ redisinsight/ui/src/pages/home/components/database-manage-tags-modal/constants.ts
@@ -32,7 +32,13 @@ export const presetTagSuggestions: {
   },
 ]
 
-export const VALID_TAG_KEY_REGEX = /^[a-zA-Z0-9\-_.@:+ ]{1,64}$/
-export const VALID_TAG_VALUE_REGEX = /^[a-zA-Z0-9\-_.@:+ ]{1,128}$/
-export const INVALID_FIELD_MESSAGE =
-  'Tag should have unique keys and can only have letters, numbers, spaces, and these special characters: “- _ . + @ :”. Max characters length is 64 for keys and 128 for values.'
+export const MAX_KEY_LENGTH = 64
+export const MAX_VALUE_LENGTH = 128
+
+export const VALID_TAG_KEY_REGEX = new RegExp(`^[a-zA-Z0-9\\-_.@:+ ]{1,${MAX_KEY_LENGTH}}$`)
+export const VALID_TAG_VALUE_REGEX = new RegExp(`^[a-zA-Z0-9\\-_.@:+ ]{1,${MAX_VALUE_LENGTH}}$`)
+
+export const INVALID_FIELD_MESSAGE = 'Tag can only have letters, numbers, spaces, and these special characters: “- _ . + @ :”'
+export const INVALID_FIELD_UNIQUE_KEY_MESSAGE = 'Key should be unique'
+export const INVALID_FIELD_MAX_KEY_LENGTH_MESSAGE = `Key must be under ${MAX_KEY_LENGTH} characters`
+export const INVALID_FIELD_MAX_VALUE_LENGTH_MESSAGE = `Value must be under ${MAX_VALUE_LENGTH} characters`

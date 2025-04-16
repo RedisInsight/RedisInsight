247c4d1cc7a06558c0f63466b57b3b6c5942715c
Krum Tyukenov <krum.tyukenov@gmail.com>
2025-04-16 17:22:33 +0300

    RI-6384: add unique tag validation errors (#4504)

--- redisinsight/ui/src/pages/home/components/database-manage-tags-modal/TagInputField.tsx
+++ redisinsight/ui/src/pages/home/components/database-manage-tags-modal/TagInputField.tsx
@@ -1,33 +1,33 @@
 import React, { useState } from 'react'
 import { EuiFieldText, EuiToolTip } from '@elastic/eui'
 
-import { INVALID_FIELD_MESSAGE } from './constants'
 import { TagSuggestions } from './TagSuggestions'
 
 type TagInputFieldProps = {
   value: string
-  isInvalid: boolean
   disabled?: boolean
   currentTagKeys: Set<string>
   suggestedTagKey?: string
   rightContent?: React.ReactNode
+  errorMessage?: string
   onChange: (value: string) => void
 }
 
 export const TagInputField = ({
   value,
-  isInvalid,
   disabled,
   currentTagKeys,
   suggestedTagKey,
   rightContent,
+  errorMessage,
   onChange,
 }: TagInputFieldProps) => {
+  const isInvalid = Boolean(errorMessage)
   const [isFocused, setIsFocused] = useState(false)
 
   return (
     <div>
-      <EuiToolTip content={isInvalid && INVALID_FIELD_MESSAGE} position="top">
+      <EuiToolTip content={errorMessage} position="top">
         <div>
           <EuiFieldText
             value={value}

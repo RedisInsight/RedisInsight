42c816e83884eedc9f7fa0713fcdc4a61d80f565
Yash Suresh Chandra <yashschandra@gmail.com>
2025-05-14 13:18:46 +0530

    Save and load theme using settings api (#4433)
    * set theme from settings api
    * store system theme in react. load theme name from api in settings page
    * theme settings component added
    
    ---------
    
    Co-authored-by: Pavel Angelov <pavel.angelov@redis.com>

--- redisinsight/ui/src/pages/settings/SettingsPage.tsx
+++ redisinsight/ui/src/pages/settings/SettingsPage.tsx
@@ -1,32 +1,23 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import cx from 'classnames'
 import {
   EuiCallOut,
   EuiCollapsibleNavGroup,
-  EuiForm,
-  EuiFormRow,
   EuiLoadingSpinner,
-  EuiSuperSelect,
   EuiText,
   EuiTitle,
 } from '@elastic/eui'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { setTitle } from 'uiSrc/utils'
-import { FeatureFlags, Theme, THEMES } from 'uiSrc/constants'
+import { FeatureFlags } from 'uiSrc/constants'
 import { useDebouncedEffect } from 'uiSrc/services'
 import {
   ConsentsNotifications,
   ConsentsPrivacy,
   FeatureFlagComponent,
 } from 'uiSrc/components'
-import {
-  sendEventTelemetry,
-  sendPageViewTelemetry,
-  TelemetryEvent,
-  TelemetryPageView,
-} from 'uiSrc/telemetry'
-import { ThemeContext } from 'uiSrc/contexts/themeContext'
+import { sendPageViewTelemetry, TelemetryPageView } from 'uiSrc/telemetry'
 import {
   fetchUserConfigSettings,
   fetchUserSettingsSpec,
@@ -45,6 +36,7 @@ import {
   AdvancedSettings,
   CloudSettings,
   WorkbenchSettings,
+  ThemeSettings,
 } from './components'
 import { DateTimeFormatter } from './components/general-settings'
 import styles from './styles.module.scss'
@@ -56,14 +48,6 @@ const SettingsPage = () => {
   const initialOpenSection = globalThis.location.hash || ''
 
   const dispatch = useDispatch()
-
-  const options = THEMES
-  const themeContext = useContext(ThemeContext)
-  let { theme, changeTheme, usingSystemTheme } = themeContext
-
-  if (usingSystemTheme) {
-    theme = Theme.System
-  }
 
   useEffect(() => {
     // componentDidMount
@@ -80,36 +64,9 @@ const SettingsPage = () => {
   useDebouncedEffect(() => setLoading(settingsLoading), 100, [settingsLoading])
   setTitle('Settings')
 
-  const onChange = (value: string) => {
-    const previousValue = theme
-    changeTheme(value)
-    sendEventTelemetry({
-      event: TelemetryEvent.SETTINGS_COLOR_THEME_CHANGED,
-      eventData: {
-        previousColorTheme: previousValue,
-        currentColorTheme: value,
-      },
-    })
-  }
-
   const Appearance = () => (
     <>
-      <EuiForm component="form">
-        <EuiTitle size="xs">
-          <h4>Color Theme</h4>
-        </EuiTitle>
-        <Spacer size="m" />
-        <EuiFormRow label="Specifies the color theme to be used in Redis Insight:">
-          <EuiSuperSelect
-            options={options}
-            valueOfSelected={theme}
-            onChange={onChange}
-            style={{ marginTop: '12px' }}
-            data-test-subj="select-theme"
-          />
-        </EuiFormRow>
-        <Spacer size="xl" />
-      </EuiForm>
+      <ThemeSettings />
       <ConsentsNotifications />
       <Divider colorVariable="separatorColor" />
       <Spacer />

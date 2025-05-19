8ce75c891cbf1def654498b740bd70484e1ad5e5
Pavel Angelov <pavel.angelov@redis.com>
2025-05-19 16:19:52 +0300

    fix: support big ints

--- redisinsight/ui/src/pages/browser/modules/key-details/components/rejson-details/monaco-editor/MonacoEditor.spec.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/rejson-details/monaco-editor/MonacoEditor.spec.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import MonacoEditor from './MonacoEditor'
 
-const mockStore = configureStore({ reducer: () => ({}) })
+const mockStore = configureStore({
+  reducer: () => ({
+    browser: {
+      rejson: {},
+    },
+  }),
+})
 
 const renderWithRedux = (ui: React.ReactNode) =>
   render(<Provider store={mockStore}>{ui}</Provider>)
@@ -19,11 +25,21 @@ const commonProps = {
   expandedRows: new Set(['someKey']),
 }
 
+jest.mock('uiSrc/components/monaco-editor/useMonacoValidation', () => ({
+  __esModule: true,
+  default: () => ({
+    isValid: true,
+    isValidating: false,
+  }),
+}))
+
 it('should preserve large numbers in Monaco editor', async () => {
   const bigNumber = '245343644508855571'
   const data = { huge: BigInt(bigNumber) }
 
-  expect(() =>
-    renderWithRedux(<MonacoEditor data={data as any} {...commonProps} />),
-  ).toThrow(/Do not know how to serialize a BigInt/)
+  renderWithRedux(<MonacoEditor data={data as any} {...commonProps} />)
+
+  const editor = await screen.findByTestId('json-data-editor')
+
+  expect(editor.textContent).toContain(`${bigNumber}`)
 })

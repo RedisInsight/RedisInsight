de283ff60ba1f4c786498147e355fed9bffb5e1d
Dijana Antovska <dijana.antovska@redis.com>
2025-05-19 09:33:14 +0300

    remove duplicate variables; remove unneeded import (at this point)

--- redisinsight/ui/src/components/base/layout/index.ts
+++ redisinsight/ui/src/components/base/layout/index.ts
@@ -1,5 +1,4 @@
 import HorizontalRule from './horizontal-rule/HorizontalRule'
 import LoadingContent from './loading-content/LoadingContent'
-import { ResizableButton } from './resizable-container/ResizableButton'
 
-export { HorizontalRule, LoadingContent, ResizableButton }
+export { HorizontalRule, LoadingContent }

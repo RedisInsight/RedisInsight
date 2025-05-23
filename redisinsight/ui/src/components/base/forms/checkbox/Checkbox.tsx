61f3a5268bb6fb7dc686f082bea280b0c3a58008
pd-redis <petar.dzhambazov@redis.com>
2025-05-23 14:12:49 +0300

    SSHDetails.tsx

--- redisinsight/ui/src/components/base/forms/checkbox/Checkbox.tsx
+++ redisinsight/ui/src/components/base/forms/checkbox/Checkbox.tsx
@@ -14,6 +14,19 @@ export type CheckboxProps = Omit<
 export const Checkbox = (props: CheckboxProps) => {
   const { onChange, onCheckedChange, id, ...rest } = props
 
+  /**
+   * Handles the change event for a checkbox input and notifies the relevant handlers.
+   *
+   * This is added to provide compatibility with the `onChange` handler expected by Formik library.
+   * Constructs a synthetic event object designed to mimic a React checkbox change event.
+   * Updates the `checked` status and passes the constructed event to the `onChange` handler
+   * if provided. Additionally, invokes the `onCheckedChange` handler with the new `checked` state,
+   * if it is defined.
+   *
+   * @param {CheckedType} checked - The new checked state of the checkbox. It is expected to
+   *        be a boolean-like value where `true` indicates checked and any other value
+   *        indicates unchecked.
+   */
   const handleCheckedChange = (checked: CheckedType) => {
     const syntheticEvent = {
       target: {

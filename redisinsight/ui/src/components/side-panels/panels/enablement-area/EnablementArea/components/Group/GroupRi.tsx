2a90204a1a5017aa43113d354170f3dde0052beb
pd-redis <petar.dzhambazov@redis.com>
2025-06-05 15:00:18 +0300

    temp

---
+++ redisinsight/ui/src/components/side-panels/panels/enablement-area/EnablementArea/components/Group/GroupRi.tsx
@@ -0,0 +1,150 @@
+import React, { useState } from 'react'
+import { EuiIcon, EuiToolTip } from '@elastic/eui'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import cx from 'classnames'
+
+import {
+  sendEventTelemetry,
+  TELEMETRY_EMPTY_VALUE,
+  TelemetryEvent,
+} from 'uiSrc/telemetry'
+import { workbenchCustomTutorialsSelector } from 'uiSrc/slices/workbench/wb-custom-tutorials'
+import { EAItemActions } from 'uiSrc/constants'
+import { ONBOARDING_FEATURES } from 'uiSrc/components/onboarding-features'
+import { OnboardingTour } from 'uiSrc/components'
+import { RiAccordion } from 'uiSrc/components/base/display/accordion/RiAccordion'
+import DeleteTutorialButton from '../DeleteTutorialButton'
+
+import './styles.scss'
+
+export interface Props {
+  id: string
+  label: string
+  actions?: string[]
+  onCreate?: () => void
+  onDelete?: (id: string) => void
+  children: React.ReactNode
+  withBorder?: boolean
+  initialIsOpen?: boolean
+  forceState?: 'open' | 'closed'
+  onToggle?: (isOpen: boolean) => void
+  isPageOpened?: boolean
+}
+
+const Group = (props: Props) => {
+  const {
+    label,
+    actions,
+    children,
+    id,
+    forceState,
+    withBorder = false,
+    initialIsOpen = false,
+    onToggle,
+    onCreate,
+    onDelete,
+    isPageOpened,
+  } = props
+  const { deleting: deletingCustomTutorials } = useSelector(
+    workbenchCustomTutorialsSelector,
+  )
+  const { instanceId = '' } = useParams<{ instanceId: string }>()
+
+  const [isGroupOpen, setIsGroupOpen] = useState<boolean>(initialIsOpen)
+
+  const handleCreate = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    onCreate?.()
+    sendEventTelemetry({
+      event: TelemetryEvent.EXPLORE_PANEL_IMPORT_CLICKED,
+      eventData: {
+        databaseId: instanceId || TELEMETRY_EMPTY_VALUE,
+      },
+    })
+  }
+
+  const handleDelete = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    onDelete?.(id)
+  }
+
+  const handleOpen = (isOpen: boolean) => {
+    if (forceState === 'open') return
+
+    setIsGroupOpen(isOpen)
+    onToggle?.(isOpen)
+  }
+
+  const actionsContent = (
+    <>
+      {actions?.includes(EAItemActions.Create) &&
+        (isGroupOpen || forceState === 'open') && (
+          <OnboardingTour
+            options={ONBOARDING_FEATURES.EXPLORE_CUSTOM_TUTORIALS}
+            anchorPosition="downLeft"
+            anchorWrapperClassName="onboardingPopoverAnchor"
+            panelClassName={cx({ hide: isPageOpened })}
+            preventPropagation
+          >
+            <EuiToolTip content="Upload Tutorial">
+              <div
+                className="group-header__btn group-header__create-btn"
+                role="presentation"
+                onClick={handleCreate}
+                data-testid="open-upload-tutorial-btn"
+              >
+                <EuiIcon type="plus" />
+              </div>
+            </EuiToolTip>
+          </OnboardingTour>
+        )}
+      {actions?.includes(EAItemActions.Delete) && (
+        <DeleteTutorialButton
+          id={id}
+          label={label}
+          onDelete={handleDelete}
+          isLoading={deletingCustomTutorials}
+        />
+      )}
+    </>
+  )
+
+  const buttonContent = (
+    <div className="group-header-wrapper">
+      <EuiText className="group-header" size="m">
+        {isShowFolder && (
+          <EuiIcon type={isGroupOpen ? 'folderOpen' : 'folderClosed'} />
+        )}
+        {label}
+      </EuiText>
+      {isShowActions && actionsContent}
+    </div>
+  )
+
+  const buttonProps: any = {
+    'data-testid': `accordion-button-${id}`,
+    style: triggerStyle,
+    className: buttonClassName,
+  }
+
+  return (
+    <RiAccordion
+      collapsible
+      id={id}
+      data-testid={`accordion-${id}`}
+      content={children}
+      defaultOpen={initialIsOpen}
+      open={forceState === 'open'}
+      label={label}
+      onOpenChange={handleOpen}
+      style={{
+        whiteSpace: 'nowrap',
+        width: 'auto',
+      }}
+      className={cx({ withBorder })}
+    />
+  )
+}
+
+export default Group

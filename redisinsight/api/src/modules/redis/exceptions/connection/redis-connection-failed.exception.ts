bbd76fc256f489c3e9ebe49fc01aa568e079be8c
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-06-06 15:44:32 +0300

    RI-7101 resolve PR comments

--- redisinsight/api/src/modules/redis/exceptions/connection/redis-connection-failed.exception.ts
+++ redisinsight/api/src/modules/redis/exceptions/connection/redis-connection-failed.exception.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpExceptionOptions } from '@nestjs/common';
+import { HttpException, HttpExceptionOptions, HttpStatus } from '@nestjs/common';
 import { isString } from 'lodash';
 import { CustomErrorCodes } from 'src/constants';
 import ERROR_MESSAGES from 'src/constants/error-messages';
@@ -6,7 +6,7 @@ import ERROR_MESSAGES from 'src/constants/error-messages';
 // The HTTP 424 Failed Dependency client error response status code indicates
 // that the method could not be performed on the resource because the requested action
 // depended on another action, and that action failed.
-export const RedisConnectionFailedStatusCode = 424;
+export const RedisConnectionFailedStatusCode = HttpStatus.FAILED_DEPENDENCY;
 
 export class RedisConnectionFailedException extends HttpException {
   constructor(

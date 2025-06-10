dfa9dc3953e9f05147155f9090b7934ae82e294e
Pavel Angelov <pavel.angelov@redis.com>
2025-06-10 17:49:13 +0300

    add hook tests

--- redisinsight/ui/src/pages/browser/modules/key-details/components/change-editor-type-button/useChangeEditorType.spec.ts
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/change-editor-type-button/useChangeEditorType.spec.ts
@@ -1,6 +1,8 @@
 import * as reactRedux from 'react-redux'
 import { renderHook, act } from '@testing-library/react-hooks'
 import { EditorType } from 'uiSrc/slices/interfaces'
+import { FeatureFlags } from 'uiSrc/constants'
+
 import { useChangeEditorType } from './useChangeEditorType'
 
 jest.mock('react-redux', () => ({
@@ -54,6 +56,64 @@ describe('useChangeEditorType', () => {
     expect(dispatchMock).toHaveBeenCalledWith({
       type: 'rejson/setEditorType',
       payload: EditorType.Default,
+    })
+  })
+
+  describe('isTextEditorDisabled', () => {
+    it('should be false when isWithinThreshold is true', () => {
+      mockedUseSelector
+        .mockImplementationOnce(() => ({
+          editorType: EditorType.Default,
+          isWithinThreshold: true,
+        }))
+        .mockImplementationOnce(() => ({
+          [FeatureFlags.envDependent]: { flag: false },
+        }))
+
+      const { result } = renderHook(() => useChangeEditorType())
+      expect(result.current.isTextEditorDisabled).toBe(false)
+    })
+
+    it('should be false when not within threshold but feature flag is true', () => {
+      mockedUseSelector
+        .mockImplementationOnce(() => ({
+          editorType: EditorType.Default,
+          isWithinThreshold: false,
+        }))
+        .mockImplementationOnce(() => ({
+          [FeatureFlags.envDependent]: { flag: true },
+        }))
+
+      const { result } = renderHook(() => useChangeEditorType())
+      expect(result.current.isTextEditorDisabled).toBe(false)
+    })
+
+    it('should be true when not within threshold and feature flag is false', () => {
+      mockedUseSelector
+        .mockImplementationOnce(() => ({
+          editorType: EditorType.Default,
+          isWithinThreshold: false,
+        }))
+        .mockImplementationOnce(() => ({
+          [FeatureFlags.envDependent]: { flag: false },
+        }))
+
+      const { result } = renderHook(() => useChangeEditorType())
+      expect(result.current.isTextEditorDisabled).toBe(true)
+    })
+
+    it('should be true when envDependentFeature is undefined', () => {
+      mockedUseSelector
+        .mockImplementationOnce(() => ({
+          editorType: EditorType.Default,
+          isWithinThreshold: false,
+        }))
+        .mockImplementationOnce(() => ({
+          [FeatureFlags.envDependent]: undefined,
+        }))
+
+      const { result } = renderHook(() => useChangeEditorType())
+      expect(result.current.isTextEditorDisabled).toBe(true)
     })
   })
 })

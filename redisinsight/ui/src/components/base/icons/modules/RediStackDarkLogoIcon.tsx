519f64a19c5ba67ff2970041b755385134dadf6e
pd-redis <petar.dzhambazov@redis.com>
2025-06-18 10:22:46 +0300

    move modules icons

--- redisinsight/ui/src/components/base/icons/modules/RediStackDarkLogoIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Icon, IconProps } from 'uiSrc/components/base/icons'
-import RediStackDarkLogoSvg from 'uiSrc/assets/img/modules/redistack/RedisStackLogoDark.svg?react'
-
-export const RediStackDarkLogoIcon = (props: IconProps) => (
-  <Icon icon={RediStackDarkLogoSvg} {...props} isSvg />
-)
--- redisinsight/ui/src/components/base/icons/modules/RediStackLightMinIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import RediStackLightMinLight from 'uiSrc/assets/img/modules/redistack/RediStackLight-min.svg?react'
-import { Icon, IconProps } from 'uiSrc/components/base/icons'
-
-export const RediStackLightMinIcon = (props: IconProps) => (
-  <Icon icon={RediStackLightMinLight} {...props} isSvg />
-)

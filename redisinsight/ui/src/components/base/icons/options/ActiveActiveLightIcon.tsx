87ae9b963aa36d7e00a59b5b0d2438d41ce774b6
pd-redis <petar.dzhambazov@redis.com>
2025-06-18 10:17:25 +0300

    move options icons

--- redisinsight/ui/src/components/base/icons/options/ActiveActiveLightIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ActiveActiveLight from 'uiSrc/assets/img/options/Active-ActiveLight.svg?react'
-import { Icon, IconProps } from 'uiSrc/components/base/icons'
-
-export const ActiveActiveLightIcon = (props: IconProps) => (
-  <Icon icon={ActiveActiveLight} {...props} isSvg />
-)
--- redisinsight/ui/src/components/base/icons/options/RedisOnFlashLightIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import RedisOnFlashLight from 'uiSrc/assets/img/options/RedisOnFlashLight.svg?react'
-import { Icon, IconProps } from 'uiSrc/components/base/icons'
-
-export const RedisOnFlashLightIcon = (props: IconProps) => (
-  <Icon icon={RedisOnFlashLight} {...props} isSvg />
-)

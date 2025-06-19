4598f44e11f24bed57f88a1e87ef5b92d536b341
Dijana Antovska <dijana.antovska@redis.com>
2025-06-19 21:03:04 +0300

    remove custom styles; fix profile link styling

--- redisinsight/ui/src/components/base/link/UserProfileLink.tsx
+++ redisinsight/ui/src/components/base/link/UserProfileLink.tsx
@@ -6,12 +6,13 @@ export const UserProfileLink = styled(Link)`
   padding: 8px 12px !important;
   width: 100%;
   color: ${({ theme }: { theme: ReturnType<typeof useTheme> }) =>
-    theme.color.azure500} !important;
+    theme.semantic.color.text.informative400} !important;
+  text-decoration: none !important;
 
   &:not(:last-child) {
     border-bottom: 1px solid
       ${({ theme }: { theme: ReturnType<typeof useTheme> }) =>
-    theme.color.azure500};
+    theme.color.gray400};
   }
 
   span {

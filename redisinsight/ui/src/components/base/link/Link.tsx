2cbbfacf4b0b34c5b347e4fdd9a5c6ecd70f025f
Dijana Antovska <dijana.antovska@redis.com>
2025-06-19 16:34:46 +0300

    WIP: add color prop to the link and remove global link styles.

--- redisinsight/ui/src/components/base/link/Link.tsx
+++ redisinsight/ui/src/components/base/link/Link.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link as RedisUiLink } from '@redis-ui/components'
+import { RiLinkProps, StyledLink } from './link.styles'
 
 export type LinkProps = React.ComponentProps<typeof RedisUiLink>
-export const Link = (props: LinkProps) => <RedisUiLink {...props} />
+export const Link = ({ color, ...rest }: RiLinkProps) => <StyledLink $color={color} {...rest} />
---
+++ redisinsight/ui/src/components/base/link/link.styles.ts
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { Link } from '@redis-ui/components'
+import { useTheme } from '@redis-ui/styles'
+
+export type LinkProps = React.ComponentProps<typeof Link>
+
+// TODO [DA]: Export the color functionality and use both for Link and Text
+export type EuiColorNames =
+  | 'default'
+  | 'primary'
+  | 'text'
+  | 'subdued'
+  | 'danger'
+  | 'ghost'
+  | 'accent'
+  | 'warning'
+  | 'success'
+
+export type ColorType = LinkProps['color'] | EuiColorNames | (string & {})
+
+export type RiLinkProps = Omit<LinkProps, 'color'> & {
+  color?: ColorType
+}
+
+export interface MapProps extends RiLinkProps {
+  $color?: ColorType
+}
+
+export const useColorTextStyles = ({ $color }: MapProps = {}) => {
+  const theme = useTheme()
+  const colors = theme.semantic.color
+
+  const getColorValue = (color?: ColorType) => {
+    if (!color) {
+      return colors.text.primary400
+    }
+    switch (color) {
+      case 'default':
+      case 'primary':
+        return colors.text.primary400
+      case 'text':
+        return colors.text.neutral700
+      case 'subdued':
+        return colors.text.informative400
+      case 'danger':
+        return colors.text.danger600
+      case 'ghost':
+        return colors.text.neutral600
+      case 'accent':
+        return colors.text.notice600
+      case 'warning':
+        return colors.text.attention600
+      case 'success':
+        return colors.text.success600
+      default:
+        return color // any supported color value e.g #fff
+    }
+  }
+
+  return css`
+    color: ${getColorValue($color)};
+  `
+}
+
+export const StyledLink = styled(Link)<MapProps>`
+  ${useColorTextStyles};
+
+  text-decoration: underline !important;
+
+  &:hover {
+    text-decoration: none !important;
+  }
+
+  &:focus {
+    background-color: inherit !important;
+  }
+`

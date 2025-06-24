1070550581bb036f9f82973d10f4c3c2f9e99947
pd-redis <petar.dzhambazov@redis.com>
2025-06-24 17:59:31 +0300

    replace in FilterKeyType.tsx, KeyTreeSettings.tsx, RediSearchIndexesList.tsx

--- redisinsight/ui/src/pages/browser/components/filter-key-type/FilterKeyType.tsx
+++ redisinsight/ui/src/pages/browser/components/filter-key-type/FilterKeyType.tsx
@@ -1,9 +1,4 @@
-import {
-  EuiModal,
-  EuiModalBody,
-  EuiSuperSelect,
-  EuiSuperSelectOption,
-} from '@elastic/eui'
+import { EuiModal, EuiModalBody } from '@elastic/eui'
 import cx from 'classnames'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -29,6 +24,7 @@ import { appFeatureFlagsFeaturesSelector } from 'uiSrc/slices/app/features'
 import { AdditionalRedisModule } from 'uiSrc/slices/interfaces'
 import { OutsideClickDetector } from 'uiSrc/components/base/utils'
 import { HealthText } from 'uiSrc/components/base/text/HealthText'
+import { RiSelect } from 'uiSrc/components/base/forms/select/RiSelect'
 import { FILTER_KEY_TYPE_OPTIONS } from './constants'
 
 import styles from './styles.module.scss'
@@ -65,32 +61,41 @@ const FilterKeyType = ({ modules }: Props) => {
     setTypeSelected(filter ?? ALL_KEY_TYPES_VALUE)
   }, [filter])
 
-  const options: EuiSuperSelectOption<string>[] =
-    FILTER_KEY_TYPE_OPTIONS.filter(({ featureFlag, skipIfNoModule }) => {
-      if (
-        skipIfNoModule &&
-        !modules?.some(({ name }) => name === skipIfNoModule)
-      ) {
-        return false
-      }
-      return !featureFlag || features[featureFlag]?.flag
-    }).map((item) => {
-      const { value, color, text } = item
-      return {
-        value,
-        inputDisplay: (
-          <HealthText color={color} className={styles.dropdownDisplay}>
-            {text}
-          </HealthText>
-        ),
-        dropdownDisplay: (
-          <HealthText color={color} className={styles.dropdownDisplay}>
-            {text}
-          </HealthText>
-        ),
-        'data-test-subj': `filter-option-type-${value}`,
-      }
-    })
+  const options: {
+    value: string
+    inputDisplay: JSX.Element
+    dropdownDisplay: JSX.Element
+  }[] = FILTER_KEY_TYPE_OPTIONS.filter(({ featureFlag, skipIfNoModule }) => {
+    if (
+      skipIfNoModule &&
+      !modules?.some(({ name }) => name === skipIfNoModule)
+    ) {
+      return false
+    }
+    return !featureFlag || features[featureFlag]?.flag
+  }).map((item) => {
+    const { value, color, text } = item
+    return {
+      value,
+      inputDisplay: (
+        <HealthText
+          color={color}
+          data-test-subj={`filter-option-type-${value}`}
+        >
+          {text}
+        </HealthText>
+      ),
+      dropdownDisplay: (
+        <HealthText
+          color={color}
+          data-test-subj={`filter-option-type-${value}`}
+        >
+          {text}
+        </HealthText>
+      ),
+      'data-test-subj': `filter-option-type-${value}`,
+    }
+  })
 
   options.unshift({
     value: ALL_KEY_TYPES_VALUE,
@@ -99,7 +104,7 @@ const FilterKeyType = ({ modules }: Props) => {
         All Key Types
       </div>
     ),
-    dropdownDisplay: 'All Key Types',
+    dropdownDisplay: <span>All Key Types</span>,
   })
 
   const onChangeType = (initValue: string) => {
@@ -166,13 +171,17 @@ const FilterKeyType = ({ modules }: Props) => {
             data-testid="unsupported-btn-anchor"
           />
         )}
-        <EuiSuperSelect
-          fullWidth
-          itemClassName={cx('withColorDefinition', styles.filterKeyType)}
+        <RiSelect
           disabled={!isVersionSupported}
           options={options}
-          isOpen={isSelectOpen}
-          valueOfSelected={typeSelected}
+          valueRender={({ option, isOptionValue }) => {
+            if (isOptionValue) {
+              return option.inputDisplay
+            }
+            return option.dropdownDisplay
+          }}
+          defaultOpen={isSelectOpen}
+          value={typeSelected}
           onChange={(value: string) => onChangeType(value)}
           data-testid="select-filter-key-type"
         />

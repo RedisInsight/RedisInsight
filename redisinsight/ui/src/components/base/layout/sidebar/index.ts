0f936e37026bfe3aee66dd8b597178d68dd07094
Dijana Antovska <dijana.antovska@redis.com>
2025-06-30 15:38:21 +0300

    Export the sidebar component

---
+++ redisinsight/ui/src/components/base/layout/sidebar/index.ts
@@ -0,0 +1,19 @@
+import { SideBar } from '@redis-ui/components'
+
+const SideBarHeader = SideBar.Header
+// eslint-disable-next-line prefer-destructuring
+const SideBarLogo = SideBar.SideBarLogo
+const SideBarContainer = SideBar.ItemsContainer
+const SideBarItem = SideBar.Item
+const SideBarDivider = SideBar.Divider
+const SideBarFooter = SideBar.Footer
+
+export {
+  SideBar,
+  SideBarHeader,
+  SideBarLogo,
+  SideBarContainer,
+  SideBarItem,
+  SideBarDivider,
+  SideBarFooter,
+}

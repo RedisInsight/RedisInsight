3f1ee2e8cbc5a7577d9ca1db85b0ccc363c6aac8
Dijana Antovska <dijana.antovska@redis.com>
2025-07-02 10:35:54 +0300

    remove the logo component - wasn't flexible enough to use in this case

--- redisinsight/ui/src/components/base/layout/sidebar/index.ts
+++ redisinsight/ui/src/components/base/layout/sidebar/index.ts
@@ -2,8 +2,6 @@ import { SideBar } from '@redis-ui/components'
 import { SideBarItemIcon } from './SideBarItemIcon'
 
 const SideBarHeader = SideBar.Header
-// eslint-disable-next-line prefer-destructuring
-const SideBarLogo = SideBar.SideBarLogo
 const SideBarContainer = SideBar.ItemsContainer
 const SideBarItem = SideBar.Item
 const SideBarDivider = SideBar.Divider
@@ -12,7 +10,6 @@ const SideBarFooter = SideBar.Footer
 export {
   SideBar,
   SideBarHeader,
-  SideBarLogo,
   SideBarContainer,
   SideBarItem,
   SideBarItemIcon,

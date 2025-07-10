42d122bcef6c62a42d1eb5207cf402b6abebb45e
dantovska <dijana.antovska@redis.com>
2025-07-10 11:13:32 +0300

    Change button text from New Report to Analyze, and the empty reports message accordingly (#4681)

--- redisinsight/ui/src/pages/database-analysis/components/header/Header.spec.tsx
+++ redisinsight/ui/src/pages/database-analysis/components/header/Header.spec.tsx
@@ -103,6 +103,7 @@ describe('DatabaseAnalysisHeader', () => {
 
     expect(screen.getByTestId('analysis-progress')).toBeInTheDocument()
   })
+
   it('should call "getDBAnalysis" action be called after click "start-database-analysis-btn"', () => {
     render(<Header {...instance(mockedProps)} />)
     fireEvent.click(screen.getByTestId('start-database-analysis-btn'))
@@ -110,6 +111,7 @@ describe('DatabaseAnalysisHeader', () => {
     const expectedActions = [getDBAnalysis()]
     expect(store.getActions()).toEqual(expectedActions)
   })
+
   it('should send telemetry event after click "new analysis" btn', async () => {
     const sendEventTelemetryMock = jest.fn()
 
@@ -129,6 +131,16 @@ describe('DatabaseAnalysisHeader', () => {
       },
     })
     ;(sendEventTelemetry as jest.Mock).mockRestore()
+  })
+
+  it('should show "Analyze" text on the start analysis button', async () => {
+    render(
+      <Header {...instance(mockedProps)} items={mockReports} progress={mockProgress} />,
+    )
+
+    const analizeButtonId = screen.getByTestId('start-database-analysis-btn')
+    expect(analizeButtonId).toBeInTheDocument()
+    expect(analizeButtonId).toHaveTextContent('Analyze')
   })
 
   it.skip('should call onChangeSelectedAnalysis after change selector', async () => {

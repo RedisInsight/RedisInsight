515afebdb7088fc08911437e808ed4b5bb113d1e
Krum Tyukenov <krum.tyukenov@redis.com>
2025-07-10 14:43:36 +0300

    fix missing import

--- redisinsight/ui/src/components/monitor/MonitorHeader/MonitorHeader.tsx
+++ redisinsight/ui/src/components/monitor/MonitorHeader/MonitorHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import cx from 'classnames'
 import { useParams } from 'react-router-dom'
 import { EuiIcon } from '@elastic/eui'
 

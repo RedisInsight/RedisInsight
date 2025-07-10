860b9f3df44d95e2984d163e5068c179b2812d17
Pavel Angelov <pavel.angelov@redis.com>
2025-07-10 10:02:18 +0300

    export styled components to separate file

--- redisinsight/ui/src/components/new-index/selection-box/SelectionBox.tsx
+++ redisinsight/ui/src/components/new-index/selection-box/SelectionBox.tsx
@@ -1,45 +1,21 @@
 import React from 'react'
 import { BoxSelectionGroup, BoxSelectionGroupBox } from '@redis-ui/components'
-import styled from 'styled-components'
-import { Text, Title } from 'uiSrc/components/base/text'
+import {
+  DisabledBar,
+  StyledBoxContent,
+  StyledText,
+  StyledTitle,
+} from './SelectionBox.styles'
 
 export interface BoxSelectionOption<T extends string = string>
   extends BoxSelectionGroupBox<T> {
   text?: string
 }
 
-const StyledBoxContent = styled.div`
-  padding: 16px;
-  text-align: left;
-`
-
-const StyledTitle = styled(Title)`
-  margin-top: 4px;
-`
-
-const StyledText = styled(Text)`
-  margin-top: 4px;
-`
-
-const StyledDisabledBar = styled.div`
-  padding: 2px 0;
-  /* Design color: #f4f5f6 */
-  background: var(--comboBoxBadgeBgColor);
-  /* Design color: #5c707a */
-  color: var(--badgeIconColor);
-  border-radius: 2px;
-  border-bottom: 1px solid var(--controlsBoxShadowColor);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-`
-
 const SelectionBox = ({ label, text, disabled, ...rest }: any) => (
   <BoxSelectionGroup.Item.Compose {...rest}>
-    {disabled && (
-      <StyledDisabledBar>
-        <Text size="xs">Coming soon</Text>
-      </StyledDisabledBar>
-    )}
+    {disabled && <DisabledBar />}
+
     <StyledBoxContent>
       <BoxSelectionGroup.Item.Icon color="neutral700" customSize="32px" />
 

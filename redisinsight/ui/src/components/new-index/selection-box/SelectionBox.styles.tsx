fbbd5e5be5a9b20ae99ae9e579825d50a5fe0582
Pavel Angelov <pavel.angelov@redis.com>
2025-07-10 10:02:18 +0300

    export styled components to separate file

---
+++ redisinsight/ui/src/components/new-index/selection-box/SelectionBox.styles.tsx
@@ -0,0 +1,40 @@
+import { BoxSelectionGroupBox } from '@redis-ui/components'
+import React from 'react'
+import styled from 'styled-components'
+import { Text, Title } from 'uiSrc/components/base/text'
+
+export interface BoxSelectionOption<T extends string = string>
+  extends BoxSelectionGroupBox<T> {
+  text?: string
+}
+
+export const StyledBoxContent = styled.div`
+  padding: 16px;
+  text-align: left;
+`
+
+export const StyledTitle = styled(Title)`
+  margin-top: 4px;
+`
+
+export const StyledText = styled(Text)`
+  margin-top: 4px;
+`
+
+export const StyledDisabledBar = styled.div`
+  padding: 2px 0;
+  /* Design color: #f4f5f6 */
+  background: var(--comboBoxBadgeBgColor);
+  /* Design color: #5c707a */
+  color: var(--badgeIconColor);
+  border-radius: 2px;
+  border-bottom: 1px solid var(--controlsBoxShadowColor);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+`
+
+export const DisabledBar = () => (
+  <StyledDisabledBar>
+    <Text size="xs">Coming soon</Text>
+  </StyledDisabledBar>
+)

7f70d1d2e68e5db4f2b755bab6a48e2e3eaa002d
Pavel Angelov <pavel.angelov@redis.com>
2025-07-11 12:07:45 +0300

    asd

--- redisinsight/ui/src/components/new-index/selection-box/SelectionBox.tsx
+++ redisinsight/ui/src/components/new-index/selection-box/SelectionBox.tsx
@@ -12,6 +12,7 @@ export interface BoxSelectionOption<T extends string = string>
   text?: string
 }
 
+// Try with something else
 type SelectionBoxProps<T extends string = string> = {
   box: BoxSelectionOption<T>
 } & HTMLAttributes<HTMLButtonElement>

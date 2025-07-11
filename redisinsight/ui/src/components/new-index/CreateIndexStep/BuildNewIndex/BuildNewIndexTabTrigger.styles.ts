8deb9154d6546145c51daf4770bbf60c23c6cef9
Valentin Kirilov <valentin.kirilov@redis.com>
2025-07-11 13:16:28 +0300

    RI-7191 Selector tabs component (#4670)
    * feat: created base wrapper for the "create index" step part of the new vector search
    * integrate the tab component to serve as a base wrapper
    * created a minimal implementation for the "use preset index" scenario
    
    re #RI-7191

---
+++ redisinsight/ui/src/components/new-index/CreateIndexStep/BuildNewIndex/BuildNewIndexTabTrigger.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const StyledBuildNewIndexTabTrigger = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.core.space.space050};
+`
---
+++ redisinsight/ui/src/components/new-index/CreateIndexStep/CreateIndexStepWrapper.spec.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { fireEvent, render, screen } from 'uiSrc/utils/test-utils'
+import { CreateIndexStepWrapper } from './CreateIndexStepWrapper'
+
+const renderComponent = () => render(<CreateIndexStepWrapper />)
+
+describe('CreateIndexStepWrapper', () => {
+  it('should render', () => {
+    const { container } = renderComponent()
+
+    expect(container).toBeTruthy()
+
+    // Check if the tabs are rendered
+    const buildNewIndexTabTrigger = screen.getByText('Build new index')
+    const usePresetIndexTabTrigger = screen.getByText('Use preset index')
+
+    expect(buildNewIndexTabTrigger).toBeInTheDocument()
+    expect(usePresetIndexTabTrigger).toBeInTheDocument()
+
+    // Check if the "Use preset index" tab content is selected by default
+    const usePresetIIndexTabContent = screen.queryByTestId(
+      'vector-inde-tabs--use-preset-index-content',
+    )
+    expect(usePresetIIndexTabContent).toBeInTheDocument()
+  })
+
+  it('should switch to "Use preset index" tab when clicked', () => {
+    renderComponent()
+
+    const buildNewIndexTabTrigger = screen.getByText('Use preset index')
+    fireEvent.click(buildNewIndexTabTrigger)
+
+    // Check if the "Use preset index" tab is rendered
+    const buildNewIndexTabContent = screen.queryByTestId(
+      'vector-inde-tabs--use-preset-index-content',
+    )
+    expect(buildNewIndexTabContent).toBeInTheDocument()
+  })
+
+  it('shouldn\'t switch to "Build new index" tab when clicked, since it is disabled', () => {
+    renderComponent()
+
+    const buildNewIndexTabTriggerLabel = screen.getByText('Build new index')
+    const buildNewIndexTabTriggerButton =
+      buildNewIndexTabTriggerLabel.closest('[type="button"]')
+
+    expect(buildNewIndexTabTriggerButton).toHaveAttribute('disabled')
+    expect(buildNewIndexTabTriggerButton).toHaveAttribute('data-disabled')
+
+    // And when clicked, it should not change the active tab
+    fireEvent.click(buildNewIndexTabTriggerLabel)
+
+    // Check if the "Use preset index" tab is still active
+    const usePresetIndexTabContent = screen.queryByTestId(
+      'vector-inde-tabs--use-preset-index-content',
+    )
+    expect(usePresetIndexTabContent).toBeInTheDocument()
+  })
+})

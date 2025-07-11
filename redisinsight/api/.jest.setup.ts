0e3993538097ea25629efff915bf7ca3fec580dc
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-07-11 19:01:01 +0300

    RI-000 poc for BE unit tests

--- redisinsight/api/.jest.setup.ts
+++ redisinsight/api/.jest.setup.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import { Logger } from '@nestjs/common';
+
 // Workaround for @Type test coverage
 jest.mock('class-transformer', () => {
   return {
@@ -6,4 +8,12 @@ jest.mock('class-transformer', () => {
     Type: (f: Function) =>
       f() && jest.requireActual('class-transformer').Type(f),
   };
+});
+
+beforeAll(() => {
+  // Disable NestJS logger during tests
+  jest.spyOn(Logger.prototype, 'debug').mockImplementation(() => {});
+  jest.spyOn(Logger.prototype, 'log').mockImplementation(() => {});
+  jest.spyOn(Logger.prototype, 'warn').mockImplementation(() => {});
+  jest.spyOn(Logger.prototype, 'error').mockImplementation(() => {});
 });

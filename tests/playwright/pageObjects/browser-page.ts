6adbc03236310abe7b3badc7829c506adffc6062
Valentin Kirilov <valentin.kirilov@redis.com>
2025-07-18 13:18:00 +0300

    test: verify edit operation for string key value in browsers module
    * added e2e test to verify whether the edit key value functionality is working fine for the string type in the browser module
    
    re #RI-6570

--- tests/playwright/pageObjects/browser-page.ts
+++ tests/playwright/pageObjects/browser-page.ts
@@ -1600,4 +1600,29 @@ export class BrowserPage extends BasePage {
         await this.verifyKeyTTL(expectedTTL)
         await this.closeKeyDetailsAndVerify()
     }
+
+    async cancelStringKeyValueEdit(newValue: string): Promise<void> {
+        await this.editKeyValueButton.click()
+        await this.stringKeyValueInput.clear()
+        await this.stringKeyValueInput.fill(newValue)
+        await this.keyDetailsHeader.click()
+    }
+
+    async waitForKeyLengthToUpdate(expectedLength: string): Promise<void> {
+        await expect
+            .poll(async () => {
+                const keyLength = await this.getKeyLength()
+                return keyLength
+            })
+            .toBe(expectedLength)
+    }
+
+    async waitForStringValueToUpdate(expectedValue: string): Promise<void> {
+        await expect
+            .poll(async () => {
+                const currentValue = await this.getStringKeyValue()
+                return currentValue
+            })
+            .toContain(expectedValue)
+    }
 }

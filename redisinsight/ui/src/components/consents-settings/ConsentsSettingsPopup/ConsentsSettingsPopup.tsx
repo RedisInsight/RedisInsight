dd2c5a4ab82046e651731722edb3286571fe559b
Dijana Antovska <dijana.antovska@redis.com>
2025-07-23 01:42:59 +0300

    replace consents settings popup modal

--- redisinsight/ui/src/components/consents-settings/ConsentsSettingsPopup/ConsentsSettingsPopup.tsx
+++ redisinsight/ui/src/components/consents-settings/ConsentsSettingsPopup/ConsentsSettingsPopup.tsx
@@ -1,30 +1,22 @@
-import React, { useContext, useEffect } from 'react'
-import {
-  EuiOverlayMask,
-  EuiModal,
-  EuiModalBody,
-  EuiModalHeader,
-} from '@elastic/eui'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
-import cx from 'classnames'
 
 import { BuildType } from 'uiSrc/constants/env'
 import { appInfoSelector } from 'uiSrc/slices/app/info'
-import { Pages, Theme } from 'uiSrc/constants'
+import { Pages } from 'uiSrc/constants'
 import { ConsentsSettings } from 'uiSrc/components'
-import { ThemeContext } from 'uiSrc/contexts/themeContext'
 import { sendEventTelemetry, TelemetryEvent } from 'uiSrc/telemetry'
 
 import { FlexItem, Row } from 'uiSrc/components/base/layout/flex'
 import { RiIcon } from 'uiSrc/components/base/icons/RiIcon'
 import { Title } from 'uiSrc/components/base/text/Title'
+import { Modal } from 'uiSrc/components/base/display'
 import styles from '../styles.module.scss'
 
 const ConsentsSettingsPopup = () => {
   const history = useHistory()
   const { server } = useSelector(appInfoSelector)
-  const { theme } = useContext(ThemeContext)
 
   const handleSubmitted = () => {
     if (
@@ -43,34 +35,25 @@ const ConsentsSettingsPopup = () => {
   }, [])
 
   return (
-    <EuiOverlayMask
-      className={cx(
-        styles.overlay,
-        theme === Theme.Dark ? styles.overlay_dark : styles.overlay_light,
-      )}
-    >
-      <EuiModal
-        className={styles.consentsPopup}
-        onClose={() => {}}
-        data-testid="consents-settings-popup"
-      >
-        <EuiModalHeader className={styles.modalHeader}>
-          <Row justify="between">
-            <FlexItem>
-              <Title size="L" className={styles.consentsPopupTitle}>
-                EULA and Privacy Settings
-              </Title>
-            </FlexItem>
-            <FlexItem>
-              <RiIcon className={styles.redisIcon} type="RedisLogoFullIcon" />
-            </FlexItem>
-          </Row>
-        </EuiModalHeader>
-        <EuiModalBody className={styles.modalBody}>
-          <ConsentsSettings onSubmitted={handleSubmitted} />
-        </EuiModalBody>
-      </EuiModal>
-    </EuiOverlayMask>
+    <Modal
+      open
+      persistent
+      className={styles.consentsPopup}
+      data-testid="consents-settings-popup"
+      title={
+        <Row justify="between">
+          <FlexItem>
+            <Title size="L" className={styles.consentsPopupTitle}>
+              EULA and Privacy Settings
+            </Title>
+          </FlexItem>
+          <FlexItem>
+            <RiIcon className={styles.redisIcon} type="RedisLogoFullIcon" />
+          </FlexItem>
+        </Row>
+      }
+      content={<ConsentsSettings onSubmitted={handleSubmitted} />}
+    />
   )
 }
 

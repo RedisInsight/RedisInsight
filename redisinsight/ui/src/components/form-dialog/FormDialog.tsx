b1c3f3943195ac7303cbe81df8ca2fe30d4b6f0d
Dijana Antovska <dijana.antovska@redis.com>
2025-07-22 14:42:23 +0300

    apply the form dialog; vefiry for the database connection form; delete unneeded styles

--- redisinsight/ui/src/components/form-dialog/FormDialog.tsx
+++ redisinsight/ui/src/components/form-dialog/FormDialog.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import {
-  EuiModal,
-  EuiModalBody,
-  EuiModalFooter,
-  EuiModalHeader,
-  EuiModalHeaderTitle,
-} from '@elastic/eui'
-import { Nullable } from 'uiSrc/utils'
 
+import { Nullable } from 'uiSrc/utils'
+import { CancelIcon } from 'uiSrc/components/base/icons'
+import { Modal } from 'uiSrc/components/base/display'
 import styles from './styles.module.scss'
 
 export interface Props {
@@ -25,13 +20,17 @@ const FormDialog = (props: Props) => {
   if (!isOpen) return null
 
   return (
-    <EuiModal className={`${styles.modal} ${className}`} onClose={onClose}>
-      <EuiModalHeader>
-        <EuiModalHeaderTitle id="formModalHeader">{header}</EuiModalHeaderTitle>
-      </EuiModalHeader>
-      <EuiModalBody>{children}</EuiModalBody>
-      <EuiModalFooter>{footer}</EuiModalFooter>
-    </EuiModal>
+    <Modal.Compose open={isOpen}>
+      <Modal.Content.Compose
+        className={`${styles.modal} ${className}`}
+        onCancel={onClose}
+      >
+        <Modal.Content.Close icon={CancelIcon} onClick={onClose}/>
+        <Modal.Content.Header.Title>{header}</Modal.Content.Header.Title>
+        <Modal.Content.Body content={children} />
+        <Modal.Content.Footer.Compose>{footer}</Modal.Content.Footer.Compose>
+      </Modal.Content.Compose>
+    </Modal.Compose>
   )
 }
 
